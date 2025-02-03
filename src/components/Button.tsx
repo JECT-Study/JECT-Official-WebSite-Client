154875803ef84c1e65337f44c297375900ff5af1
onejuneee <onejuneee721@gmail.com>
2025-02-03 16:07:39 +0900

    feat: 버튼 디버깅을 위해 width 속성을 수정합니다.

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,7 +7,7 @@ const Button = ({ label, onClick }: ButtonProps) => {
   return (
     <button
       onClick={onClick}
-      className='rounded bg-[var(--color-accent-hero-light)] px-4 py-2 font-semibold text-white shadow transition-colors hover:bg-[var(--color-accent-hero-dark)]'
+      className='rounded bg-[var(--color-accent-hero-light)] px-3.5 py-2 font-semibold text-white shadow transition-colors hover:bg-[var(--color-accent-hero-dark)]'
     >
       {label}
     </button>
