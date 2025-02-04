481b16ddba942cbf74bc7edaa47071a7c4d742c3
onejuneee <onejuneee721@gmail.com>
2025-02-04 16:23:57 +0900

    feat: 추가 패키지를 통해 eslint와 prettier에서 충돌이 나는 구간을 제어합니다.

--- eslint.config.js
+++ eslint.config.js
@@ -3,34 +3,35 @@ import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import importPlugin from 'eslint-plugin-import';
 
 export default tseslint.config(
   { ignores: ['dist', '.storybook/**/*'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended, 'eslint-config-prettier'],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      tseslint.configs.recommendedTypeChecked,
+      eslintConfigPrettier,
+    ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
     plugins: {
+      import: importPlugin,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-      'import/order': [
-        'error',
-        {
-          groups: ['builtin', 'external', 'internal', ['parent', 'sibling', 'index']],
-          'newlines-between': 'always',
-          alphabetize: {
-            order: 'asc',
-            caseInsensitive: true,
-          },
-        },
-      ],
       '@typescript-eslint/naming-convention': [
         'error',
         {
@@ -45,18 +46,24 @@ export default tseslint.config(
         },
         {
           selector: 'variable',
-          modifiers: ['let'],
-          format: ['camelCase'],
-        },
-        {
-          selector: 'variable',
           types: ['boolean'],
-          format: ['camelCase'],
+          format: ['PascalCase'],
           prefix: ['is'],
         },
         {
           selector: 'typeAlias',
           format: ['PascalCase'],
+        },
+      ],
+      'import/order': [
+        'error',
+        {
+          groups: ['builtin', 'external', 'internal', ['parent', 'sibling', 'index']],
+          'newlines-between': 'always',
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
         },
       ],
     },
