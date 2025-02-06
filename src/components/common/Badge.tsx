eff48aa6fcc5a7a72da0e94c49d61d2d268d0ccb
rkdcodus <rcd7275@naver.com>
2025-02-06 21:59:16 +0900

    feat: Badge 컴포넌트 재구현
    Label 컴포넌트 이용

--- src/components/common/Badge.tsx
+++ src/components/common/Badge.tsx
@@ -1,42 +1,15 @@
-type Variant = 'normal' | 'info';
+import Label from './Label';
 
 interface BadgeProps {
-  variant: Variant;
   text: string;
+  backgroundColor: string;
+  textColor: string;
 }
 
-interface Color {
-  normal: {
-    bg: string;
-    text: string;
-  };
-  info: {
-    bg: string;
-    text: string;
-  };
-}
-
-function Badge({ variant, text }: BadgeProps) {
-  const color: Color = {
-    normal: {
-      bg: 'bg-fill-assistive-dark',
-      text: 'text-object-normal-dark',
-    },
-    info: {
-      bg: 'bg-feedback-trans-information-dark',
-      text: 'text-feedback-information-dark',
-    },
-  };
-
+function Badge({ text, backgroundColor, textColor }: BadgeProps) {
   return (
-    <div
-      className={`radius-2xs ${variant === 'normal' ? color.normal.bg : color.info.bg} inline-block px-(--gap-xs) py-(--gap-5xs)`}
-    >
-      <span
-        className={`label-lg whitespace-nowrap ${variant === 'normal' ? color.normal.text : color.info.text} leading-[27px]`}
-      >
-        {text}
-      </span>
+    <div className={`radius-2xs ${backgroundColor} inline-block px-(--gap-xs) py-(--gap-5xs)`}>
+      <Label hierarchy='stronger' weight='normal' text={text} textColor={textColor} />
     </div>
   );
 }
