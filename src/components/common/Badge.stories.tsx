2e17e4a97d2ab626fd8d9daab2ca4aaf63e290d9
rkdcodus <rcd7275@naver.com>
2025-02-07 00:07:15 +0900

    feat: Badge 스토리북 작성

---
+++ src/components/common/Badge.stories.tsx
@@ -0,0 +1,30 @@
+import { Meta, StoryObj } from '@storybook/react';
+
+import Badge from './Badge';
+
+const meta: Meta<typeof Badge> = {
+  title: 'Components/Badge',
+  component: Badge,
+  tags: ['autodocs'],
+  argTypes: {
+    text: { control: 'text', description: 'Badge에 들어갈 텍스트' },
+    backgroundColor: { control: 'color', description: '배경색' },
+    textColor: { control: 'color', description: '폰트 색상' },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<{
+  text: string;
+  backgroundColor: string;
+  textColor: string;
+}>;
+
+export const DEFAULT: Story = {
+  args: {
+    text: '레이블',
+    backgroundColor: 'bg-feedback-trans-information-dark',
+    textColor: 'text-feedback-information-dark',
+  },
+};
