af7fe97cc00f3a939a71488ffa4674b866177428
rkdcodus <rcd7275@naver.com>
2025-02-06 23:04:08 +0900

    refactor: InteractionStyle 객체 활용하여 중복되는 코드 정리

--- src/components/common/Interaction.tsx
+++ src/components/common/Interaction.tsx
@@ -1,5 +1,5 @@
-type Variant = 'default' | 'brand';
-type Density = 'bold' | 'normal' | 'subtle';
+import { Density, interactionStyle, Variant } from '@/styles/interactionStyle';
+
 type Radius =
   | 'radius-4xs'
   | 'radius-3xs'
@@ -17,78 +17,19 @@ interface InteractionProps {
   radius?: Radius;
 }
 
-export function Interaction({ children, variant, density, radius }: InteractionProps) {
-  if (variant === 'default' && density === 'bold') {
-    return (
-      <div>
-        <div
-          className={`hover:bg-object-hero-dark focus-visible::bg-object-hero-dark active:bg-object-hero-dark *:focus-visible:outline-interactive-focus-dark *:focus-visible:hover:opacity-visible inline-block *:hover:opacity-88 *:focus-visible:outline-4 *:active:opacity-78 ${radius}`}
-        >
-          {children}
-        </div>
-      </div>
-    );
-  }
+function Interaction({ children, variant, density, radius }: InteractionProps) {
+  const background = interactionStyle.variant[variant].density[density].bgColor;
+  const opacity = interactionStyle.variant[variant].density[density].opacity;
 
-  if (variant === 'default' && density === 'normal') {
-    return (
-      <div>
-        <div
-          className={`hover:bg-object-normal-dark focus-visible:bg-object-normal-dark active:bg-object-normal-dark *:focus-visible:outline-interactive-focus-dark *:focus-visible:hover:opacity-visible inline-block *:hover:opacity-92 *:focus-visible:outline-4 *:active:opacity-84 ${radius}`}
-        >
-          {children}
-        </div>
+  return (
+    <div>
+      <div
+        className={`hover:${background} focus-visible:${background} active:${background} *:hover:${opacity} *:active:${opacity} ${radius} *:focus-visible:outline-interactive-focus-dark *:focus-visible:hover:opacity-visible inline-block *:focus-visible:outline-4`}
+      >
+        {children}
       </div>
-    );
-  }
-
-  if (variant === 'default' && density === 'subtle') {
-    return (
-      <div>
-        <div
-          className={`hover:bg-object-neutral-dark active:bg-object-neutral-dark focus-visible:bg-object-neutral-dark *:focus-visible:outline-interactive-focus-dark *:focus-visible:hover:opacity-visible inline-block *:hover:opacity-95 *:focus-visible:outline-4 *:active:opacity-88 ${radius}`}
-        >
-          {children}
-        </div>
-      </div>
-    );
-  }
-
-  if (variant === 'brand' && density === 'bold') {
-    return (
-      <div>
-        <div
-          className={`hover:bg-accent-hero-dark active:bg-accent-hero-dark focus-visible:bg-accent-hero-dark *:focus-visible:outline-interactive-focus-dark *:focus-visible:hover:opacity-visible inline-block *:hover:opacity-88 *:focus-visible:outline-4 *:active:opacity-78 ${radius}`}
-        >
-          {children}
-        </div>
-      </div>
-    );
-  }
-
-  if (variant === 'brand' && density === 'normal') {
-    return (
-      <div>
-        <div
-          className={`hover:bg-accent-normal-dark active:bg-accent-normal-dark focus-visible:bg-accent-normal-dark *:focus:outline-interactive-focus-dark *:focus-visible:hover:opacity-visible inline-block *:hover:opacity-92 *:focus-visible:outline-4 *:active:opacity-84 ${radius}`}
-        >
-          {children}
-        </div>
-      </div>
-    );
-  }
-
-  if (variant === 'brand' && density === 'subtle') {
-    return (
-      <div>
-        <div
-          className={`hover:bg-accent-neutral-dark active:bg-accent-neutral-dark focus-visible:bg-accent-neutral-dark *:focus-visible:outline-interactive-focus-dark *:focus-visible:hover:opacity-visible inline-block *:hover:opacity-95 *:focus-visible:outline-4 *:active:opacity-88 ${radius}`}
-        >
-          {children}
-        </div>
-      </div>
-    );
-  }
-
-  return;
+    </div>
+  );
 }
+
+export default Interaction;
