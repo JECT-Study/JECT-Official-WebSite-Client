665a7063018400a5668224a393a4615430c12c4c
rkdcodus <rcd7275@naver.com>
2025-02-07 15:48:49 +0900

    style: 코드 정리

--- src/components/common/Label.tsx
+++ src/components/common/Label.tsx
@@ -9,10 +9,11 @@ interface LabelProps {
 }
 
 function Label({ weight, hierarchy, text, textColor, isRequired }: LabelProps) {
+  const typo = labelStyle.weight[weight].hierarchy[hierarchy].typo;
+  const lineHeight = labelStyle.weight[weight].hierarchy[hierarchy].lineHeight;
+
   return (
-    <div
-      className={`${labelStyle.weight[weight].hierarchy[hierarchy].typo} ${labelStyle.weight[weight].hierarchy[hierarchy].lineHeight} gap-5xs flex`}
-    >
+    <div className={`${typo} ${lineHeight} gap-5xs flex`}>
       <span className={`${textColor} whitespace-nowrap`}>{text}</span>
       {isRequired && <span className='text-feedback-notification-dark'>*</span>}
     </div>
