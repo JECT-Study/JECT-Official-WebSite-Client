624ef3be1103ab54541b136d3949de49e4487716
rkdcodus <rcd7275@naver.com>
2025-02-09 01:27:44 +0900

    feat: CalloutNumerical 컴포넌트 구현

---
+++ src/components/common/callout/CalloutNumerical.tsx
@@ -0,0 +1,23 @@
+import Label from '../Label';
+import Title from '../Title';
+
+interface CalloutProps {
+  title: string;
+  labelText: string;
+}
+
+function CalloutNumerical({ title, labelText }: CalloutProps) {
+  return (
+    <div className='bg-surface-deep-dark border-border-assistive-dark gap-md radius-sm flex flex-col items-center border px-(--gap-4xl) py-(--gap-3xl)'>
+      <Title hierarchy='stronger'>{title}</Title>
+      <Label
+        hierarchy='normal'
+        weight='bold'
+        text={labelText}
+        textColor='text-object-neutral-dark'
+      ></Label>
+    </div>
+  );
+}
+
+export default CalloutNumerical;
