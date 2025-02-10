fb9253f15f51239de2c20f5fe805493a5250df9f
rkdcodus <rcd7275@naver.com>
2025-02-10 22:03:12 +0900

    feat: Progress 관련 스토리북 작성 (Bridge 컴포넌트 제외)

---
+++ src/components/common/progress/ProgressIndicator.stories.tsx
@@ -0,0 +1,36 @@
+import { Meta, StoryObj } from '@storybook/react';
+
+import ProgressIndicator from './ProgressIndicator';
+
+const meta: Meta<typeof ProgressIndicator> = {
+  title: 'Components/Progress/ProgressIndicator',
+  component: ProgressIndicator,
+  argTypes: {
+    totalStep: {
+      control: 'number',
+      description: '단계의 총 개수를 적어줍니다.',
+    },
+    currentStep: {
+      control: 'number',
+      description: '현재까지 active된 마지막 단계를 적어줍니다.',
+    },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof ProgressIndicator>;
+
+export const Default: Story = {
+  args: {
+    totalStep: 3,
+    currentStep: 1,
+  },
+};
+
+export const Primary: Story = {
+  name: 'ProgressIndicator',
+  render: () => {
+    return <ProgressIndicator totalStep={4} currentStep={3}></ProgressIndicator>;
+  },
+};
