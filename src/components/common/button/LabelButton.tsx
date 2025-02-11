d25eb474fdfc66a74ad0ec4d8405d81ab2fcf59b
onejuneee <onejuneee721@gmail.com>
2025-02-11 22:34:54 +0900

    refactor: 버튼 컴포넌트가 Interaction 컴포넌트에 맞춰진 radius를 가지도록 설정합니다.

--- src/components/common/button/LabelButton.tsx
+++ src/components/common/button/LabelButton.tsx
@@ -20,7 +20,7 @@ export interface LabelButtonProps extends ComponentPropsWithoutRef<'button'> {
 export const LabelButton = forwardRef<HTMLButtonElement, LabelButtonProps>(
   ({ children, leftIcon, rightIcon, size, hierarchy, className, ...props }, ref) => {
     const baseClasses =
-      'inline-flex flex-row py-0 px-0 justify-center items-center gap-4xs rounded-[var(--radius-none,0px)]';
+      'inline-flex flex-row py-0 px-0 justify-center items-center gap-4xs radius-xs';
 
     const combinedClasses = clsx(
       baseClasses,
