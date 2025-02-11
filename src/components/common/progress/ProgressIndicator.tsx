29fcda8d56a6b0ced68ea278a5f926b67119bc6f
codus <97906653+rkdcodus@users.noreply.github.com>
2025-02-11 23:42:18 +0900

    feat: progress 컴포넌트 구현 (#29)
    * feat: ProgressIndex 컴포넌트 구현
    
    * feat: ProgressBridge 및 ProgressIndicator 컴포넌트 구현
    
    * feat: ProgressItem 컴포넌트 구현
    
    * feat: Title textColor 선택적인 Prop 추가
    
    * feat: ProgressVerticalBridge 구현
    
    * feat: Progress 관련 스토리북 작성 (Bridge 컴포넌트 제외)
    
    * refactor: active style 상수 분리
    
    * remove: console.log 제거
    
    * feat: new Array -> Array.from() 방식으로 변경
    
    * feat: Title || -> ?? 널 병합 연산자로 변경
    
    undefined, null 일 경우에만 기본 폰트 색상 적용 (빈문자열과 같이 실수로 값을 넣었을 경우는 스타일 적용 X)

---
+++ src/components/common/progress/ProgressIndicator.tsx
@@ -0,0 +1,24 @@
+import ProgressBridge from './ProgressBridge';
+import ProgressIndex from './ProgressIndex';
+
+interface ProgressIndicatorProps {
+  totalStep: number;
+  currentStep: number;
+}
+
+function ProgressIndicator({ totalStep, currentStep }: ProgressIndicatorProps) {
+  return (
+    <div className='flex'>
+      <ProgressIndex isActive={true}>{1}</ProgressIndex>
+      {totalStep > 1 &&
+        Array.from({ length: totalStep - 1 }).map((_, index) => (
+          <>
+            <ProgressBridge isActive={currentStep >= index + 2} />
+            <ProgressIndex isActive={currentStep >= index + 2}>{index + 2}</ProgressIndex>
+          </>
+        ))}
+    </div>
+  );
+}
+
+export default ProgressIndicator;
---
+++ src/components/common/progress/ProgressItem.tsx
@@ -0,0 +1,36 @@
+import Title from '../Title';
+import ProgressIndex from './ProgressIndex';
+
+interface ProgressItemProps {
+  index: number;
+  title: string;
+  subTitle: string;
+  content: string;
+  isActive: boolean;
+}
+
+const ACTIVE_STYLE = 'border-border-alternative-dark bg-accent-trans-normal-dark';
+const INACTIVE_STYLE = 'border-border-assistive-dark bg-surface-deep-dark';
+
+function ProgressItem({ index, title, subTitle, content, isActive }: ProgressItemProps) {
+  const activeStyle = isActive ? ACTIVE_STYLE : INACTIVE_STYLE;
+
+  return (
+    <div
+      className={`${activeStyle} radius-xs gap-3xl flex items-center border px-(--gap-3xl) py-(--gap-2xl)`}
+    >
+      <ProgressIndex isActive={isActive}>{index}</ProgressIndex>
+      <div className='gap-xs flex flex-col'>
+        <div className='gap-sm flex items-center'>
+          <Title hierarchy='normal'>{title}</Title>
+          <Title hierarchy='weak' textColor='text-object-alternative-dark'>
+            {subTitle}
+          </Title>
+        </div>
+        <p className='body-lg text-object-normal-dark'>{content}</p>
+      </div>
+    </div>
+  );
+}
+
+export default ProgressItem;
