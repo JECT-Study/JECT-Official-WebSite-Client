5517a21119c3a5a45df7eca21f1220c41d04bcd1
rkdcodus <rcd7275@naver.com>
2025-02-12 02:31:18 +0900

    feat: router 세팅 및 /apply 라우터 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import routerList from './utils/router';
+
+const router = createBrowserRouter(routerList);
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
