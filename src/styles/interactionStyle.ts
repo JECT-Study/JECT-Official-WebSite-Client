442deee605024a0990e2bbdfcc24ac4057b59a70
rkdcodus <rcd7275@naver.com>
2025-02-12 23:54:42 +0900

    feat: Interaction 컴포넌트 isInversed props 추가

--- src/styles/interactionStyle.ts
+++ src/styles/interactionStyle.ts
@@ -2,28 +2,37 @@ export type Variant = 'default' | 'brand';
 export type Density = 'bold' | 'normal' | 'subtle';
 
 interface InteractionStyleType {
-  variant: {
-    [key in Variant]: {
-      density: Record<Density, string>;
-    };
+  noInverse: {
+    [key in Variant]: Record<Density, string>;
+  };
+  inverse: {
+    [key in Variant]: Record<Density, string>;
   };
 }
-// peer-hover:bg-[rgba(255,255,255,0.12)] peer-active:bg-[rgba(255,255,255,0.12)]
+
 export const interactionStyle: InteractionStyleType = {
-  variant: {
+  noInverse: {
     default: {
-      density: {
-        bold: 'peer-hover:bg-[rgba(255,255,255,0.12)] peer-active:bg-[rgba(255,255,255,0.12)]',
-        normal: 'peer-hover:bg-[rgba(212,211,222,0.08)] peer-active:bg-[rgba(212,211,222,0.08)]',
-        subtle: 'peer-hover:bg-[rgba(247,245,255,0.033)] peer-active:bg-[rgba(247,245,255,0.033)]',
-      },
+      bold: 'peer-hover:bg-[rgba(255,255,255,0.12)] peer-active:bg-[rgba(255,255,255,0.12)]',
+      normal: 'peer-hover:bg-[rgba(212,211,222,0.08)] peer-active:bg-[rgba(212,211,222,0.08)]',
+      subtle: 'peer-hover:bg-[rgba(247,245,255,0.033)] peer-active:bg-[rgba(247,245,255,0.033)]',
     },
     brand: {
-      density: {
-        bold: 'peer-hover:bg-[rgba(145,147,255,0.12)] peer-active:bg-[rgba(145,147,255,0.12)]',
-        normal: 'peer-hover:bg-[rgba(113,115,255,0.08)] peer-active:bg-[rgba(113,115,255,0.08)]',
-        subtle: 'peer-hover:bg-[rgba(87,88,227,0.05)] peer-active:bg-[rgba(87,88,227,0.05)]',
-      },
+      bold: 'peer-hover:bg-[rgba(145,147,255,0.12)] peer-active:bg-[rgba(145,147,255,0.12)]',
+      normal: 'peer-hover:bg-[rgba(113,115,255,0.08)] peer-active:bg-[rgba(113,115,255,0.08)]',
+      subtle: 'peer-hover:bg-[rgba(87,88,227,0.05)] peer-active:bg-[rgba(87,88,227,0.05)]',
+    },
+  },
+  inverse: {
+    default: {
+      bold: 'peer-hover:bg-[rgba(26,27,35,0.12)] peer-active:bg-[rgba(26,27,35,0.12)]',
+      normal: 'peer-hover:bg-[rgba(58,59,67,0.08)] peer-active:bg-[rgba(58,59,67,0.08)]',
+      subtle: 'peer-hover:bg-[rgba(2,3,18,0.032)] peer-active:bg-[rgba(2,3,18,0.032)]',
+    },
+    brand: {
+      bold: 'peer-hover:bg-[rgba(48,45,190,0.12)] peer-active:bg-[rgba(48,45,190,0.12)]',
+      normal: 'peer-hover:bg-[rgba(74,74,214,0.08)] peer-active:bg-[rgba(74,74,214,0.08)]',
+      subtle: 'peer-hover:bg-[rgba(100,101,241,0.05)] peer-active:bg-[rgba(100,101,241,0.05)]',
     },
   },
 };
