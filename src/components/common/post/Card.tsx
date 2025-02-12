1a893fc431bdf97350d89076c30f7b6ddda81e83
onejuneee <onejuneee721@gmail.com>
2025-02-13 04:28:44 +0900

    feat: descriptionVisible props에 따라 children 요소의 출력 유무를 조건부로 렌더링합니다.

--- src/components/common/post/Card.tsx
+++ src/components/common/post/Card.tsx
@@ -8,26 +8,31 @@ interface CardProps {
   label: string;
   children: ReactNode;
   imgUrl: string;
+  descriptionVisible?: boolean;
 }
 
-export const Card = ({ title, label, children, imgUrl }: CardProps) => {
+const IMG_CLASS_DEFAULT =
+  'border-border-assistive-dark h-[12.0625rem] w-full border-b object-cover';
+const IMG_CLASS_EXPANDED = 'border-border-assistive-dark w-full flex-1 border-b object-cover';
+
+export const Card = ({ title, label, children, imgUrl, descriptionVisible = true }: CardProps) => {
+  const imgClass = descriptionVisible ? IMG_CLASS_DEFAULT : IMG_CLASS_EXPANDED;
+
   return (
     <div className='radius-md stroke-normal border-border-assistive-dark bg-surface-embossed-dark box-border flex h-[21.25rem] w-[17.5rem] flex-col items-start overflow-hidden border'>
-      <img
-        src={imgUrl}
-        alt='카드 이미지'
-        className='h=-[12.0625rem] border-border-assistive-dark box-border w-full border-b object-cover'
-      />
-      <div className='gap-3xs flex flex-[1_0_0] flex-col items-start self-stretch px-(--gap-md) py-(--gap-xs)'>
+      <img src={imgUrl} alt='카드 이미지' className={imgClass} />
+      <div className='gap-3xs flex flex-col items-start self-stretch px-(--gap-md) py-(--gap-xs)'>
         <div className='gap-4xs flex flex-col items-start self-stretch'>
           <Title hierarchy='weak'>{title}</Title>
           <Label hierarchy='stronger' weight='normal' textColor='text-object-normal-dark'>
             {label}
           </Label>
         </div>
-        <div className='body-lg text-object-neutral-dark min-h-[3rem] flex-[1_0_0] self-stretch'>
-          {children}
-        </div>
+        {descriptionVisible && (
+          <div className='body-lg text-object-neutral-dark min-h-[3rem] flex-[1_0_0] self-stretch'>
+            {children}
+          </div>
+        )}
       </div>
     </div>
   );
