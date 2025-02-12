34f9a824dd347f522d72aded318b0265f0564dea
rkdcodus <rcd7275@naver.com>
2025-02-13 03:19:33 +0900

    Merge branch 'dev' of https://github.com/JECT-Study/JECT-Official-WebSite-Client into feat/24-input

---
+++ src/components/common/input/InputField.tsx
@@ -0,0 +1,26 @@
+import { forwardRef, InputHTMLAttributes, ReactNode } from 'react';
+
+interface InputFieldProps extends InputHTMLAttributes<HTMLInputElement> {
+  isNegative: boolean;
+  children?: ReactNode;
+}
+
+const InputField = forwardRef<HTMLInputElement, InputFieldProps>(
+  ({ children, isNegative, disabled, ...props }, ref) => {
+    return (
+      <div
+        className={`${disabled ? 'bg-surface-standard-dark' : 'bg-surface-embossed-dark'} ${isNegative ? (disabled ? 'border-feedback-trans-negative-dark' : 'border-feedback-negative-dark') : disabled ? 'border-border-trans-assistive-dark' : 'border-border-trans-assistive-dark hover:border-border-trans-neutral-dark focus-within:border-border-trans-hero-dark focus-within:hover:border-border-trans-hero-dark'} gap-2xs radius-xs flex border px-(--gap-md) py-(--gap-sm)`}
+      >
+        <input
+          {...props}
+          ref={ref}
+          disabled={disabled}
+          className={`${disabled ? 'placeholder:text-object-disabled-dark' : 'placeholder:text-object-assistive-dark'} caret-object-hero-dark text-object-hero-dark body-md outline-none`}
+        />
+        {children}
+      </div>
+    );
+  },
+);
+
+export default InputField;
