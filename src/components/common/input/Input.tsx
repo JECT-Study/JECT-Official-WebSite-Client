1361afdf66555863bdfc082e2c3095c43c77310f
rkdcodus <rcd7275@naver.com>
2025-02-14 02:40:31 +0900

    rename: InputField -> Input 컴포넌트명 변경

---
+++ src/components/common/input/Input.tsx
@@ -0,0 +1,26 @@
+import { forwardRef, InputHTMLAttributes, ReactNode } from 'react';
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  isNegative: boolean;
+  children?: ReactNode;
+}
+
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ children, isNegative, disabled, ...props }, ref) => {
+    return (
+      <div
+        className={`${disabled ? 'bg-surface-standard-dark' : 'bg-surface-embossed-dark'} ${isNegative ? (disabled ? 'border-feedback-trans-negative-dark' : 'border-feedback-negative-dark') : disabled ? 'border-border-trans-assistive-dark' : 'border-border-trans-assistive-dark hover:border-border-trans-neutral-dark focus-within:border-border-trans-hero-dark focus-within:hover:border-border-trans-hero-dark'} duration-faster gap-2xs radius-xs flex items-center border px-(--gap-md) py-(--gap-sm) ease-(--motion-fluent)`}
+      >
+        <input
+          {...props}
+          ref={ref}
+          disabled={disabled}
+          className={`${disabled ? 'placeholder:text-object-disabled-dark' : 'placeholder:text-object-assistive-dark'} caret-object-hero-dark text-object-hero-dark body-md grow outline-none`}
+        />
+        {children}
+      </div>
+    );
+  },
+);
+
+export default Input;
