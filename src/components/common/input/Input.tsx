15f78da87b4a4de48c621319e1d1d6b71a54de87
rkdcodus <rcd7275@naver.com>
2025-02-14 04:55:25 +0900

    style: Input 코드 정리

--- src/components/common/input/Input.tsx
+++ src/components/common/input/Input.tsx
@@ -1,21 +1,40 @@
+import clsx from 'clsx';
 import { forwardRef, InputHTMLAttributes, ReactNode } from 'react';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
-  isNegative: boolean;
+  isError: boolean;
   children?: ReactNode;
+  className?: string;
 }
 
 const Input = forwardRef<HTMLInputElement, InputProps>(
-  ({ children, isNegative, disabled, ...props }, ref) => {
+  ({ children, isError, disabled, className, ...props }, ref) => {
     return (
       <div
-        className={`${disabled ? 'bg-surface-standard-dark' : 'bg-surface-embossed-dark'} ${isNegative ? (disabled ? 'border-feedback-trans-negative-dark' : 'border-feedback-negative-dark') : disabled ? 'border-border-trans-assistive-dark' : 'border-border-trans-assistive-dark hover:border-border-trans-neutral-dark focus-within:border-border-trans-hero-dark focus-within:hover:border-border-trans-hero-dark'} duration-faster gap-2xs radius-xs flex items-center border px-(--gap-md) py-(--gap-sm) ease-(--motion-fluent)`}
+        className={
+          clsx( {
+            'bg-surface-standard-dark': disabled,
+            'bg-surface-embossed-dark': !disabled,
+            'border-feedback-trans-negative-dark': isError && disabled,
+            'border-feedback-negative-dark' :  isError && !disabled,
+            'border-border-trans-assistive-dark': !isError && disabled,
+            'border-border-trans-assistive-dark hover:border-border-trans-neutral-dark focus-within:border-border-trans-hero-dark focus-within:hover:border-border-trans-hero-dark': !isError && !disabled,
+          },
+        className,
+        'duration-faster gap-2xs radius-xs flex items-center border px-(--gap-md) py-(--gap-sm) ease-(--motion-fluent)'
+        )}
       >
         <input
           {...props}
           ref={ref}
           disabled={disabled}
-          className={`${disabled ? 'placeholder:text-object-disabled-dark' : 'placeholder:text-object-assistive-dark'} caret-object-hero-dark text-object-hero-dark body-md grow outline-none`}
+          className={`
+            body-md grow outline-none
+            disabled:placeholder:text-object-disabled-dark 
+            placeholder:text-object-assistive-dark 
+            caret-object-hero-dark 
+            text-object-hero-dark 
+          `}
         />
         {children}
       </div>
