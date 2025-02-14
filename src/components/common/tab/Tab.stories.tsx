05f4e9fe667492378be9ab8c8815af04c3ce7059
onejuneee <onejuneee721@gmail.com>
2025-02-15 05:56:34 +0900

    feat: 수정된 컴포넌트를 스토리북에 반영합니다.

--- src/components/common/tab/Tab.stories.tsx
+++ src/components/common/tab/Tab.stories.tsx
@@ -1,15 +1,23 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import React from 'react';
 
-import { Tab, TabItem } from './Tab';
+import { Tab, TabHeader, TabItem, TabPanel } from './Tab';
 
-const meta: Meta<typeof Tab> = {
+const meta: Meta<typeof Tabs> = {
   title: 'Components/Tab',
   component: Tab,
+  parameters: {
+    docs: {
+      description: {
+        component:
+          'Tab 컴포넌트는  탭 헤더(TabHeader 및 TabItem)와 탭 콘텐츠(TabPanel)를 자유롭게 구성할 수 있습니다. Context API를 사용합니다.',
+      },
+    },
+  },
   argTypes: {
     defaultActiveTabId: {
       control: 'text',
-      description: '기본 활성 탭의 id를 설정합니다. 전달하지 않으면 첫 번째 탭이 활성화됩니다.',
+      description: '초기 활성 탭의 id를 설정합니다. string 형태입니다.',
     },
     onTabChange: {
       action: '탭 변경됨',
@@ -20,16 +28,32 @@ const meta: Meta<typeof Tab> = {
 
 export default meta;
 
-type Story = StoryObj<typeof Tab>;
+type Story = StoryObj<typeof Tabs>;
 
 export const TabStory: Story = {
   name: 'Tab',
-  render: () => (
-    <Tab>
-      <TabItem id='1' label='프론트엔드 개발자' />
-      <TabItem id='2' label='백엔드 개발자' />
-      <TabItem id='3' label='프로젝트 매니저' />
-      <TabItem id='4' label='프로덕트 디자이너' />
+  render: args => (
+    <Tab defaultActiveTabId='1' {...args}>
+      <TabHeader>
+        <TabItem id='1' label='프론트엔드 개발자' />
+        <TabItem id='2' label='백엔드 개발자' />
+        <TabItem id='3' label='프로젝트 매니저' />
+        <TabItem id='4' label='프로덕트 디자이너' />
+      </TabHeader>
+      <div>
+        <TabPanel id='1'>
+          <div>프론트엔드 개발자 콘텐츠</div>
+        </TabPanel>
+        <TabPanel id='2'>
+          <div>백엔드 개발자 콘텐츠</div>
+        </TabPanel>
+        <TabPanel id='3'>
+          <div>프로젝트 매니저 콘텐츠</div>
+        </TabPanel>
+        <TabPanel id='4'>
+          <div>프로덕트 디자이너 콘텐츠</div>
+        </TabPanel>
+      </div>
     </Tab>
   ),
 };
