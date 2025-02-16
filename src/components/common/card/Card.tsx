aea2ffd4d04b425d69734a5a19bf59b0ce024cda
onejuneee <onejuneee721@gmail.com>
2025-02-17 02:38:37 +0900

    refactor: 부모 container에 items-stretch를 적용하고, flex grow를 적용합니다.

--- src/components/common/card/Card.tsx
+++ src/components/common/card/Card.tsx
@@ -16,9 +16,9 @@ const IMG_CLASS_DEFAULT =
 const IMG_CLASS_EXPANDED = 'border-border-assistive-dark w-full flex-1 border-b object-cover';
 
 const CONTAINER_CLASS_VISIBLE =
-  'gap-3xs flex flex-col items-start self-stretch px-(--gap-md) py-(--gap-xs)';
+  'gap-3xs flex flex-col items-start flex-1  px-(--gap-md) py-(--gap-xs)';
 const CONTAINER_CLASS_HIDDEN =
-  'gap-3xs flex flex-col items-start self-stretch px-(--gap-md) pt-(--gap-xs) pb-(--gap-lg)';
+  'gap-3xs flex flex-col items-start flex-1  px-(--gap-md) pt-(--gap-xs) pb-(--gap-lg)';
 
 export const Card = ({
   title,
@@ -31,17 +31,17 @@ export const Card = ({
   const containerClass = isDescriptionVisible ? CONTAINER_CLASS_VISIBLE : CONTAINER_CLASS_HIDDEN;
 
   return (
-    <div className='radius-md stroke-normal border-border-assistive-dark bg-surface-embossed-dark box-border flex h-[21.25rem] w-[17.5rem] flex-col items-start overflow-hidden border'>
+    <div className='radius-md stroke-normal border-border-assistive-dark bg-surface-embossed-dark box-border flex h-[21.25rem] w-[17.5rem] flex-col items-stretch overflow-hidden border'>
       <img src={imgUrl} alt='카드 이미지' className={imgClass} />
       <div className={containerClass}>
-        <div className='gap-4xs flex flex-col items-start self-stretch'>
+        <div className='gap-4xs flex flex-col items-start'>
           <Title hierarchy='weak'>{title}</Title>
           <Label hierarchy='stronger' weight='normal' textColor='text-object-normal-dark'>
             {label}
           </Label>
         </div>
         {isDescriptionVisible && (
-          <div className='body-lg text-object-neutral-dark min-h-[3rem] flex-[1_0_0] self-stretch'>
+          <div className='body-lg text-object-neutral-dark min-h-[3rem] flex-[1_0_0]'>
             {children}
           </div>
         )}
