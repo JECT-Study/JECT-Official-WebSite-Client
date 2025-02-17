96f327d29b692a0bcb51f1ecbb31d8d2cbcebb75
rkdcodus <rcd7275@naver.com>
2025-02-18 02:39:36 +0900

    feat: 지원 과정 페이지(step1,2,3,완료) 라우터 설정

--- src/router.tsx
+++ src/router.tsx
@@ -1,9 +1,30 @@
+import ApplyComplete from './pages/ApplyComplete';
+import ApplyFirstStep from './pages/ApplyFirstStep';
+import ApplySecondStep from './pages/ApplySecondStep';
+import ApplyThirdStep from './pages/ApplyThirdStep';
+
 import Apply from '@/pages/Apply';
 
 const routerList = [
   {
     path: '/apply',
     element: <Apply />,
+  },
+  {
+    path: '/apply/step1',
+    element: <ApplyFirstStep />,
+  },
+  {
+    path: '/apply/step2',
+    element: <ApplySecondStep />,
+  },
+  {
+    path: '/apply/step3',
+    element: <ApplyThirdStep />,
+  },
+  {
+    path: '/apply/complete',
+    element: <ApplyComplete />,
   },
 ];
 
