013459e739af7dad2416a8c3a620c4bd08b2750d
onejuneee <onejuneee721@gmail.com>
2025-02-17 23:55:09 +0900

    feat: 수정된 LabelButton을 스토리북에 반영합니다.

--- src/components/common/button/LabelButton.stories.tsx
+++ src/components/common/button/LabelButton.stories.tsx
@@ -2,6 +2,8 @@ import type { Meta, StoryObj } from '@storybook/react';
 
 import LabelButton from './LabelButton';
 
+import Icon from '@/components/common/icon/Icon';
+
 const meta: Meta<typeof LabelButton> = {
   title: 'Components/LabelButton',
   component: LabelButton,
@@ -16,12 +18,34 @@ const meta: Meta<typeof LabelButton> = {
       options: ['accent', 'primary', 'secondary', 'tertiary'],
       description: '버튼의 색상이 분기되는 위계 요소입니다.',
     },
+    disabled: {
+      control: { type: 'boolean' },
+      description: '버튼 비활성화 여부입니다.',
+    },
+  },
+  args: {
+    size: 'lg',
+    hierarchy: 'accent',
+    disabled: false,
   },
 };
 
 export default meta;
 
 type Story = StoryObj<typeof LabelButton>;
+
+export const DefaultStory: Story = {
+  name: 'Default LabelButton',
+  render: args => (
+    <div className='story-container'>
+      <div className='story-inner-container'>
+        <div className='story-inner-row-container'>
+          <LabelButton {...args}>레이블</LabelButton>
+        </div>
+      </div>
+    </div>
+  ),
+};
 
 export const ButtonStory: Story = {
   name: 'Button',
@@ -41,6 +65,9 @@ export const ButtonStory: Story = {
           <LabelButton size='lg' hierarchy='tertiary'>
             레이블
           </LabelButton>
+          <LabelButton size='lg' hierarchy='accent' disabled={true}>
+            레이블
+          </LabelButton>
         </div>
       </div>
     </div>
@@ -48,40 +75,47 @@ export const ButtonStory: Story = {
 };
 
 export const IconButtonStory: Story = {
-  name: 'Icon Button',
-  render: () => (
+  name: 'Icon LabelButton',
+  render: args => (
     <div className='story-container'>
       <div className='story-inner-container'>
         <div className='story-inner-row-container'>
           <LabelButton
-            size='lg'
-            hierarchy='accent'
-            leftIcon={<span>→</span>}
-            rightIcon={<span>→</span>}
+            {...args}
+            leftIcon={<Icon name='check' size='md' fillColor='fill-accent-hero-dark' />}
+            rightIcon={<Icon name='check' size='md' fillColor='fill-accent-hero-dark' />}
           >
             레이블
           </LabelButton>
           <LabelButton
-            size='lg'
+            {...args}
             hierarchy='primary'
-            leftIcon={<span>→</span>}
-            rightIcon={<span>→</span>}
+            leftIcon={<Icon name='check' size='md' fillColor='fill-object-hero-dark' />}
+            rightIcon={<Icon name='check' size='md' fillColor='fill-object-hero-dark' />}
           >
             레이블
           </LabelButton>
           <LabelButton
-            size='lg'
+            {...args}
             hierarchy='secondary'
-            leftIcon={<span>→</span>}
-            rightIcon={<span>→</span>}
+            leftIcon={<Icon name='check' size='md' fillColor='fill-object-neutral-dark' />}
+            rightIcon={<Icon name='check' size='md' fillColor='fill-object-neutral-dark' />}
           >
             레이블
           </LabelButton>
           <LabelButton
-            size='lg'
+            {...args}
             hierarchy='tertiary'
-            leftIcon={<span>→</span>}
-            rightIcon={<span>→</span>}
+            leftIcon={<Icon name='check' size='md' fillColor='fill-object-alternative-dark' />}
+            rightIcon={<Icon name='check' size='md' fillColor='fill-object-alternative-dark' />}
+          >
+            레이블
+          </LabelButton>
+          <LabelButton
+            {...args}
+            leftIcon={<Icon name='check' size='md' fillColor='fill-accent-trans-hero-dark' />}
+            rightIcon={<Icon name='check' size='md' fillColor='fill-accent-trans-hero-dark' />}
+            disabled={true}
           >
             레이블
           </LabelButton>
