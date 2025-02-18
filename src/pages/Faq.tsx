99f03a2e675df1e4cc1806b8ca3236db52027ca7
rkdcodus <rcd7275@naver.com>
2025-02-19 00:20:42 +0900

    feat: faq 페이지 및 라우터 생성

---
+++ src/pages/Faq.tsx
@@ -0,0 +1,11 @@
+interface FaqProps {}
+
+function Faq({}: FaqProps) {
+  return (
+    <div className='bg-surface-standard-dark gap-12xl flex min-h-dvh flex-col items-center py-(--gap-12xl)'>
+      faq
+    </div>
+  );
+}
+
+export default Faq;
--- src/router.tsx
+++ src/router.tsx
@@ -1,9 +1,14 @@
 import Apply from '@/pages/Apply';
+import Faq from '@/pages/Faq';
 
 const routerList = [
   {
     path: '/apply',
     element: <Apply />,
+  },
+  {
+    path: '/faq',
+    element: <Faq />,
   },
 ];
 
