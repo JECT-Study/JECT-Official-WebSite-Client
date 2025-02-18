2557ae4e1a2f8223346a3137043c4b9e75f06913
rkdcodus <rcd7275@naver.com>
2025-02-18 19:21:53 +0900

    move: InputFile 컴포넌트 common/input 폴더로 이동

--- src/components/common/input/InputFile.tsx
+++ src/components/common/input/InputFile.tsx
@@ -1,11 +1,13 @@
 import { Dispatch, useState } from 'react';
 
-import File from './File';
-import Uploader from './Uploader';
+import File from '../file/File';
+import Uploader from '../file/Uploader';
 
 interface InputFileProps {
   setAnswers: Dispatch<React.SetStateAction<string | null>>;
 }
+
+// TODO: file 전체 용량 10MB로 제한 + Uploader isDisabled 설정
 
 function InputFile({ setAnswers }: InputFileProps) {
   const [fileList, setFileList] = useState<File[]>([]);
