89077720eaf8ed6014054c5f1c47019ad83e6549
rkdcodus <rcd7275@naver.com>
2025-02-18 21:18:05 +0900

    feat: InputFile 스토리북 description 추가

--- src/components/common/input/InputFile.stories.tsx
+++ src/components/common/input/InputFile.stories.tsx
@@ -6,6 +6,14 @@ import InputFile from './InputFile';
 const meta: Meta<typeof InputFile> = {
   title: 'Components/InputFile',
   component: InputFile,
+  parameters: {
+    docs: {
+      description: {
+        component: `InputFile 컴포넌트는 File 컴포넌트와 Uploader 컴포넌트를 조합한 컴포넌트 입니다. <br/> 파일의 추가, 삭제 기능이 가능합니다. 
+          직접 파일을 추가해 보세요! `,
+      },
+    },
+  },
   argTypes: {
     setAnswers: {
       description: '지원자 답변 상태를 업데이트하는 setter 함수입니다.',
