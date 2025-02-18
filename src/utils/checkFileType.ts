265161aeec42ae11ea68172bdf45c73d3fffc11c
rkdcodus <rcd7275@naver.com>
2025-02-18 22:08:02 +0900

    feat: checkFileType.ts에 정리한 FileExtension, fileMimeType 적용

--- src/utils/checkFileType.ts
+++ src/utils/checkFileType.ts
@@ -1,3 +1,5 @@
+import { FileExtension, fileMimeType } from '@/constants/file';
+
 /**
  * 주어진 FileList에서 모든 파일이 types 확장자 목록에 해당하는지 검사하는 함수
  *
@@ -6,18 +8,8 @@
  * @returns 모든 파일이 허용된 확장자에 해당하면 `true`, 그렇지 않으면 `false`
  *
  */
-type FileType = '.pdf' | '.jpeg' | '.png' | '.zip' | '.ppt';
-
-const fileType = {
-  '.pdf': 'application/pdf',
-  '.jpeg': '.jpg',
-  '.png': 'image/png',
-  '.zip': 'application/x-zip-compressed',
-  '.ppt': 'application/vnd.ms-powerpoint',
-};
-
-export const checkFileType = (files: FileList, types: FileType[]) => {
+export const checkFileType = (files: FileList, types: FileExtension[]) => {
   return Array.from(files).every(file => {
-    return types.some(type => fileType[type] === file.type);
+    return types.some(type => fileMimeType[type] === file.type);
   });
 };
