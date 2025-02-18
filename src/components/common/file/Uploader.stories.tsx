8c1b7d0e2fa8404a9a85e5e1a92e04727791d27e
rkdcodus <rcd7275@naver.com>
2025-02-18 19:55:42 +0900

    feat: Uploader 컴포넌트 스토리북 작성

---
+++ src/components/common/file/Uploader.stories.tsx
@@ -0,0 +1,49 @@
+import { Meta, StoryObj } from '@storybook/react';
+
+import Uploader from './Uploader';
+
+const meta: Meta<typeof Uploader> = {
+  title: 'Components/Uploader',
+  component: Uploader,
+  parameters: {
+    docs: {
+      description: {
+        component:
+          'Uploader 컴포넌트는 드래그 & 드롭 또는 버튼을 통해 파일을 업로드할 수 있습니다. 드래그 상태일 경우 드롭 박스의 디자인이 변경됩니다.',
+      },
+    },
+  },
+  argTypes: {
+    onChangeFile: {
+      action: 'onChange',
+      description:
+        '드래그&드롭 혹은 버튼을 통해 파일을 업로드했을 때 호출되는 함수입니다. 업로드된 파일 리스트를 인수로 받습니다. ',
+    },
+    isDisabled: {
+      control: 'boolean',
+      description: 'true일 경우, 파일 업로드가 불가능합니다.',
+    },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Uploader>;
+
+export const Default = {
+  args: {
+    onChangeFile: () => {},
+    isDisabled: false,
+  },
+};
+
+export const UploaderStory: Story = {
+  name: 'UploaderStory',
+  render: () => {
+    const addFile = (file: FileList | null) => {
+      console.log('file을 추가합니다.');
+    };
+
+    return <Uploader isDisabled={false} onChangeFile={addFile} />;
+  },
+};
