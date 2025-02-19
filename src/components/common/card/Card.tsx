dd604c80c59d05b6eceb7d07c313a618761eb88b
onejuneee <onejuneee721@gmail.com>
2025-02-19 16:14:56 +0900

    refactor: Card 컴포넌트에 disabled 속성을 추가합니다.

--- src/components/common/card/Card.tsx
+++ src/components/common/card/Card.tsx
@@ -1,10 +1,11 @@
+import clsx from 'clsx';
 import { ReactNode } from 'react';
 
 import Interaction from '@/components/common/interaction/Interaction';
 import Label from '@/components/common/Label';
 import Title from '@/components/common/Title';
 
-interface CardProps {
+interface CardProps extends ComponentPropsWithoutRef<'button'> {
   title: string;
   label: string;
   children: ReactNode;
@@ -27,9 +28,21 @@ export const Card = ({
   children,
   imgUrl,
   isDescriptionVisible = true,
+  disabled,
+  ...restProps
 }: CardProps) => {
   const imgClass = isDescriptionVisible ? IMG_CLASS_DEFAULT : IMG_CLASS_EXPANDED;
   const containerClass = isDescriptionVisible ? CONTAINER_CLASS_VISIBLE : CONTAINER_CLASS_HIDDEN;
+
+  const bgClass = disabled ? 'bg-surface-deep-dark' : 'bg-surface-embossed-dark';
+  const buttonClass = clsx(
+    'peer radius-md stroke-normal border-border-assistive-dark box-border flex h-[21.25rem] w-[17.5rem] flex-col items-stretch overflow-hidden border',
+    bgClass,
+  );
+
+  const titleTextColor = disabled ? 'text-object-disabled-dark' : 'text-object-hero-dark';
+  const labelTextColor = disabled ? 'text-object-disabled-dark' : 'text-object-normal-dark';
+  const descriptionTextColor = disabled ? 'text-object-disabled-dark' : 'text-object-neutral-dark';
 
   return (
     <Interaction
@@ -38,20 +51,19 @@ export const Card = ({
       isInversed={false}
       className='peer-hover:duration-slower peer-focus:duration-slower peer-hover:ease-(--motion-fluent) peer-focus:ease-(--motion-fluent)'
     >
-      <button
-        type='button'
-        className='peer radius-md stroke-normal border-border-assistive-dark bg-surface-embossed-dark box-border flex h-[21.25rem] w-[17.5rem] flex-col items-stretch overflow-hidden border'
-      >
+      <button type='button' disabled={!!disabled} className={buttonClass} {...restProps}>
         <img src={imgUrl} alt='카드 이미지' className={imgClass} />
         <div className={containerClass}>
           <div className='gap-4xs flex flex-col items-start'>
-            <Title hierarchy='weak'>{title}</Title>
-            <Label hierarchy='stronger' weight='normal' textColor='text-object-normal-dark'>
+            <Title hierarchy='weak' textColor={titleTextColor}>
+              {title}
+            </Title>
+            <Label hierarchy='stronger' weight='normal' textColor={labelTextColor}>
               {label}
             </Label>
           </div>
           {isDescriptionVisible && (
-            <div className='body-lg text-object-neutral-dark min-h-[3rem] flex-[1_0_0]'>
+            <div className={`body-lg min-h-[3rem] flex-[1_0_0] ${descriptionTextColor}`}>
               {children}
             </div>
           )}
