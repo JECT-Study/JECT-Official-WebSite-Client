19659e3cfdc27e798ab9fba7614cda3d9f11f0cf
rkdcodus <rcd7275@naver.com>
2025-02-20 22:48:44 +0900

    fix: InputFile 스토리북 수정

--- src/components/common/input/InputFile.stories.tsx
+++ src/components/common/input/InputFile.stories.tsx
@@ -1,5 +1,5 @@
-import { action } from '@storybook/addon-actions';
 import { Meta, StoryObj } from '@storybook/react';
+import { useState } from 'react';
 
 import InputFile from './InputFile';
 
@@ -9,18 +9,28 @@ const meta: Meta<typeof InputFile> = {
   parameters: {
     docs: {
       description: {
-        component: `InputFile 컴포넌트는 File 컴포넌트와 Uploader 컴포넌트를 조합한 컴포넌트 입니다. <br/> 파일의 추가, 삭제 기능이 가능합니다. 
-          직접 파일을 추가해 보세요!`,
+        component: `InputFile 컴포넌트는 File 컴포넌트와 Uploader 컴포넌트를 조합한 컴포넌트 입니다. <br/> 파일의 추가, 삭제 기능이 가능합니다. (아직 파일 용량 제한 기능은 없습니다) <br/> 직접 파일을 추가해 보세요!`,
       },
     },
   },
   argTypes: {
-    setAnswers: {
-      description: '지원자 답변 상태를 업데이트하는 setter 함수입니다.',
+    fileList: {
+      description: `사용자가 업로드하는 파일의 배열입니다.<br/> 한 번의 파일 선택기를 통해 선택된 FileList 타입이 아닌 여러번 파일 선택기를 열어서 쌓인 파일들입니다.`,
+    },
+    deleteFile: {
+      description: '사용자가 File 컴포넌트의 X 버튼 클릭 시 호출되는 함수입니다. ',
+    },
+    addFile: {
+      description:
+        '사용자가 드래그 혹은 버튼을 통해 선택된 파일을 업로드할 때마다 호출되는 함수입니다.',
+    },
+    isDisabled: {
+      control: 'boolean',
+      description: '파일 업로드 가능/불가능을 결정하는 prop 입니다.',
     },
     fileExtensions: {
       control: 'object',
-      description: '업로드 가능한 파일 확장자를 담은 배열입니다. ',
+      description: '업로드 가능한 파일 확장자를 담은 배열입니다.',
     },
   },
 };
@@ -29,30 +39,55 @@ export default meta;
 
 type Story = StoryObj<typeof InputFile>;
 
-export const Default: Story = {
-  args: {
-    setAnswers: () => {},
-    fileExtensions: ['.pdf'],
-  },
-};
-
 export const InputFilePdfStory: Story = {
   name: 'Only PDF File Story',
   render: () => {
+    const [fileList, setFileList] = useState<File[]>([]);
+
+    const addFile = (file: FileList | null) => {
+      if (file) setFileList(prev => [...prev, ...Array.from(file)]);
+    };
+
+    const deleteFile = (lastModified: number) => {
+      setFileList(fileList.filter(file => file.lastModified !== lastModified));
+    };
+
     return (
       <div className='gap-2xl flex flex-col'>
-        <InputFile setAnswers={action('setAnswers')} fileExtensions={['.pdf']} />
+        <InputFile
+          fileList={fileList}
+          addFile={addFile}
+          deleteFile={deleteFile}
+          isDisabled={false}
+          fileExtensions={['pdf']}
+        />
       </div>
     );
   },
 };
 
-export const InputFilePdfPngStory: Story = {
-  name: 'Pdf/Png File Story',
+export const InputFileOtherFileExtensionStory: Story = {
+  name: 'Other extension File Story',
   render: () => {
+    const [fileList, setFileList] = useState<File[]>([]);
+
+    const addFile = (file: FileList | null) => {
+      if (file) setFileList(prev => [...prev, ...Array.from(file)]);
+    };
+
+    const deleteFile = (lastModified: number) => {
+      setFileList(fileList.filter(file => file.lastModified !== lastModified));
+    };
+
     return (
       <div className='gap-2xl flex flex-col'>
-        <InputFile setAnswers={action('setAnswers')} fileExtensions={['.pdf', '.png']} />
+        <InputFile
+          fileList={fileList}
+          addFile={addFile}
+          deleteFile={deleteFile}
+          isDisabled={false}
+          fileExtensions={['pdf', 'jpeg', 'png']}
+        />
       </div>
     );
   },
