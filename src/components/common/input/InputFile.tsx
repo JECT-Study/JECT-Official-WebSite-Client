e5690486fafb2e0dc6c4a0d1c4d8ac2a27607ea2
rkdcodus <rcd7275@naver.com>
2025-02-20 21:46:03 +0900

    fix: InputFile 컴포넌트 응집도 높히기 (setter 대신 콜백함수 이용)

--- src/components/common/input/InputFile.tsx
+++ src/components/common/input/InputFile.tsx
@@ -1,28 +1,17 @@
-import { Dispatch, useState } from 'react';
-
 import File from '../file/File';
 import Uploader from '../file/Uploader';
 
 import { FileExtension } from '@/constants/file';
 
 interface InputFileProps {
-  setAnswers: Dispatch<React.SetStateAction<string | null>>;
+  fileList: File[];
+  deleteFile: (lastModified: number) => void;
+  addFile: (file: FileList | null) => void;
   fileExtensions: FileExtension[];
+  isDisabled: boolean;
 }
 
-// TODO: file 전체 용량 10MB로 제한 + Uploader isDisabled 설정
-
-function InputFile({ setAnswers, fileExtensions }: InputFileProps) {
-  const [fileList, setFileList] = useState<File[]>([]);
-
-  const addFile = (file: FileList | null) => {
-    if (file) setFileList(prev => [...prev, ...Array.from(file)]);
-  };
-
-  const deleteFile = (lastModified: number) => {
-    setFileList(fileList.filter(file => file.lastModified !== lastModified));
-  };
-
+function InputFile({ fileList, deleteFile, addFile, fileExtensions, isDisabled }: InputFileProps) {
   return (
     <>
       <div className='gap-2xs flex flex-col'>
@@ -30,7 +19,7 @@ function InputFile({ setAnswers, fileExtensions }: InputFileProps) {
           <File key={file.lastModified} file={file} onClick={deleteFile} />
         ))}
       </div>
-      <Uploader fileExtensions={fileExtensions} isDisabled={false} onChangeFile={addFile} />
+      <Uploader fileExtensions={fileExtensions} isDisabled={isDisabled} onChangeFile={addFile} />
     </>
   );
 }
