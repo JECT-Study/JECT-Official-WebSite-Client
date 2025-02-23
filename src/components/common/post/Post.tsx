6ffe8416eb303a3fe0d93fad3ff7393da10252c6
onejuneee <onejuneee721@gmail.com>
2025-02-24 03:54:59 +0900

    refactor: Post 컴포넌트에 disabled 속성을 추가합니다.

--- src/components/common/post/Post.tsx
+++ src/components/common/post/Post.tsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx';
 import { ReactNode } from 'react';
 
 import Label from '@/components/common/Label';
@@ -8,22 +9,48 @@ interface PostProps {
   label: string;
   date: string;
   children: ReactNode;
+  disabled?: boolean;
 }
-export const Post = ({ title, label, date, children }: PostProps) => {
+export const Post = ({ title, label, date, children, disabled = false }: PostProps) => {
+  const containerClass = clsx(
+    'box-border radius-sm gap-3xs border-x border-border-assistive-dark flex w-full flex-col items-start px-(--gap-lg) py-(--gap-md)',
+    disabled
+      ? 'bg-surface-deep-dark pointer-events-none cursor-not-allowed'
+      : 'bg-surface-embossed-dark',
+  );
+
   return (
-    <div className='radius-sm gap-3xs border-border-assistive-dark bg-surface-embossed-dark flex w-[53.75rem] flex-col items-start px-(--gap-lg) py-(--gap-md)'>
+    <div className={containerClass}>
       <div className='gap-md flex items-center self-stretch'>
         <div className='gap-xs flex flex-[1_0_0] items-center self-stretch'>
-          <Title hierarchy='weak'>{title}</Title>
-          <Label hierarchy='strong' weight='bold' textColor='text-object-neutral-dark'>
+          <Title
+            hierarchy='weak'
+            textColor={disabled ? 'text-object-disabled-dark' : 'text-object-hero-dark'}
+          >
+            {title}
+          </Title>
+          <Label
+            hierarchy='strong'
+            weight='bold'
+            textColor={disabled ? 'text-object-disabled-dark' : 'text-object-neutral-dark'}
+          >
             {label}
           </Label>
         </div>
-        <Label hierarchy='normal' weight='normal' textColor='text-object-alternative-dark'>
+        <Label
+          hierarchy='normal'
+          weight='normal'
+          textColor={disabled ? 'text-object-disabled-dark' : 'text-object-alternative-dark'}
+        >
           {date}
         </Label>
       </div>
-      <div className='body-lg text-object-neutral-dark min-h-[3.375rem] self-stretch'>
+      <div
+        className={clsx(
+          'body-lg flex h-[3.375rem] self-stretch text-left',
+          disabled ? 'text-object-disabled-dark' : 'text-object-neutral-dark',
+        )}
+      >
         {children}
       </div>
     </div>
