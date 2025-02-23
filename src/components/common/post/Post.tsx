22c0f05b0d24adb2028c5104b659850b134113fe
onejuneee <onejuneee721@gmail.com>
2025-02-24 03:59:13 +0900

    feat: Post 컴포넌트에 Interaction을 주입합니다.

--- src/components/common/post/Post.tsx
+++ src/components/common/post/Post.tsx
@@ -1,6 +1,7 @@
 import clsx from 'clsx';
 import { ReactNode } from 'react';
 
+import Interaction from '@/components/common/interaction/Interaction';
 import Label from '@/components/common/Label';
 import Title from '@/components/common/Title';
 
@@ -13,46 +14,53 @@ interface PostProps {
 }
 export const Post = ({ title, label, date, children, disabled = false }: PostProps) => {
   const containerClass = clsx(
-    'box-border radius-sm gap-3xs border-x border-border-assistive-dark flex w-full flex-col items-start px-(--gap-lg) py-(--gap-md)',
+    'peer box-border radius-sm gap-3xs border-x border-border-assistive-dark flex w-full flex-col items-start px-(--gap-lg) py-(--gap-md)',
     disabled
       ? 'bg-surface-deep-dark pointer-events-none cursor-not-allowed'
       : 'bg-surface-embossed-dark',
   );
 
   return (
-    <div className={containerClass}>
-      <div className='gap-md flex items-center self-stretch'>
-        <div className='gap-xs flex flex-[1_0_0] items-center self-stretch'>
-          <Title
-            hierarchy='weak'
-            textColor={disabled ? 'text-object-disabled-dark' : 'text-object-hero-dark'}
-          >
-            {title}
-          </Title>
+    <Interaction
+      variant='default'
+      density='subtle'
+      isInversed={false}
+      className='peer-hover:duration-normal peer-focus:duration-normal peer-hover:ease-(--motion-fluent) peer-focus:ease-(--motion-fluent)'
+    >
+      <div className={containerClass}>
+        <div className='gap-md flex items-center self-stretch'>
+          <div className='gap-xs flex flex-[1_0_0] items-center self-stretch'>
+            <Title
+              hierarchy='weak'
+              textColor={disabled ? 'text-object-disabled-dark' : 'text-object-hero-dark'}
+            >
+              {title}
+            </Title>
+            <Label
+              hierarchy='strong'
+              weight='bold'
+              textColor={disabled ? 'text-object-disabled-dark' : 'text-object-neutral-dark'}
+            >
+              {label}
+            </Label>
+          </div>
           <Label
-            hierarchy='strong'
-            weight='bold'
-            textColor={disabled ? 'text-object-disabled-dark' : 'text-object-neutral-dark'}
+            hierarchy='normal'
+            weight='normal'
+            textColor={disabled ? 'text-object-disabled-dark' : 'text-object-alternative-dark'}
           >
-            {label}
+            {date}
           </Label>
         </div>
-        <Label
-          hierarchy='normal'
-          weight='normal'
-          textColor={disabled ? 'text-object-disabled-dark' : 'text-object-alternative-dark'}
+        <div
+          className={clsx(
+            'body-lg flex h-[3.375rem] self-stretch text-left',
+            disabled ? 'text-object-disabled-dark' : 'text-object-neutral-dark',
+          )}
         >
-          {date}
-        </Label>
+          {children}
+        </div>
       </div>
-      <div
-        className={clsx(
-          'body-lg flex h-[3.375rem] self-stretch text-left',
-          disabled ? 'text-object-disabled-dark' : 'text-object-neutral-dark',
-        )}
-      >
-        {children}
-      </div>
-    </div>
+    </Interaction>
   );
 };
