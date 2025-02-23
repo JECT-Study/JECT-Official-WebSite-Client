c2c415c6d4e6d230cc8821163abfb8a15f274b68
onejuneee <onejuneee721@gmail.com>
2025-02-24 03:24:45 +0900

    refactor: Card 컴포넌트의 사진이 출력되는 구조를 변경합니다.

--- src/components/common/card/Card.tsx
+++ src/components/common/card/Card.tsx
@@ -1,5 +1,5 @@
 import clsx from 'clsx';
-import { ReactNode } from 'react';
+import { ComponentPropsWithoutRef, ReactNode } from 'react';
 
 import Interaction from '@/components/common/interaction/Interaction';
 import Label from '@/components/common/Label';
@@ -13,14 +13,12 @@ interface CardProps extends ComponentPropsWithoutRef<'button'> {
   isDescriptionVisible?: boolean;
 }
 
-const IMG_CLASS_DEFAULT =
-  'border-border-assistive-dark h-[12.0625rem] w-full border-b object-cover';
-const IMG_CLASS_EXPANDED = 'border-border-assistive-dark w-full flex-1 border-b object-cover';
-
-const CONTAINER_CLASS_VISIBLE =
-  'gap-3xs flex flex-col items-start flex-1  px-(--gap-md) py-(--gap-xs)';
-const CONTAINER_CLASS_HIDDEN =
-  'gap-3xs flex flex-col items-start flex-1  px-(--gap-md) pt-(--gap-xs) pb-(--gap-lg)';
+const BUTTON_BASE_CLASS =
+  'peer radius-md stroke-normal border-border-assistive-dark box-border border-x flex w-full h-[21.25rem] flex-col overflow-hidden';
+const CONTENT_CONTAINER_CLASS =
+  'gap-3xs flex w-full flex-col px-(--gap-md) pt-(--gap-xs) pb-(--gap-lg)';
+const TITLE_LABEL_CONTAINER_CLASS = 'gap-4xs flex flex-col items-start';
+const IMAGE_CONTAINER_BASE_CLASS = 'flex w-full overflow-hidden';
 
 export const Card = ({
   title,
@@ -31,18 +29,15 @@ export const Card = ({
   disabled,
   ...restProps
 }: CardProps) => {
-  const imgClass = isDescriptionVisible ? IMG_CLASS_DEFAULT : IMG_CLASS_EXPANDED;
-  const containerClass = isDescriptionVisible ? CONTAINER_CLASS_VISIBLE : CONTAINER_CLASS_HIDDEN;
-
   const bgClass = disabled ? 'bg-surface-deep-dark' : 'bg-surface-embossed-dark';
-  const buttonClass = clsx(
-    'peer radius-md stroke-normal border-border-assistive-dark box-border flex h-[21.25rem] w-[17.5rem] flex-col items-stretch overflow-hidden border',
-    bgClass,
-  );
+  const buttonClass = clsx(BUTTON_BASE_CLASS, bgClass);
 
   const titleTextColor = disabled ? 'text-object-disabled-dark' : 'text-object-hero-dark';
   const labelTextColor = disabled ? 'text-object-disabled-dark' : 'text-object-normal-dark';
   const descriptionTextColor = disabled ? 'text-object-disabled-dark' : 'text-object-neutral-dark';
+
+  const imageHeightClass = isDescriptionVisible ? 'h-[11.6875rem]' : 'h-[15.5625rem]';
+  const imageContainerClass = clsx(IMAGE_CONTAINER_BASE_CLASS, imageHeightClass);
 
   return (
     <Interaction
@@ -52,9 +47,11 @@ export const Card = ({
       className='peer-hover:duration-slower peer-focus:duration-slower peer-hover:ease-(--motion-fluent) peer-focus:ease-(--motion-fluent)'
     >
       <button type='button' disabled={!!disabled} className={buttonClass} {...restProps}>
-        <img src={imgUrl} alt='카드 이미지' className={imgClass} />
-        <div className={containerClass}>
-          <div className='gap-4xs flex flex-col items-start'>
+        <div className={imageContainerClass}>
+          <img src={imgUrl} alt='카드 이미지' className='block h-full w-full object-cover' />
+        </div>
+        <div className={CONTENT_CONTAINER_CLASS}>
+          <div className={TITLE_LABEL_CONTAINER_CLASS}>
             <Title hierarchy='weak' textColor={titleTextColor}>
               {title}
             </Title>
@@ -63,7 +60,7 @@ export const Card = ({
             </Label>
           </div>
           {isDescriptionVisible && (
-            <div className={`body-lg min-h-[3rem] flex-[1_0_0] ${descriptionTextColor}`}>
+            <div className={`body-lg flex h-[3.375rem] ${descriptionTextColor} text-left`}>
               {children}
             </div>
           )}
