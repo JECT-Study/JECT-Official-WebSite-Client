1fdb6b1644037a22c1396ee873c1b64f3912ac4b
rkdcodus <rcd7275@naver.com>
2025-02-25 01:52:29 +0900

    refactor: NavigationItem button props 제거 및 disabled props 추가

--- src/components/common/navigation/NavigationItem.tsx
+++ src/components/common/navigation/NavigationItem.tsx
@@ -1,43 +1,42 @@
-import { ComponentPropsWithoutRef } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 
 import Interaction from '@/components/common/interaction/Interaction';
 import { PathName } from '@/types/pathname';
 
-interface NavigationItemProps extends ComponentPropsWithoutRef<'button'> {
+interface NavigationItemProps {
   children: string;
   pathName: PathName;
+  disabled?: boolean;
 }
 
-function NavigationItem({ children, pathName, disabled, ...props }: NavigationItemProps) {
+function NavigationItem({ children, pathName, disabled = false }: NavigationItemProps) {
   const location = useLocation();
   const navigate = useNavigate();
 
+  if (disabled) {
+    return (
+      <button
+        disabled
+        className={`text-object-disabled-dark radius-2xs label-bold-lg px-(--gap-xs) py-(--gap-4xs)`}
+      >
+        {children}
+      </button>
+    );
+  }
+
   return (
-    <>
-      {disabled ? (
-        <button
-          disabled
-          className={`text-object-disabled-dark radius-2xs label-bold-lg px-(--gap-xs) py-(--gap-4xs)`}
-        >
-          {children}
-        </button>
-      ) : (
-        <Interaction
-          variant={pathName === location.pathname ? 'brand' : 'default'}
-          density='subtle'
-          isInversed={false}
-        >
-          <button
-            {...props}
-            onClick={() => void navigate(pathName)}
-            className={`${pathName === location.pathname ? 'text-accent-hero-dark' : 'text-object-hero-dark'} peer radius-2xs label-bold-lg cursor-pointer px-(--gap-xs) py-(--gap-4xs)`}
-          >
-            {children}
-          </button>
-        </Interaction>
-      )}
-    </>
+    <Interaction
+      variant={pathName === location.pathname ? 'brand' : 'default'}
+      density='subtle'
+      isInversed={false}
+    >
+      <button
+        onClick={() => void navigate(pathName)}
+        className={`${pathName === location.pathname ? 'text-accent-hero-dark' : 'text-object-hero-dark'} peer radius-2xs label-bold-lg cursor-pointer px-(--gap-xs) py-(--gap-4xs)`}
+      >
+        {children}
+      </button>
+    </Interaction>
   );
 }
 
