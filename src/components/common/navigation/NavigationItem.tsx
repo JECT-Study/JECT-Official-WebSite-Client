136d4fea38f7ae34471458b7cad2d66c4cc870f3
rkdcodus <rcd7275@naver.com>
2025-02-24 23:06:27 +0900

    feat: NavigationItem path에 따라 UI 변경

--- src/components/common/navigation/NavigationItem.tsx
+++ src/components/common/navigation/NavigationItem.tsx
@@ -1,27 +1,37 @@
 import { ComponentPropsWithoutRef } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 
-import Interaction from '../interaction/Interaction';
+import Interaction from '@/components/common/interaction/Interaction';
+import { PathName } from '@/types/pathname';
 
 interface NavigationItemProps extends ComponentPropsWithoutRef<'button'> {
   children: string;
-  isSelected: boolean;
+  pathName: PathName;
 }
 
-function NavigationItem({ children, isSelected, disabled, ...props }: NavigationItemProps) {
+function NavigationItem({ children, pathName, disabled, ...props }: NavigationItemProps) {
+  const location = useLocation();
+  const navigate = useNavigate();
+
   return (
     <>
       {disabled ? (
         <button
           disabled
-          className={`text-object-disabled-dark radius-2xs label-bold-lg cursor-pointer px-(--gap-xs) py-(--gap-4xs)`}
+          className={`text-object-disabled-dark radius-2xs label-bold-lg px-(--gap-xs) py-(--gap-4xs)`}
         >
           {children}
         </button>
       ) : (
-        <Interaction variant={isSelected ? 'brand' : 'default'} density='subtle' isInversed={false}>
+        <Interaction
+          variant={pathName === location.pathname ? 'brand' : 'default'}
+          density='subtle'
+          isInversed={false}
+        >
           <button
             {...props}
-            className={`${isSelected ? 'text-accent-hero-dark' : 'text-object-hero-dark'} peer radius-2xs label-bold-lg cursor-pointer px-(--gap-xs) py-(--gap-4xs)`}
+            onClick={() => void navigate(pathName)}
+            className={`${pathName === location.pathname ? 'text-accent-hero-dark' : 'text-object-hero-dark'} peer radius-2xs label-bold-lg cursor-pointer px-(--gap-xs) py-(--gap-4xs)`}
           >
             {children}
           </button>
