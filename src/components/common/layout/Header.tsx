927d2f69f7d886ed08f15ca7efb49f66900dab66
rkdcodus <rcd7275@naver.com>
2025-02-24 23:06:54 +0900

    feat: Header 구현

--- src/components/common/layout/Header.tsx
+++ src/components/common/layout/Header.tsx
@@ -1,5 +1,15 @@
+import Navigation from '@/components/common/navigation/Navigation';
+import NavigationItem from '@/components/common/navigation/NavigationItem';
+
 function Header() {
-  return <div>헤더</div>;
+  return (
+    <Navigation>
+      <NavigationItem pathName='/project'>프로젝트</NavigationItem>
+      <NavigationItem pathName='/activity'>활동</NavigationItem>
+      <NavigationItem pathName='/apply'>지원하기</NavigationItem>
+      <NavigationItem pathName='/faq'>자주 묻는 질문</NavigationItem>
+    </Navigation>
+  );
 }
 
 export default Header;
