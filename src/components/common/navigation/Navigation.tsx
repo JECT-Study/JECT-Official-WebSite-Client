ed993a21ae195f7abc0b3813fd4a4d3170becbf7
rkdcodus <rcd7275@naver.com>
2025-02-25 00:04:18 +0900

    fix: Navigation 컴포넌트 시멘틱 태그 활용

--- src/components/common/navigation/Navigation.tsx
+++ src/components/common/navigation/Navigation.tsx
@@ -8,12 +8,12 @@ interface NavigationProps {
 
 function Navigation({ children }: NavigationProps) {
   return (
-    <nav className='bg-surface-standard-dark gap-6xl flex h-[3.68rem] items-center pl-(--gap-5xl)'>
+    <header className='bg-surface-standard-dark gap-6xl flex h-[3.68rem] items-center pl-(--gap-5xl)'>
       <button className='focus-visible:shadow-focus-visible cursor-pointer'>
         <Logo height={24} fillColor='fill-object-hero-dark' />
       </button>
-      <div className='gap-md flex whitespace-nowrap'>{children}</div>
-    </nav>
+      <nav className='gap-md flex whitespace-nowrap'>{children}</nav>
+    </header>
   );
 }
 
