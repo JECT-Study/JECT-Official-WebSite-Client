683acb28fa10ab82c484528f5ab661682b60110a
rkdcodus <rcd7275@naver.com>
2025-02-25 14:48:09 +0900

    feat: Footer의 젝트 소개 문구 및 이메일 상수화

--- src/components/common/layout/Footer.tsx
+++ src/components/common/layout/Footer.tsx
@@ -3,16 +3,15 @@ import Logo from '../logo/Logo';
 
 import Youtube from '@/assets/svg/footerYoutube.svg?react';
 import NewTabLink from '@/components/apply/NewTabLink';
+import { JECT_EMAIL, JECT_FOOTER_INFO } from '@/constants/ject';
 
 function Footer() {
   return (
     <footer className='bg-surface-standard-dark text-object-alternative-dark body-2xs gap-xl flex h-[4.875rem] items-center px-(--gap-5xl)'>
       <Logo height={20} fillColor='fill-object-normal-dark' />
       <div className='border-border-trans-alternative-dark h-[2rem] border-r'></div>
-      <p className='grow'>
-        젝트는 IT 사이드 프로젝트 동아리입니다. 몰입하며 진행하는 즐거운 프로젝트를 지향하고 있어요.
-      </p>
-      <p>jectofficial@gmail.com</p>
+      <p className='grow'>{JECT_FOOTER_INFO}</p>
+      <p>{JECT_EMAIL}</p>
       <div className='gap-xl flex'>
         <NewTabLink href='https://github.com/JECT-Study'>
           <Icon name='github' size='2xl' fillColor='fill-object-alternative-dark' />
