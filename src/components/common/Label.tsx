f99e2c55dd57c27011b0c3cbda3a48847a5f835a
onejuneee <onejuneee721@gmail.com>
2025-02-26 04:43:47 +0900

    refactor: Label와 Title 컴포넌트에 스타일을 오버라이딩 할 수 있는 요소를 추가합니다.

--- src/components/common/Label.tsx
+++ src/components/common/Label.tsx
@@ -8,14 +8,17 @@ interface LabelProps {
   weight: Weight;
   textColor: string;
   isRequired?: boolean;
+  className?: string;
 }
 
-function Label({ children, weight, hierarchy, textColor, isRequired }: LabelProps) {
+function Label({ children, weight, hierarchy, textColor, isRequired, className }: LabelProps) {
   const typo = labelStyle.weight[weight].hierarchy[hierarchy].typo;
 
   return (
-    <div className={`${typo} gap-5xs flex `}>
-      <span className={`${textColor} whitespace-nowrap cursor-default`}>{children}</span>
+    <div className={`${typo} ${className ?? ''} gap-5xs flex`}>
+      <span className={`${textColor} ${className ?? ''} cursor-default whitespace-nowrap`}>
+        {children}
+      </span>
       {isRequired && <span className='text-feedback-notification-dark'>*</span>}
     </div>
   );
--- src/components/common/Title.tsx
+++ src/components/common/Title.tsx
@@ -7,12 +7,17 @@ interface TitleProps {
   children: ReactNode;
   hierarchy: Hierarchy;
   textColor?: string;
+  className?: string;
 }
 
-function Title({ children, hierarchy, textColor }: TitleProps) {
+function Title({ children, hierarchy, textColor, className }: TitleProps) {
   const typo = titleStyle.hierarchy[hierarchy].typo;
 
-  return <div className={`${typo} ${textColor ?? 'text-object-hero-dark'}`}>{children}</div>;
+  return (
+    <div className={`${typo} ${textColor ?? 'text-object-hero-dark'} ${className ?? ''}`}>
+      {children}
+    </div>
+  );
 }
 
 export default Title;
