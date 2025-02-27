02ae839572cd6adba316a2e171243966fd7cd561
onejuneee <onejuneee721@gmail.com>
2025-02-28 08:11:01 +0900

    refactor: 덮어씌워지는 block 요소를 제거 후 className으로만 스타일을 재설정합니다.

--- src/components/common/card/Card.tsx
+++ src/components/common/card/Card.tsx
@@ -67,17 +67,9 @@ export const Card = ({
           {isDescriptionVisible && (
             <span
               className={clsx(
-                'body-lg block h-[3.375rem] w-full text-left whitespace-normal',
+                'body-lg line-clamp-2 w-full text-left break-words whitespace-normal',
                 disabled ? 'text-object-disabled-dark' : 'text-object-neutral-dark',
               )}
-              style={{
-                display: '-webkit-box',
-                WebkitLineClamp: 2,
-                WebkitBoxOrient: 'vertical',
-                overflow: 'hidden',
-                wordBreak: 'break-word',
-                overflowWrap: 'break-word',
-              }}
             >
               {children}
             </span>
