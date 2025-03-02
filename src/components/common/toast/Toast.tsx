fb9d35ce43a3aab32884f781e1ff9a3edda7b1c4
rkdcodus <rcd7275@naver.com>
2025-03-03 02:24:19 +0900

    feat: Toast 컴포넌트 구현 및 toast 애니메이션 정의

---
+++ src/components/common/toast/Toast.tsx
@@ -0,0 +1,67 @@
+import { useEffect, useState } from 'react';
+
+import Label from '../label/Label';
+
+import { ToastType, useToastActions, useToastItem } from '@/stores/toastStore';
+
+type ToastStyleType = Record<
+  ToastType,
+  {
+    bg: string;
+    textColor: string;
+  }
+>;
+
+const toastStyle: ToastStyleType = {
+  normal: {
+    bg: 'bg-object-hero-dark',
+    textColor: 'text-object-inverse-hero-dark',
+  },
+  negative: {
+    bg: 'bg-feedback-negative-dark',
+    textColor: 'text-object-static-inverse-hero-dark',
+  },
+  positive: {
+    bg: 'bg-feedback-positive-dark',
+    textColor: 'text-object-static-inverse-hero-dark',
+  },
+};
+
+function Toast() {
+  const [isFadingOut, setIsFadingOut] = useState<boolean>(false);
+  const toastItem = useToastItem();
+  const { removeToast } = useToastActions();
+
+  const handleAnimationEnd = () => {
+    if (isFadingOut) {
+      setIsFadingOut(false);
+      removeToast();
+    }
+  };
+
+  useEffect(() => {
+    if (toastItem) {
+      const timer = setTimeout(() => {
+        setIsFadingOut(true);
+      }, 2800);
+
+      return () => clearTimeout(timer);
+    }
+  }, [toastItem]);
+
+  if (!toastItem) return null;
+
+  return (
+    <div
+      key={toastItem.id}
+      className={`${toastStyle[toastItem.type].bg} ${isFadingOut ? 'animate-toast-fade-out' : 'animate-toast-fade-in'} shadow-overlay radius-xs fixed left-1/2 -translate-x-1/2 px-(--gap-lg) py-(--gap-3xs) *:inline`}
+      onAnimationEnd={handleAnimationEnd}
+    >
+      <Label hierarchy='strong' weight='normal' textColor={toastStyle[toastItem.type].textColor}>
+        {toastItem.message}
+      </Label>
+    </div>
+  );
+}
+
+export default Toast;
