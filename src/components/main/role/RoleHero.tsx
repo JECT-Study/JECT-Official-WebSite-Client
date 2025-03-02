ed5cbec5db8a6cdefa74c3e80a064d732c81f81c
Banana_milk <101927445+WonJuneKim@users.noreply.github.com>
2025-03-03 06:38:05 +0900

    feat: Role 컴포넌트 구현 (#63)
    * feat: 이미지 asset을 png 형태로 추가합니다.
    
    * feat: Badge 컴포넌트를 구현합니다.
    
    * feat: Badge 컴포넌트의 storybook을 추가합니다.
    
    * feat: Role 컴포넌트 중 Hero를 구현합니다.
    
    * feat: Hero 컴포넌트의 스토리북을 추가합니다.
    
    * fix: 스토리북의 title을 중복되지 않게 수정합니다.
    
    * fix: props의 네이밍을 직관적으로 수정합니다.
    
    * refactor: 단순한 스타일을 템플릿 리터럴 형식으로 수정합니다.
    
    * rename: 공통된 이름을 가진 Badge를 RoleBadge로 네이밍을 수정합니다.
    
    * rename: 공통된 이름을 가진 Hero를 RoleHero로 네이밍을 수정합니다.
    
    * refactor: img의 alt 네이밍을 직관적으로 수정합니다.
    
    * refactor: 최상위 div의 스타일을 템플릿 리터럴 형태로 변경합니다.
    
    * move: 불필요한 import 문을 제거합니다.
    
    * move: 불필요한 import 문을 제거합니다.
    
    * refactor: 컴포넌트 배경에 반영된 opacity를 반영합니다.
    
    * refactor: 공통적으로 사용되는 타입을 분리합니다.
    
    * fix: opacity 스타일 적용 방식을 변경합니다.

---
+++ src/components/main/role/RoleHero.tsx
@@ -0,0 +1,74 @@
+import { ReactNode } from 'react';
+
+import atom from '@/assets/images/atom.png';
+import clipboard from '@/assets/images/clipboard.png';
+import cursor from '@/assets/images/cursor.png';
+import mobius from '@/assets/images/mobius.png';
+import stack from '@/assets/images/stack.png';
+import Badge from '@/components/common/badge/Badge';
+import { RoleVariant } from '@/types/role';
+
+const heroMap: Record<RoleVariant, { icon: string; style: string }> = {
+  fe: {
+    icon: atom,
+    style: 'bg-gradient-to-r from-role-fe-normal-light to-role-fe-trans-neutral-dark',
+  },
+  be: {
+    icon: stack,
+    style: 'bg-gradient-to-r from-role-be-normal-dark to-role-be-trans-neutral-dark',
+  },
+  do: {
+    icon: mobius,
+    style: 'bg-gradient-to-r from-role-do-normal-dark to-role-do-trans-neutral-dark',
+  },
+  pm: {
+    icon: clipboard,
+    style: 'bg-gradient-to-r from-role-pm-normal-dark to-role-pm-trans-neutral-dark',
+  },
+  pd: {
+    icon: cursor,
+    style: 'bg-gradient-to-r from-role-pd-normal-dark to-role-pd-trans-neutral-dark',
+  },
+};
+
+interface HeroProps {
+  title: string;
+  labels?: string[];
+  children: ReactNode;
+  variant: RoleVariant;
+}
+
+export const RoleHero = ({ title, labels = [], children, variant }: HeroProps) => {
+  const displayedLabels = labels.slice(0, 5);
+  const { icon: heroIcon, style } = heroMap[variant];
+
+  return (
+    <div className='radius-sm gap-6xl relative flex items-center pt-(--gap-3xl) pr-(--gap-3xl) pb-(--gap-3xl) pl-(--gap-6xl)'>
+      <div className={`${style} radius-sm absolute inset-0 z-0 opacity-35`} />
+      <div className='gap-xl relative z-10 flex min-h-[12.1875rem] shrink-0 grow basis-0 flex-col items-start'>
+        <div className='gap-sm self-strectch flex flex-col items-start'>
+          <span className='text-object-hero-dark title-03'>{title}</span>
+          <div className='gap-2xs flex max-h-[5rem] flex-wrap content-start items-start'>
+            {displayedLabels.map((label, idx) => (
+              <Badge
+                key={idx}
+                backgroundColor='bg-fill-assistive-dark'
+                textColor='text-object-normal-dark'
+              >
+                {label}
+              </Badge>
+            ))}
+          </div>
+        </div>
+        <span className='body-lg text-object-normal-dark'>{children}</span>
+      </div>
+      <img
+        src={heroIcon}
+        alt={`${variant} 아이콘`}
+        className='relative z-10 h-24 w-24 object-contain'
+      />
+    </div>
+  );
+};
+
+export default RoleHero;
---
+++ src/types/role.ts
@@ -0,0 +1,1 @@
+export type RoleVariant = 'fe' | 'be' | 'do' | 'pm' | 'pd';
