44a00825278a894d4c2880161d81573fa33beaf3
onejuneee <onejuneee721@gmail.com>
2025-03-05 04:39:23 +0900

    feat: 메인페이지를 라우트에 추가합니다.

--- src/router.tsx
+++ src/router.tsx
@@ -3,12 +3,13 @@ import { PATH } from './constants/path';
 import Layout from '@/components/layout/Layout';
 import Apply from '@/pages/Apply';
 import Faq from '@/pages/Faq';
+import Main from '@/pages/Main';
 
 const routerList = [
   {
     element: <Layout />,
     children: [
-      { path: PATH.main, element: <></> },
+      { path: PATH.main, element: <Main /> },
       { path: PATH.project, element: <></> },
       { path: PATH.activity, element: <></> },
       { path: PATH.apply, element: <Apply /> },
