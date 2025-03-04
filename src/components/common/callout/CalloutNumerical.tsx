7e9f802eb294774168cbf5a976dc358a867dcc42
onejuneee <onejuneee721@gmail.com>
2025-03-05 04:56:45 +0900

    refactor: CalloutNumerical의 width에 100%를 부여합니다.

--- src/components/common/callout/CalloutNumerical.tsx
+++ src/components/common/callout/CalloutNumerical.tsx
@@ -8,7 +8,7 @@ interface CalloutProps {
 
 function CalloutNumerical({ title, labelText }: CalloutProps) {
   return (
-    <div className='bg-surface-deep-dark border-border-assistive-dark gap-md radius-sm flex flex-col items-center border px-(--gap-4xl) py-(--gap-3xl)'>
+    <div className='bg-surface-deep-dark border-border-assistive-dark gap-md radius-sm flex w-full flex-col items-center border px-(--gap-4xl) py-(--gap-3xl)'>
       <Title hierarchy='stronger'>{title}</Title>
       <Label hierarchy='normal' weight='bold' textColor='text-object-neutral-dark'>
         {labelText}
