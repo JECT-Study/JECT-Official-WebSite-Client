a309231aa2c2d848a0e81839bd705b4458670096
onejuneee <onejuneee721@gmail.com>
2025-03-05 00:41:13 +0900

    feat: AnimatedSection을 메인페이지에 적용합니다.

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,3 +1,5 @@
+import AnimatedSection from '@/components/main/animatedSection/AnimatedSection.tsx';
+
 const sectionClassName =
   'flex h-[60.3125rem] py-(--gap-7xl) px-(--gap-4xl) flex-col justify-center items-center gap0-7xl w-full snap-start';
 
@@ -5,13 +7,7 @@ const Main = () => {
   return (
     <div className='scroll-snap-y scroll-snap-mandatory h-screen overflow-y-scroll'>
       <section className={sectionClassName}>
-        <div className='py-( gap-7xl flex shrink-0 grow basis-0 flex-col items-center justify-center self-stretch'>
-          <div className='gap-2xl flex flex-col items-center justify-center self-stretch'>
-            <div className='gap-md display-04 text-object-hero-dark flex items-center justify-center self-stretch text-center'></div>
-            <div className='gap-md flex items-center justify-center self-stretch'></div>
-            <div className='gap-md display-04 text-object-hero-dark flex items-center justify-center self-stretch text-center'></div>
-          </div>
-        </div>
+        <AnimatedSection />
       </section>
       <section className={sectionClassName}>Section 2 content</section>
       <section className={sectionClassName}>Section 3 content</section>
