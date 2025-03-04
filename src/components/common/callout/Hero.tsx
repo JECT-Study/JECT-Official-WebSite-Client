3c47aec4d0e1969d570e3744d649c15c6595dc77
onejuneee <onejuneee721@gmail.com>
2025-03-05 06:13:22 +0900

    refactor: Hero 컴포넌트의 content를 children 형태로 받도록 수정합니다.

--- src/components/common/callout/Hero.tsx
+++ src/components/common/callout/Hero.tsx
@@ -1,13 +1,15 @@
+import { ReactNode } from 'react';
+
 import Badge from '@/components/common/badge/Badge';
 import Title from '@/components/common/title/Title';
 
 export interface HeroProps {
   title: string;
   badgeText: string;
-  content: string;
+  children: ReactNode;
 }
 
-function Hero({ title, badgeText, content }: HeroProps) {
+function Hero({ title, badgeText, children }: HeroProps) {
   return (
     <div className='gap-xl radius-xs border-border-assistive-dark bg-surface-deep-dark flex flex-col border px-(--gap-3xl) py-(--gap-2xl)'>
       <div className='gap-sm flex'>
@@ -16,7 +18,7 @@ function Hero({ title, badgeText, content }: HeroProps) {
           {badgeText}
         </Badge>
       </div>
-      <p className='text-object-normal-dark body-lg'>{content}</p>
+      <p className='text-object-normal-dark body-lg'>{children}</p>
     </div>
   );
 }
