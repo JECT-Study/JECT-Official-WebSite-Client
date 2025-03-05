ecfb85c6edf2b50e47fda56b8a604ac68f2b0eaa
rkdcodus <rcd7275@naver.com>
2025-03-05 15:26:31 +0900

    fix: useToast -> useToastActions 사용으로 스토리북 수정

--- src/components/common/toast/Toast.stories.tsx
+++ src/components/common/toast/Toast.stories.tsx
@@ -3,7 +3,7 @@ import { Meta } from '@storybook/react';
 import Toast from './Toast';
 import BlockButton from '../button/BlockButton';
 
-import useToast from '@/hooks/useToast';
+import { useToastActions } from '@/stores/toastStore';
 
 const meta: Meta<typeof Toast> = {
   title: 'Components/Toast',
@@ -12,7 +12,7 @@ const meta: Meta<typeof Toast> = {
     docs: {
       description: {
         component:
-          'Toast 컴포넌트는 Layout 컴포넌트에 전역적으로 위치합니다. <br/> useToast 훅을 사용하여 toast 액션을 실행할 수 있습니다. <br/> toast의 타입은 "normal", "negative", "positive" 3종류이며, 기본값은 normal입니다. <br/> toast({message: "", type: ""}) 형태로 사용하며 type은 생략할 경우 기본 타입인 normal 타입으로 설정됩니다.',
+          'Toast 컴포넌트는 Layout 컴포넌트에 전역적으로 위치합니다. <br/> useToastActions를 사용하여 toast 액션을 실행할 수 있습니다. <br/> ToastType은 "normal", "negative", "positive" 3종류이며, 기본값은 normal입니다. <br/> toast(message: string, type: ToastType) 형태로 사용하며 type은 생략할 경우 기본 타입인 normal 타입으로 설정됩니다.',
       },
     },
   },
@@ -21,20 +21,18 @@ const meta: Meta<typeof Toast> = {
 export default meta;
 
 export const All = () => {
-  const { toast } = useToast();
+  const { addToast } = useToastActions();
 
   const normal = () => {
-    toast({
-      message: '토스트 레이블',
-    });
+    addToast('토스트 레이블');
   };
 
   const negative = () => {
-    toast({ message: '토스트 레이블', type: 'negative' });
+    addToast('토스트 레이블', 'negative');
   };
 
   const positive = () => {
-    toast({ message: '토스트 레이블', type: 'positive' });
+    addToast('토스트 레이블', 'positive');
   };
 
   return (
