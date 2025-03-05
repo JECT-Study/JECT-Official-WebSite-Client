0ceafc08236b6e70c907735b007601f87409c821
rkdcodus <rcd7275@naver.com>
2025-03-06 01:30:17 +0900

    feat: apply-3 페이지 UI 구현 (이름, 전화번호 입력 페이지)

---
+++ src/pages/ApplyApplicationInfo.tsx
@@ -0,0 +1,64 @@
+import { useState } from 'react';
+
+import BlockButton from '@/components/common/button/BlockButton';
+import Icon from '@/components/common/icon/Icon';
+import InputField from '@/components/common/input/InputField';
+import ProgressIndicator from '@/components/common/progress/ProgressIndicator';
+import Title from '@/components/common/title/Title';
+import { APPLY_TITLE } from '@/constants/applyPageData';
+
+// TODO: 이름 필드 요구사항 - 5글자 이내, 영어 불가
+// TODO: 전화번호 요구사항 - 숫자 11자리 이내, 010으로 시작, 공백 및 하이픈 허용(자동 제거 후 api 요청)
+// TODO: 이름, 전화번호 필수 작성 후 버튼 활성화
+
+function ApplyApplicantInfo() {
+  const [isReady, setIsReady] = useState(false);
+
+  return (
+    <div className='bg-surface-standard-dark gap-9xl flex min-h-dvh flex-col items-center pb-(--gap-12xl)'>
+      <div className='mt-(--gap-9xl)'>
+        <ProgressIndicator totalStep={3} currentStep={2} />
+      </div>
+      <main className='gap-9xl flex w-[26.25rem] max-w-[26.25rem] flex-col items-stretch'>
+        <div className='self-center'>
+          <Title hierarchy='strong'>{APPLY_TITLE.applicantInfo}</Title>
+        </div>
+        <form action='' className='gap-3xl flex flex-col'>
+          <InputField
+            labelText='이름'
+            isError={false}
+            isSuccess={false}
+            placeholder='김젝트'
+            // helper='이름은 5자 이내로 작성해주세요'
+          />
+          <InputField
+            labelText='휴대폰 번호'
+            isError={false}
+            isSuccess={false}
+            placeholder='01012345678'
+            // helper='“010”을 포함해 총 11자리까지만 입력해주세요.'
+          />
+        </form>
+        <BlockButton
+          disabled={!isReady}
+          size='lg'
+          style='solid'
+          hierarchy='accent'
+          rightIcon={
+            <Icon
+              name='forward'
+              size='md'
+              fillColor={
+                isReady ? 'fill-object-static-inverse-hero-dark' : 'fill-accent-trans-hero-dark'
+              }
+            />
+          }
+        >
+          다음 단계로 진행하기
+        </BlockButton>
+      </main>
+    </div>
+  );
+}
+
+export default ApplyApplicantInfo;
