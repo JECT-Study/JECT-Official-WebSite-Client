a4da93b5f3690d824c7f1c393c79e458564506be
onejuneee <onejuneee721@gmail.com>
2025-03-07 01:15:33 +0900

    feat: 프로젝트 페이지를 라우터에 추가합니다.

--- src/router.tsx
+++ src/router.tsx
@@ -4,13 +4,14 @@ import Layout from '@/components/layout/Layout';
 import Activity from '@/pages/Activity';
 import Apply from '@/pages/Apply';
 import Faq from '@/pages/Faq';
+import Project from '@/pages/Project';
 
 const routerList = [
   {
     element: <Layout />,
     children: [
       { path: PATH.main, element: <></> },
-      { path: PATH.project, element: <></> },
+      { path: PATH.project, element: <Project /> },
       { path: PATH.activity, element: <Activity /> },
       { path: PATH.apply, element: <Apply /> },
       { path: PATH.faq, element: <Faq /> },
