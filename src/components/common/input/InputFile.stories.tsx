5e8cd3c60a2c7a271d228e7ad1f0065356ae1d25
rkdcodus <rcd7275@naver.com>
2025-03-07 21:51:24 +0900

    fix: InputFile에서 File 컴포넌트 제거 File 컴포넌트는 children으로 받도록 수정

--- src/components/common/input/InputFile.stories.tsx
+++ src/components/common/input/InputFile.stories.tsx
@@ -2,6 +2,7 @@ import { Meta, StoryObj } from '@storybook/react';
 import { useState } from 'react';
 
 import InputFile from './InputFile';
+import File from '../file/File';
 
 import { FileUrl } from '@/types/file';
 
@@ -16,27 +17,28 @@ const meta: Meta<typeof InputFile> = {
     },
   },
   argTypes: {
-    fileList: {
-      description: `사용자가 업로드한 파일의 배열입니다. <br/> 이때 배열의 file 요소들은 File 타입의 객체이거나 CDN URL 입니다.`,
-    },
-    onDeleteFile: {
-      description: '사용자가 File 컴포넌트의 X 버튼 클릭 시 호출되는 함수입니다. ',
-    },
-    onAddFile: {
-      description:
-        '사용자가 드래그 혹은 버튼을 통해 선택된 파일을 업로드할 때마다 호출되는 함수입니다.',
-    },
-    isDisabled: {
-      control: 'boolean',
-      description: '파일 업로드 가능/불가능을 결정하는 prop 입니다.',
+    children: {
+      description: `File 컴포넌트를 위한 위치입니다.`,
     },
     fileExtensions: {
       control: 'object',
       description: '업로드 가능한 파일 확장자를 담은 배열입니다.',
     },
+    currentSize: {
+      control: 'number',
+      description: '사용자가 담은 파일의 총 용량값을 나타냅니다.',
+    },
     maxSize: {
       control: 'number',
       description: '총 파일의 최대 용량값을 나타냅니다. 단위는 MB입니다.',
+    },
+    isDisabled: {
+      control: 'boolean',
+      description: '파일 업로드 가능/불가능을 결정하는 prop 입니다.',
+    },
+    onAddFile: {
+      description:
+        '사용자가 드래그 혹은 버튼을 통해 선택된 파일을 업로드할 때마다 호출되는 함수입니다.',
     },
     labelText: {
       control: 'text',
@@ -86,15 +88,22 @@ export const InputFilePdfStory: Story = {
     return (
       <div className='gap-2xl flex flex-col'>
         <InputFile
-          fileList={fileList}
-          onAddFile={addFile}
-          onDeleteFile={deleteFile}
-          isDisabled={false}
           fileExtensions={['pdf']}
-          labelText='첨부파일'
+          currentSize={0}
           maxSize={100}
+          isDisabled={false}
+          onAddFile={addFile}
+          labelText='첨부파일'
           isRequired={true}
-        />
+        >
+          {fileList.length > 0 && (
+            <div className='gap-2xs flex flex-col'>
+              {fileList.map(file => {
+                return <File key={file.id} file={file} onDelete={deleteFile} feedback='error' />;
+              })}
+            </div>
+          )}
+        </InputFile>
       </div>
     );
   },
@@ -133,15 +142,22 @@ export const InputFileOtherFileExtensionStory: Story = {
     return (
       <div className='gap-2xl flex flex-col'>
         <InputFile
-          fileList={fileList}
-          onAddFile={addFile}
-          onDeleteFile={deleteFile}
-          isDisabled={false}
           fileExtensions={['pdf', 'jpeg', 'png']}
-          labelText='첨부파일'
+          currentSize={0}
           maxSize={100}
-          isRequired={false}
-        />
+          isDisabled={false}
+          onAddFile={addFile}
+          labelText='첨부파일'
+          isRequired={true}
+        >
+          {fileList.length > 0 && (
+            <div className='gap-2xs flex flex-col'>
+              {fileList.map(file => {
+                return <File key={file.id} file={file} onDelete={deleteFile} />;
+              })}
+            </div>
+          )}
+        </InputFile>
       </div>
     );
   },
--- src/components/common/input/InputFile.tsx
+++ src/components/common/input/InputFile.tsx
@@ -1,28 +1,28 @@
-import File from '../file/File';
+import { ReactNode } from 'react';
+
 import Label from '../label/Label';
 import Uploader from '../uploader/Uploader';
 
 import { FileExtension } from '@/constants/file';
-import { FileUrl } from '@/types/file';
 
 interface InputFileProps {
-  fileList: FileUrl[];
-  onDeleteFile: (id: number | string) => void;
-  onAddFile: (file: FileList | null) => void;
+  children: ReactNode;
   fileExtensions: FileExtension[];
-  isDisabled: boolean;
+  currentSize: number;
   maxSize: number;
+  isDisabled: boolean;
+  onAddFile: (file: FileList | null) => void;
   labelText?: string;
   isRequired?: boolean;
 }
 
 function InputFile({
-  fileList,
-  onDeleteFile,
-  onAddFile,
+  children,
   fileExtensions,
+  currentSize = 0,
+  maxSize = 0,
   isDisabled,
-  maxSize,
+  onAddFile,
   labelText,
   isRequired = false,
 }: InputFileProps) {
@@ -37,13 +37,7 @@ function InputFile({
         {labelText}
       </Label>
       <div className='bg-surface-standard-dark radius-sm border-border-trans-assistive-dark gap-md flex flex-col border px-(--gap-md) py-(--gap-sm)'>
-        {fileList.length > 0 && (
-          <div className='gap-2xs flex flex-col'>
-            {fileList.map(file => {
-              return <File key={file.id} file={file} onDelete={onDeleteFile} />;
-            })}
-          </div>
-        )}
+        {children}
         <Uploader
           fileExtensions={fileExtensions}
           isDisabled={isDisabled}
@@ -51,7 +45,7 @@ function InputFile({
         />
       </div>
       <div className={`text-object-assistive-dark body-sm cursor-default self-end`}>
-        {`${0}/${maxSize || 0}MB`}
+        {`${currentSize}/${maxSize}MB`}
       </div>
     </div>
   );
