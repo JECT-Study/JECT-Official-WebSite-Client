561528aaeb03824760491fc9422cda4d55ca9742
rkdcodus <rcd7275@naver.com>
2025-03-08 00:49:16 +0900

    refactor: TextArea 컴포넌트 outline none 처리 및 자동 높이 조절을 위한 row=1 지정

--- src/components/common/input/TextArea.tsx
+++ src/components/common/input/TextArea.tsx
@@ -1,19 +1,23 @@
+import clsx from 'clsx';
 import { ComponentPropsWithoutRef, forwardRef } from 'react';
 
-const TextArea = forwardRef<HTMLTextAreaElement, ComponentPropsWithoutRef<"textarea">>(
+const TextArea = forwardRef<HTMLTextAreaElement, ComponentPropsWithoutRef<'textarea'>>(
   ({ disabled, ...props }, ref) => {
     return (
       <textarea
         {...props}
+        rows={1}
         ref={ref}
         disabled={disabled}
-        className={`
-          peer scroll border radius-sm body-md box-border h-[10.375rem] w-full px-(--gap-xl) py-(--gap-lg)
-          bg-surface-embossed-dark border-border-trans-assistive-dark text-object-hero-dark
-          disabled:placeholder:text-object-disabled-dark placeholder:text-object-assistive-dark
-          disabled:hover:border-border-trans-assistive-dark 
-          hover:border-border-trans-neutral-dark focus:border-border-trans-hero-dark 
-          duration-faster ease-(--motion-fluent) resize-none`}
+        className={clsx(
+          'body-md radius-sm box-border min-h-[10.375rem] w-full px-(--gap-xl) py-(--gap-lg)',
+          'peer scroll resize-none outline-none',
+          'duration-faster ease-(--motion-fluent)',
+          'bg-surface-embossed-dark',
+          'border-border-trans-assistive-dark hover:border-border-trans-neutral-dark focus:border-border-trans-hero-dark disabled:hover:border-border-trans-assistive-dark border',
+          'text-object-hero-dark disabled:text-object-disabled-dark',
+          'placeholder:text-object-assistive-dark disabled:placeholder:text-object-disabled-dark',
+        )}
       />
     );
   },
