fd9f066f2ac89bd3eca88ffdaf13742fb20a8ce2
rkdcodus <rcd7275@naver.com>
2025-03-08 18:19:32 +0900

    design: Input 컴포넌트 readonly일 경우엔 cursor default

--- src/components/common/input/Input.tsx
+++ src/components/common/input/Input.tsx
@@ -1,7 +1,7 @@
 import clsx from 'clsx';
 import { ComponentPropsWithoutRef, forwardRef, ReactNode } from 'react';
 
-interface InputProps extends ComponentPropsWithoutRef<"input"> {
+interface InputProps extends ComponentPropsWithoutRef<'input'> {
   isError: boolean;
   children?: ReactNode;
   className?: string;
@@ -11,30 +11,25 @@ const Input = forwardRef<HTMLInputElement, InputProps>(
   ({ children, isError, disabled, className, ...props }, ref) => {
     return (
       <div
-        className={
-          clsx( {
+        className={clsx(
+          {
             'bg-surface-standard-dark': disabled,
             'bg-surface-embossed-dark': !disabled,
             'border-feedback-trans-negative-dark': isError && disabled,
-            'border-feedback-negative-dark' :  isError && !disabled,
+            'border-feedback-negative-dark': isError && !disabled,
             'border-border-trans-assistive-dark': !isError && disabled,
-            'border-border-trans-assistive-dark hover:border-border-trans-neutral-dark focus-within:border-border-trans-hero-dark focus-within:hover:border-border-trans-hero-dark': !isError && !disabled,
+            'border-border-trans-assistive-dark hover:border-border-trans-neutral-dark focus-within:border-border-trans-hero-dark focus-within:hover:border-border-trans-hero-dark':
+              !isError && !disabled,
           },
-        className,
-        'duration-faster gap-2xs radius-xs flex items-center border px-(--gap-md) py-(--gap-sm) ease-(--motion-fluent)'
+          className,
+          'duration-faster gap-2xs radius-xs flex items-center border px-(--gap-md) py-(--gap-sm) ease-(--motion-fluent)',
         )}
       >
         <input
           {...props}
           ref={ref}
           disabled={disabled}
-          className={`
-            body-md grow outline-none
-            disabled:placeholder:text-object-disabled-dark 
-            placeholder:text-object-assistive-dark 
-            caret-object-hero-dark 
-            text-object-hero-dark 
-          `}
+          className={`body-md disabled:placeholder:text-object-disabled-dark placeholder:text-object-assistive-dark caret-object-hero-dark text-object-hero-dark grow outline-none read-only:cursor-default`}
         />
         {children}
       </div>
