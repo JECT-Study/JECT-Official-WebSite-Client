3df0fe5eb33db3e911e9af76fa050d5adb97b3c3
rkdcodus <rcd7275@naver.com>
2025-03-11 18:19:05 +0900

    refactor: ReactQueryDevtools는 개발환경 조건부 렌더링 처리

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
 
   return (
     <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools />
+      {import.meta.env.MODE !== 'production' && <ReactQueryDevtools />}
       <RouterProvider router={router} />
     </QueryClientProvider>
   );
