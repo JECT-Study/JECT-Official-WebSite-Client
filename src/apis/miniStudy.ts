8882a24150ce1e93e4875021c2a8c7505267fd5b
codus <97906653+rkdcodus@users.noreply.github.com>
2025-03-12 22:04:28 +0900

    refactor: requestHandler 제네릭 타입 수정 (#102)
    * refactor: requestHandler 제네릭 타입 수정
    
    * refactor: getMiniStudies에서 ApiResponse 제거

--- src/apis/miniStudy.ts
+++ src/apis/miniStudy.ts
@@ -1,8 +1,7 @@
 import { API_ENDPOINT } from '@/constants/apiEndpoint';
 import { MiniStudies } from '@/types/apis/miniStudy';
-import { ApiResponse } from '@/types/apis/response';
 import { requestHandler } from '@/utils/httpClient';
 
 export const getMiniStudies = async () => {
-  return await requestHandler<ApiResponse<MiniStudies>>('get', API_ENDPOINT.miniStudy);
+  return await requestHandler<MiniStudies>('get', API_ENDPOINT.miniStudy);
 };
