30a87ecd9170d359cd568a063e9ee77efa1ee40f
rkdcodus <rcd7275@naver.com>
2025-03-12 22:09:05 +0900

    refactor: jectalk fetch 함수 타입 수정

--- src/apis/jectalk.ts
+++ src/apis/jectalk.ts
@@ -1,8 +1,7 @@
 import { API_ENDPOINT } from '@/constants/apiEndpoint';
 import { Jectalks } from '@/types/apis/jectalk';
-import { ApiResponse } from '@/types/apis/response';
 import { requestHandler } from '@/utils/httpClient';
 
 export const getJectalks = async () => {
-  return await requestHandler<ApiResponse<Jectalks>>('get', API_ENDPOINT.jectalk);
+  return await requestHandler<Jectalks>('get', API_ENDPOINT.jectalk);
 };
