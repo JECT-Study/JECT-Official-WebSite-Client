26631c5fcc9c1f9e74d9921771d48adcaaa0bdd6
rkdcodus <rcd7275@naver.com>
2025-03-12 15:12:55 +0900

    feat: EmptyData 공통 컴포넌트 생성 및 스토리북 작성

---
+++ src/components/common/emptyState/EmptyData.stories.tsx
@@ -0,0 +1,17 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import EmptyData from './EmptyData';
+
+const meta: Meta<typeof EmptyData> = {
+  title: 'Components/EmptyData',
+  component: EmptyData,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof EmptyData>;
+
+export const EmptyDataStory: Story = {
+  name: 'EmptyData Story',
+  render: () => <EmptyData />,
+};
