eaa146b1e4322b02d044034152c65cd134d94be3
rkdcodus <rcd7275@naver.com>
2025-03-14 13:51:06 +0900

    refactor: Chip 컴포넌트 useSearchParams 부모 컴포넌트에서 제어

--- src/components/admin/chip/Chip.tsx
+++ src/components/admin/chip/Chip.tsx
@@ -1,25 +1,21 @@
-import { ReactNode } from 'react';
+import { ComponentPropsWithoutRef, ReactNode } from 'react';
 import { useSearchParams } from 'react-router-dom';
 
-interface ChipProps {
-  param: string;
+interface ChipProps extends ComponentPropsWithoutRef<'button'> {
+  param?: {
+    key: string;
+    value: string;
+  };
   children: ReactNode;
 }
 
-function Chip({ param, children }: ChipProps) {
-  const [searchParams, setSearchParams] = useSearchParams();
-
-  const handlerClick = () => {
-    const newSearchParams = new URLSearchParams(searchParams);
-
-    newSearchParams.set('position', param);
-    setSearchParams(newSearchParams);
-  };
+function Chip({ param, children, ...props }: ChipProps) {
+  const [searchParams] = useSearchParams();
 
   return (
     <button
-      onClick={handlerClick}
-      className={`${searchParams.get('position') === param ? 'bg-fill-hero-dark label-md text-object-inverse-hero-dark' : 'bg-fill-assistive-dark label-md text-object-neutral-dark'} interaction-default-normal radius-circle px-(--gap-sm) py-(--gap-4xs)`}
+      {...props}
+      className={`${param && searchParams.get(param.key) === param.value ? 'bg-fill-hero-dark label-md text-object-inverse-hero-dark' : 'bg-fill-assistive-dark label-md text-object-neutral-dark'} interaction-default-normal radius-circle px-(--gap-sm) py-(--gap-4xs)`}
     >
       {children}
     </button>
