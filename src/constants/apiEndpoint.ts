73e3099c57bb4b3040882016594237ed945cac81
onejuneee <onejuneee721@gmail.com>
2025-03-26 20:07:12 +0900

    feat: api 연동을 위한 엔드 포인트를 설정합니다.

--- src/constants/apiEndpoint.ts
+++ src/constants/apiEndpoint.ts
@@ -1,4 +1,7 @@
 export const API_ENDPOINT = {
   miniStudy: '/ministudies',
   jectalk: '/jectalks',
+  sendEmailAuthCode: '/email/send/auth',
+  verifyEmailCode: '/auth/code',
+  pinLogin: '/auth/login/pin',
 } as const;
