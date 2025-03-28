1ce91fc503fa77629626d0845f775ed0ebc61728
rkdcodus <rcd7275@naver.com>
2025-03-28 18:55:15 +0900

    remove: 불필요한 주석 제거

--- src/apis/changeJob.ts
+++ src/apis/changeJob.ts
@@ -1,7 +1,6 @@
 import { API_ENDPOINT } from '@/constants/apiEndpoint';
 import { JobFamily } from '@/types/apis/question';
 import { requestHandler } from '@/utils/httpClient';
-// jobFamilyRequest라고 해야할지..? 이미 jobFamily를 정의해준게 있는데
 
 export const putJob = async (job: JobFamily) => {
   return await requestHandler<null, JobFamily>('put', API_ENDPOINT.changeJob, job);
