985f4b9b28249272e4cc633e13d9aad2e7733927
onejuneee <onejuneee721@gmail.com>
2025-03-29 23:26:00 +0900

    feat: PIN을 이용한 로그인 시 로컬 개발 환경에서 토큰을 header에 사용하도록 설정합니다.

--- src/hooks/usePinLoginMutation.ts
+++ src/hooks/usePinLoginMutation.ts
@@ -4,6 +4,7 @@ import { AxiosError } from 'axios';
 import { postPinLogin } from '@/apis/apply';
 import { PinLoginPayload, PinLoginResponseData } from '@/types/apis/apply';
 import { ApiResponse } from '@/types/apis/response';
+import { isLocalStorageEnabled, tokenUtils } from '@/utils/interceptor';
 
 export const usePinLoginMutation = (): UseMutationResult<
   ApiResponse<PinLoginResponseData>,
@@ -18,6 +19,12 @@ export const usePinLoginMutation = (): UseMutationResult<
     },
     onSuccess: data => {
       console.log('usePinLoginMutation 성공:', data);
+
+      if (isLocalStorageEnabled && data.status === 'SUCCESS' && data.data) {
+        const { accessToken, refreshToken } = data.data;
+        tokenUtils.setAccessToken(accessToken);
+        tokenUtils.setRefreshToken(refreshToken);
+      }
     },
     onError: error => {
       console.error('usePinLoginMutation 에러:', error);
