6c22c5be6e67d43641454a8a6e11d87cd96a65cb
onejuneee <onejuneee721@gmail.com>
2025-03-29 10:07:35 +0900

    feat: 순환 참조를 고려하여 requestHandler를 사용하지 않고 토큰 리프레쉬 api를 호출합니다.

---
+++ src/apis/auth.ts
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+import { API_ENDPOINT } from '@/constants/apiEndpoint';
+import { RefreshTokenRequest, RefreshTokenResponse } from '@/types/apis/auth';
+import { ApiResponse } from '@/types/apis/response';
+
+const BASE_URL = import.meta.env.DEV
+  ? import.meta.env.VITE_API_URL_DEV
+  : import.meta.env.VITE_API_URL_PROD;
+
+export const refreshAccessToken = async (
+  data: RefreshTokenRequest,
+): Promise<ApiResponse<RefreshTokenResponse>> => {
+  return await axios
+    .post<ApiResponse<RefreshTokenResponse>>(`${BASE_URL}${API_ENDPOINT.refreshToken}`, data)
+    .then(response => response.data);
+};
