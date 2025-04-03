c1605aa49cfff5f65bcd8437049ef2993fc99257
rkdcodus <rcd7275@naver.com>
2025-04-03 18:13:51 +0900

    refactor: Dialog 컴포넌트 수정 (dialogStore 이용)

--- src/components/common/dialog/Dialog.tsx
+++ src/components/common/dialog/Dialog.tsx
@@ -1,46 +1,51 @@
-import { ReactNode } from 'react';
-import { createPortal } from 'react-dom';
-
 import BlockButton from '@/components/common/button/BlockButton';
+import { useDialogActions, useDialogItem } from '@/stores/dialogStore';
 
-export interface DialogProps {
-  btnLayout: 'vertical' | 'horizontal';
-  title: string;
-  children: ReactNode;
-  primaryBtnLabel: string;
-  secondaryBtnLabel: string;
-  isOpen: boolean;
-  onPrimaryBtnClick: () => void;
-  onSecondaryBtnClick: () => void;
-}
+function Dialog() {
+  const {
+    isOpen,
+    btnLayout,
+    title,
+    content,
+    primaryBtnLabel,
+    secondaryBtnLabel,
+    onPrimaryBtnClick,
+    onSecondaryBtnClick,
+  } = useDialogItem();
+  const { resetDialog, closeDialog } = useDialogActions();
 
-function Dialog({
-  btnLayout,
-  title,
-  children,
-  primaryBtnLabel,
-  secondaryBtnLabel,
-  isOpen,
-  onPrimaryBtnClick,
-  onSecondaryBtnClick,
-}: DialogProps) {
+  const handleClickPrimaryBtn = () => {
+    if (onPrimaryBtnClick) {
+      onPrimaryBtnClick();
+      resetDialog();
+    }
+    closeDialog();
+  };
+
+  const handleClickSecondaryBtn = () => {
+    if (onSecondaryBtnClick) {
+      onSecondaryBtnClick();
+      resetDialog();
+    }
+    closeDialog();
+  };
+
   return (
-    isOpen &&
-    createPortal(
+    isOpen && (
       <div aria-label='dialog' className='bg-surface-dimmed-dark fixed inset-0 z-50 h-dvh w-dvw'>
         <div className='bg-surface-embossed-dark shadow-overlay radius-md gap-5xl absolute top-1/2 left-1/2 flex w-[25rem] -translate-1/2 flex-col p-(--gap-2xl)'>
           <div className='gap-md flex flex-col items-center pt-(--gap-3xs)'>
             <p className='title-01 text-object-hero-dark'>{title}</p>
-            <div className='body-lg text-object-normal-dark text-center'>{children}</div>
+            <div className='body-lg text-object-normal-dark text-center'>{content}</div>
           </div>
           <div
             className={`gap-xs i *: flex *:flex-1 ${btnLayout === 'vertical' ? 'flex-col' : '*:first:order-2 *:last:order-1'}`}
           >
-            <BlockButton onClick={onPrimaryBtnClick} size='md' hierarchy='accent' style='solid'>
+            <BlockButton onClick={handleClickPrimaryBtn} size='md' hierarchy='accent' style='solid'>
               {primaryBtnLabel}
             </BlockButton>
             <BlockButton
-              onClick={onSecondaryBtnClick}
+              onClick={handleClickSecondaryBtn}
               size='md'
               hierarchy='secondary'
               style='solid'
@@ -49,8 +54,7 @@ function Dialog({
             </BlockButton>
           </div>
         </div>
-      </div>,
-      document.body,
+      </div>
     )
   );
 }
