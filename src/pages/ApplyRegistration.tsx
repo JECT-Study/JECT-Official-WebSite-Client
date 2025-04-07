10872ede3988eace9a2850d38db247333c24f792
codus <97906653+rkdcodus@users.noreply.github.com>
2025-04-08 00:09:27 +0900

    feat: 로컬스토리지 자동 임시 저장 기능 구현 (#122)
    * feat: 로컬스토리지를 이용한 자동 임시저장 기능 구현
    
    * fix: 업로드된 파일 열기 실패 버그 해결
    
    * refactor: 함수명 수정
    
    * refactor: 불필요한 변수 제거

--- src/pages/ApplyRegistration.tsx
+++ src/pages/ApplyRegistration.tsx
@@ -18,6 +18,7 @@ import useDraftQuery from '@/hooks/useDraftQuery';
 import useSaveDraftQuery from '@/hooks/useSaveDraftQuery';
 import useSubmitAnswerQuery from '@/hooks/useSubmitAnswerQuery';
 import { JobFamily } from '@/types/apis/question';
+import { getDraftLocal, removeDraftLocal, setDraftLocal } from '@/utils/draftUtils';
 
 interface LocationState {
   continue: boolean;
@@ -52,7 +53,7 @@ function ApplyRegistration() {
     setSubmitButtonActive,
   } = useApplicationState();
 
-  const { draft } = useDraftQuery();
+  const { draft: draftServer } = useDraftQuery();
   const { saveDraftMutate } = useSaveDraftQuery();
   const { changeJobMutate } = useChangeJobQuery();
   const { submitAnswerMutate } = useSubmitAnswerQuery();
@@ -68,10 +69,11 @@ function ApplyRegistration() {
     closeDialog: closeDialogSubmitAnswer,
   } = useDialog();
 
-  const saveDraft = useCallback(() => {
+  const saveDraftServerAndLocal = useCallback(() => {
     if (!selectedJob) return;
 
     saveDraftMutate({ param: selectedJob, answers: answersPayload });
+    setDraftLocal({ jobFamily: selectedJob, ...answersPayload });
     removeLocationState(location);
   }, [saveDraftMutate, answersPayload, selectedJob, location]);
 
@@ -81,6 +83,7 @@ function ApplyRegistration() {
     resetAnswers();
     changeJobMutate(selectedJob);
     closeDialogChangeJob();
+    removeDraftLocal();
   };
 
   const notChangeJob = () => {
@@ -95,24 +98,45 @@ function ApplyRegistration() {
 
     submitAnswerMutate(answer, {
       onSuccess: data => {
-        if (data?.status === 'SUCCESS') void navigate(PATH.applyComplete);
+        if (data?.status === 'SUCCESS') {
+          void navigate(PATH.applyComplete);
+          removeDraftLocal();
+        }
       },
     });
   };
 
+  // 임시 저장 불러오기
   useEffect(() => {
     if (!isLoadDraft(location)) return;
 
-    if (!draft || draft.status !== 'SUCCESS') return;
+    const draftLocal = getDraftLocal();
 
-    updateAnswerByDraft(draft.data);
-  }, [draft, location, updateAnswerByDraft]);
+    if (draftLocal) {
+      return updateAnswerByDraft(draftLocal);
+    }
 
+    if (draftServer && draftServer.status === 'SUCCESS') {
+      return updateAnswerByDraft(draftServer.data);
+    }
+  }, [location, updateAnswerByDraft, draftServer]);
+
+  // 로컬 스토리지 및 서버 임시저장
   useEffect(() => {
-    const autosaveDraft = setInterval(saveDraft, 900000);
+    const autosaveDraft = setInterval(saveDraftServerAndLocal, 900000);
 
     return () => clearInterval(autosaveDraft);
-  }, [saveDraft]);
+  }, [saveDraftServerAndLocal]);
+
+  // 로컬 스토리지 임시 저장
+  useEffect(() => {
+    if (!selectedJob) return;
+
+    const saveDraftLocal = () => setDraftLocal({ jobFamily: selectedJob, ...answersPayload });
+    const autoSaveDraftLocal = setInterval(saveDraftLocal, 60000);
+
+    return () => clearInterval(autoSaveDraftLocal);
+  }, [selectedJob, answersPayload]);
 
   return (
     <div className='gap-9xl flex flex-col items-center pt-(--gap-9xl) pb-(--gap-12xl)'>
@@ -146,7 +170,12 @@ function ApplyRegistration() {
           )}
 
           <div aria-label='button-area' className='gap-md flex w-full self-center *:flex-1'>
-            <BlockButton size='lg' style='solid' hierarchy='secondary' onClick={saveDraft}>
+            <BlockButton
+              size='lg'
+              style='solid'
+              hierarchy='secondary'
+              onClick={saveDraftServerAndLocal}
+            >
               임시 저장하기
             </BlockButton>
             <BlockButton
