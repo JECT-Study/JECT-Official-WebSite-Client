bfbc6b726c06e94ae7d3e222b7e3dcd8d0d1b81a
rkdcodus <rcd7275@naver.com>
2025-04-09 18:37:42 +0900

    style: useCreatePresignedUrlsMutation 함수명 적용

--- src/components/apply/FileField.tsx
+++ src/components/apply/FileField.tsx
@@ -4,7 +4,7 @@ import FileItem from '@/components/common/file/FileItem';
 import InputFile from '@/components/common/input/InputFile';
 import Title from '@/components/common/title/Title';
 import { APPLY_MESSAGE } from '@/constants/applyMessages';
-import useCreatePresignedUrlsQuery from '@/hooks/useCreatePresignedUrlsMutation';
+import useCreatePresignedUrlsMutation from '@/hooks/useCreatePresignedUrlsMutation';
 import { useToastActions } from '@/stores/toastStore';
 import {
   NewPortfolio,
@@ -67,7 +67,7 @@ function FileField({ data, onChange, values }: FileFieldProps) {
   const [portfolios, setPortfolios] = useState<NewPortfolio[]>(formatDraftValues(values) ?? []);
   const [invalidFiles, setInvalidFiles] = useState<File[]>([]);
   const [totalSize, setTotalSize] = useState(0);
-  const { mutate: createPresignedUrlsMutate } = useCreatePresignedUrlsQuery();
+  const { mutate: createPresignedUrlsMutate } = useCreatePresignedUrlsMutation();
   const { addToast } = useToastActions();
 
   const addFile = async (newFiles: FileList | null) => {
