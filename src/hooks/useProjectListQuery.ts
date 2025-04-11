fa869775cdb91b0619fd1db738850bf976a5c101
onejuneee <onejuneee721@gmail.com>
2025-04-12 06:41:40 +0900

    feat: 커스텀 훅을 이용해 프로젝트 목록 호출 api를 무한 데이터로 관리합니다.

---
+++ src/hooks/useProjectListQuery.ts
@@ -0,0 +1,26 @@
+import { useInfiniteQuery } from '@tanstack/react-query';
+
+import { getProjectList } from '@/apis/project';
+import { ProjectCategory } from '@/types/apis/project';
+
+export const useProjectListQuery = (semesterId: number, category: ProjectCategory) => {
+  return useInfiniteQuery({
+    queryKey: ['getProjectList', semesterId, category],
+    queryFn: ({ pageParam = 0 }) => {
+      const size = pageParam === 0 ? 6 : 12;
+      return getProjectList({
+        semesterId,
+        category,
+        page: pageParam,
+        size,
+      });
+    },
+    initialPageParam: 0,
+    getNextPageParam: lastPage => {
+      if (lastPage.data.hasNext) {
+        return lastPage.data.number + 1;
+      }
+      return undefined;
+    },
+  });
+};
