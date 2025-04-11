a2cebc16fafc8a4111fac47df1a7a8d0da4ea3f1
onejuneee <onejuneee721@gmail.com>
2025-04-12 06:41:02 +0900

    feat: 프로젝트 목록 조회 api의 호출부를 작성합니다.

--- src/apis/project.ts
+++ src/apis/project.ts
@@ -1,5 +1,10 @@
 import { API_ENDPOINT } from '@/constants/apiEndpoint';
-import { ProjectReviewsQueryParams, ProjectReviewsResponse } from '@/types/apis/project';
+import {
+  ProjectListQueryParams,
+  ProjectListResponse,
+  ProjectReviewsQueryParams,
+  ProjectReviewsResponse,
+} from '@/types/apis/project';
 import { requestHandler } from '@/utils/httpClient';
 
 export const getProjectReviews = async ({ page, size }: ProjectReviewsQueryParams) => {
@@ -9,4 +14,21 @@ export const getProjectReviews = async ({ page, size }: ProjectReviewsQueryParam
   });
   const url = `${API_ENDPOINT.projectReview}?${params.toString()}`;
   return await requestHandler<ProjectReviewsResponse>('get', url);
+};
+
+export const getProjectList = async ({
+  semesterId,
+  category,
+  page,
+  size,
+}: ProjectListQueryParams) => {
+  const params = new URLSearchParams({
+    semesterId: semesterId.toString(),
+    category: category,
+    page: page.toString(),
+    size: size.toString(),
+  });
+
+  const url = `${API_ENDPOINT.projectList}?${params.toString()}`;
+  return await requestHandler<ProjectListResponse>('get', url);
 };
