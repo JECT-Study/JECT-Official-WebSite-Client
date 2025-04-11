aee44546a5dabd8228e24de43da162021546cad2
onejuneee <onejuneee721@gmail.com>
2025-04-12 06:40:37 +0900

    feat: 프로젝트 목록 api response에 대한 인터페이스를 작성합니다.

--- src/types/apis/project.ts
+++ src/types/apis/project.ts
@@ -21,3 +21,31 @@ export interface ProjectReviewsQueryParams {
   page: number;
   size: number;
 }
+
+export type ProjectCategory = 'MAIN' | 'HACKATHON';
+
+export interface Project {
+  id: number;
+  name: string;
+  summary: string;
+  description: string;
+  thumbnailUrl: string | null;
+}
+
+export interface ProjectListResponse {
+  content: Project[];
+  number: number;
+  size: number;
+  totalElements: number;
+  last: boolean;
+  totalPages: number;
+  numberOfElements: number;
+  hasNext: boolean;
+}
+
+export interface ProjectListQueryParams {
+  semesterId: number;
+  category: ProjectCategory;
+  page: number;
+  size: number;
+}
