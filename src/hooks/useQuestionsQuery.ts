3b6446f57619063683224a07bfae506f9f940c6d
rkdcodus <rcd7275@naver.com>
2025-04-11 13:03:32 +0900

    feat: 지원서 문항 조회 실패 시 UI 및 토스트 피드백 구현

--- src/hooks/useQuestionsQuery.ts
+++ src/hooks/useQuestionsQuery.ts
@@ -4,19 +4,12 @@ import { getQuestions } from '@/apis/question';
 import { JobFamily } from '@/types/apis/question';
 
 const useQuestionsQuery = (param: JobFamily | null) => {
-  const {
-    data: questions,
-    isError,
-    error,
-  } = useQuery({
+  return useQuery({
     queryKey: ['question', param],
     queryFn: () => getQuestions(param),
     enabled: !!param,
+    retry: 0,
   });
-
-  if (isError) console.error(`Query Error ${error}`);
-
-  return { questions };
 };
 
 export default useQuestionsQuery;
