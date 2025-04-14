1efdc7cac24f397081aa303145927147db973b98
rkdcodus <rcd7275@naver.com>
2025-04-14 13:00:14 +0900

    refactor: 에러 매핑 객체 파일 분리

--- src/hooks/useGlobalErrorHandler.ts
+++ src/hooks/useGlobalErrorHandler.ts
@@ -1,29 +1,9 @@
 import { PATH } from '@/constants/path';
 import { ExternalAPIError, InternalAPIError, NetworkError } from '@/errors/APIError';
+import { externalApiErrorMapping, internalApiErrorMapping } from '@/errors/errorMappings';
 import router from '@/router';
-import { OpenDialogOption, useDialogActions } from '@/stores/dialogStore';
+import { useDialogActions } from '@/stores/dialogStore';
 import { useToastActions } from '@/stores/toastStore';
-
-const internalApiErrorMapping: Record<
-  string,
-  (handlers: { openDialog: (option: OpenDialogOption) => void }) => void
-> = {
-  'G-10': () => void router.navigate(PATH.nonSpecificError),
-  'G-07': ({ openDialog }) =>
-    openDialog({
-      type: 'expiredSession',
-      onPrimaryBtnClick: () => void router.navigate(PATH.applyVerify),
-    }),
-  NOT_FOUND_MEMBER: () => void router.navigate(PATH.notFoundError),
-  PROJECT_NOT_FOUND: () => void router.navigate(PATH.notFoundError),
-  QUESTION_NOT_FOUND: () => void router.navigate(PATH.notFoundError),
-  RECRUIT_NOT_FOUND: () => void router.navigate(PATH.notFoundError),
-};
-
-const externalApiErrorMapping: Record<number, () => void> = {
-  403: () => void router.navigate(PATH.notFoundError),
-  404: () => void router.navigate(PATH.notFoundError),
-};
 
 export const useGlobalErrorHandler = () => {
   const { openDialog } = useDialogActions();
