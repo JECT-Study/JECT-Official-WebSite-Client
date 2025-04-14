6399f2b699e76337ee02a0fe4b7eb6de99ede089
onejuneee <onejuneee721@gmail.com>
2025-04-15 04:38:11 +0900

    typo: 변경된 인터페이스 명을 반영합니다.

--- src/apis/semester.ts
+++ src/apis/semester.ts
@@ -1,7 +1,7 @@
 import { API_ENDPOINT } from '@/constants/apiEndpoint';
-import { SemesterResponse } from '@/types/apis/semester';
+import { SemestersResponse } from '@/types/apis/semester';
 import { requestHandler } from '@/utils/httpClient';
 
 export const getSemesters = async () => {
-  return await requestHandler<SemesterResponse>('get', API_ENDPOINT.semesters);
+  return await requestHandler<SemestersResponse>('get', API_ENDPOINT.semesters);
 };
--- src/hooks/useSemestersQuery.ts
+++ src/hooks/useSemestersQuery.ts
@@ -2,10 +2,10 @@ import { useQuery } from '@tanstack/react-query';
 
 import { getSemesters } from '@/apis/semester';
 import { ApiResponse } from '@/types/apis/response';
-import { SemesterResponse } from '@/types/apis/semester';
+import { SemestersResponse } from '@/types/apis/semester';
 
 export const useSemestersQuery = () => {
-  return useQuery<ApiResponse<SemesterResponse>>({
+  return useQuery<ApiResponse<SemestersResponse>>({
     queryKey: ['getSemesters'],
     queryFn: getSemesters,
   });
