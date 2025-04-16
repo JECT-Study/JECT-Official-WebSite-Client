353183a51bb38d1b7ecadc334d6bd2d4e5682246
Banana_milk <101927445+WonJuneKim@users.noreply.github.com>
2025-04-16 17:59:43 +0900

    feat: 프로젝트 탭 API 연동 (#147)
    * feat: 프로젝트 관련 api의 엔드포인트를 추가합니다.
    
    * feat: 프로젝트 후기 api의 response에 대한 인터페이스를 추가합니다.
    
    * feat: 프로젝트 후기 api 호출부를 구현합니다.
    
    * feat: 커스텀 훅을 이용해 프로젝트 후기 api를 호출합니다.
    
    * feat: 프로젝트 후기 커스텀 훅에 InfinityQuery를 적용합니다.
    
    * feat: 프로젝트 페이지에 후기 부분을 커스텀 훅과 연동합니다.
    
    * feat: 데이터가 없는 상황에 EmptyData 컴포넌트를 렌더링합니다.
    
    * feat: 프로젝트 목록 api response에 대한 인터페이스를 작성합니다.
    
    * feat: 프로젝트 목록 조회 api의 호출부를 작성합니다.
    
    * feat: 커스텀 훅을 이용해 프로젝트 목록 호출 api를 무한 데이터로 관리합니다.
    
    * feat: 무한 스크롤을 관리하기 위한 공용 훅을 생성합니다.
    
    * feat: 프로젝트 목록과 프로젝트 후기 페이지를 모두 공통 무한 스크롤 훅을 이용해 관리합니다.
    
    * typo: 프로젝트 후기 조회 훅의 네이밍을 변경합니다.
    
    * typo: 프로젝트 후기 조회 훅의 네이밍을 프로젝트 페이지에 반영합니다.
    
    * move: 프로젝트 상세 페이지의 더미 데이터를 제거합니다.
    
    * feat: 프로젝트 상세 조회 api response의 인터페이스를 작성합니다.
    
    * feat: 프로젝트 상세 조회 api의 호출부를 작성합니다.
    
    * feat: 커스텀 훅을 이용해 프로젝트 상세 조회 api을 관리합니다.
    
    * feat: 프로젝트 상세 페이지에 커스텀 훅을 이용한 데이터 패칭을 적용합니다.
    
    * feat: 기수 등록 관련 api의 엔드 포인트를 생성합니다.
    
    * feat: 기수 등록 관련 api response의 인터페이스를 생성합니다.
    
    * feat: 기수 목록 조회 api 호출부를 생성합니다.
    
    * feat: 커스텀 훅을 이용하여 기수 목록 조회 api를 관리합니다.
    
    * fix: response로 오는 api의 인터페이스를 조정합니다.
    
    * feat: CalloutInformation 컴포넌트의 props 기본 값을 추가합니다.
    
    * move: 불필요한 형태 변화 함수를 제거합니다.
    
    * move: 사용하지 않는 외부 함수를 제거합니다.
    
    * feat: 프로젝트 페이지에서 기수 목록 조회 api를 호출하여 기수 목록을 출력합니다.
    
    * fix: 기수 목록 조회 api response의 인터페이스를 실제 api와 맞게 수정합니다.
    
    * typo: 변경된 인터페이스 명을 반영합니다.
    
    * feat: 기수 목록 조회 api로 받은 데이터를 가공하여 기수의 id를 저장합니다.
    
    * fix: 실제 데이터가 없을 때 화면이 렌더링 되지 않던 문제를 옵셔널 체이닝을 통해 해결합니다.
    
    * docs: 예상되는 ErrorBoundary 설정을 todo로 기록합니다.
    
    * fix: 커스텀 훅의 선언 구간을 통일하여 상단부로 조정합니다.
    
    * refactor: CalloutInformation의 labels props를 옵셔널로 처리합니다.
    
    * feat: 전달 받는 데이터 중 객체 형태의 데이터가 falsy한 값일 경우를 전처리합니다.
    
    * refactor: 실제 EmptyData가 랜더링 되는 케이스에 api response의 Status 코드를 반영합니다.
    
    * feat: api response의 status 및 상태에 따라 해당하는 페이지로의 네비게이션을 추가합니다.
    
    * feat: Tab에 사용되는 요소들이 빈 객체일 때의 타입 체크를 한 뒤, 결과에 따른 렌더링을 분기합니다.

---
+++ src/apis/semester.ts
@@ -0,0 +1,7 @@
+import { API_ENDPOINT } from '@/constants/apiEndpoint';
+import { SemestersResponse } from '@/types/apis/semester';
+import { requestHandler } from '@/utils/httpClient';
+
+export const getSemesters = async () => {
+  return await requestHandler<SemestersResponse>('get', API_ENDPOINT.semesters);
+};
