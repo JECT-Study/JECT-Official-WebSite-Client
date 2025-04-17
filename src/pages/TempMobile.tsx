ba3937207742504955efddfca0fbd4cc582095e0
rkdcodus <rcd7275@naver.com>
2025-04-17 17:35:42 +0900

    refactor: 임시 모바일 UI 푸터 class 속성 수정

--- src/pages/TempMobile.tsx
+++ src/pages/TempMobile.tsx
@@ -11,7 +11,7 @@ function TempMobile() {
       <header className='bg-surface-standard-dark border-border-assistive-dark border-b px-(--gap-md) py-(--gap-xs)'>
         <Logo height={18} fillColor='fill-object-hero-dark' />
       </header>
-      <div className='gap-2xl bg-surface-standard-dark flex min-h-dvh flex-col items-center justify-center *:-translate-y-[4.875rem]'>
+      <div className='gap-2xl bg-surface-standard-dark flex min-h-dvh flex-col items-center justify-center'>
         <img src={coneImage} alt='물음표 이미지' className='w-[6rem]' />
         <div className='gap-3xs flex flex-col text-center'>
           <p className='title-02 text-object-hero-dark'>
@@ -22,26 +22,28 @@ function TempMobile() {
           </p>
         </div>
       </div>
-      <footer className='bg-surface-tinted-dark text-object-alternative-dark gap-md absolute bottom-0 left-0 flex h-[8.875rem] w-dvw flex-col p-(--gap-md)'>
-        <div>
-          <Logo height={16} fillColor='fill-object-normal-dark' />
-        </div>
-        <p className='body-2xs'>
-          젝트는 IT 사이드 프로젝트 동아리입니다.
-          <br /> 몰입하며 진행하는 즐거운 프로젝트를 지향하고 있어요.
-        </p>
-        <div className='flex items-center'>
-          <p className='body-2xs flex-1'>{JECT_EMAIL}</p>
-          <div className='flex h-[1.5rem] gap-[1.25rem]'>
-            <NewTabLink href='https://github.com/JECT-Study'>
-              <Github width={24} />
-            </NewTabLink>
-            <NewTabLink href='https://www.youtube.com/@ject_it_club'>
-              <Youtube height={24} />
-            </NewTabLink>
+      <div className='bg-surface-standard-dark'>
+        <footer className='bg-surface-tinted-dark text-object-alternative-dark gap-md flex min-h-[8.875rem] flex-col p-(--gap-md)'>
+          <div>
+            <Logo height={16} fillColor='fill-object-normal-dark' />
           </div>
-        </div>
-      </footer>
+          <p className='body-2xs'>
+            젝트는 IT 사이드 프로젝트 동아리입니다.
+            <br /> 몰입하며 진행하는 즐거운 프로젝트를 지향하고 있어요.
+          </p>
+          <div className='flex items-center'>
+            <p className='body-2xs flex-1'>{JECT_EMAIL}</p>
+            <div className='flex h-[1.5rem] gap-[1.25rem]'>
+              <NewTabLink href='https://github.com/JECT-Study'>
+                <Github width={24} />
+              </NewTabLink>
+              <NewTabLink href='https://www.youtube.com/@ject_it_club'>
+                <Youtube height={24} />
+              </NewTabLink>
+            </div>
+          </div>
+        </footer>
+      </div>
     </div>
   );
 }
