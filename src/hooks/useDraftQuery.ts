b9c2af954a3836edbe11250398282b14dc4a3056
rkdcodus <rcd7275@naver.com>
2025-04-17 19:47:23 +0900

    refactor: 임시저장 조회 retry 0번으로 수정

--- src/hooks/useDraftQuery.ts
+++ src/hooks/useDraftQuery.ts
@@ -15,7 +15,7 @@ const useDraftQuery = (enabled?: boolean) => {
     queryKey: ['getDraft'],
     queryFn: getDraft,
     enabled: enabled ?? true,
-    retry: 1,
+    retry: 0,
   });
 
   return { data, refetch };
