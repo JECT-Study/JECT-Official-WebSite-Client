cf63bdbb7bfead236f860dc25f81122430e808f4
rkdcodus <rcd7275@naver.com>
2025-04-17 18:58:31 +0900

    fix: 지원서 제출 시 페이지 이탈 다이얼로그 제출 안되도록 수정

--- src/hooks/useDirtyCheckDialog.ts
+++ src/hooks/useDirtyCheckDialog.ts
@@ -1,12 +1,17 @@
 import { useEffect } from 'react';
 import { useBlocker } from 'react-router-dom';
 
+import { PATH } from '@/constants/path';
 import { useDialogActions } from '@/stores/dialogStore';
 
 const useDirtyCheckDialog = (when: boolean) => {
   const { openDialog } = useDialogActions();
   const blocker = useBlocker(({ currentLocation, nextLocation }) => {
-    return when && currentLocation.pathname !== nextLocation.pathname;
+    return (
+      when &&
+      currentLocation.pathname !== nextLocation.pathname &&
+      nextLocation.pathname !== PATH.applyComplete
+    );
   });
 
   useEffect(() => {
