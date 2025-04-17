1715199502d02ef8bec6b892802724515cadcdaa
onejuneee <onejuneee721@gmail.com>
2025-04-17 20:15:44 +0900

    refactor: 수정된 토큰 재발급 api의 요청과 반환 인터페이스를 반영합니다.

--- src/apis/auth.ts
+++ src/apis/auth.ts
@@ -1,17 +1,15 @@
 import axios from 'axios';
 
 import { API_ENDPOINT } from '@/constants/apiEndpoint';
-import { RefreshTokenRequest, RefreshTokenResponse } from '@/types/apis/auth';
+import { RefreshTokenResponse } from '@/types/apis/auth';
 import { ApiResponse } from '@/types/apis/response';
 
 const BASE_URL = import.meta.env.PROD
   ? import.meta.env.VITE_API_URL_PROD
   : import.meta.env.VITE_API_URL_DEV;
 
-export const refreshAccessToken = async (
-  data: RefreshTokenRequest,
-): Promise<ApiResponse<RefreshTokenResponse>> => {
+export const refreshAccessToken = async (): Promise<ApiResponse<RefreshTokenResponse>> => {
   return await axios
-    .post<ApiResponse<RefreshTokenResponse>>(`${BASE_URL}${API_ENDPOINT.refreshToken}`, data)
+    .post<ApiResponse<RefreshTokenResponse>>(`${BASE_URL}${API_ENDPOINT.refreshToken}`)
     .then(response => response.data);
 };
