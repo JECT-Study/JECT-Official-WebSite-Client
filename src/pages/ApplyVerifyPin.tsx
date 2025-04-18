84189e27810b37e729fb589de94115bc23204302
rkdcodus <rcd7275@naver.com>
2025-04-18 20:08:35 +0900

    test: 재설정 버튼 disabled 풀기

--- src/pages/ApplyVerifyPin.tsx
+++ src/pages/ApplyVerifyPin.tsx
@@ -197,7 +197,7 @@ function ApplyVerifyPin({ email }: ApplyVerifyPinProps) {
             <Label hierarchy='weak' weight='normal' textColor='text-object-alternative-dark'>
               혹시 PIN을 잊어버리셨나요?
             </Label>
-            <button disabled className='*:underline' onClick={() => setIsResetPin(true)}>
+            <button className='*:underline' onClick={() => setIsResetPin(true)}>
               <Label hierarchy='weak' weight='normal' textColor='text-feedback-information-dark'>
                 PIN 다시 설정하기
               </Label>
