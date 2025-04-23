b6d69152dca51587c432a621d35b4d02415e4d5b
rkdcodus <rcd7275@naver.com>
2025-04-23 18:08:41 +0900

    feat: 헤더 로고 버튼 aria-label 속성 추가

--- src/components/common/navigation/Navigation.tsx
+++ src/components/common/navigation/Navigation.tsx
@@ -29,6 +29,7 @@ function Navigation({ children }: NavigationProps) {
   return (
     <header className='border-border-assistive-dark bg-surface-standard-dark gap-6xl fixed z-49 flex h-[3.68rem] w-dvw items-center border-b pl-(--gap-5xl)'>
       <button
+        aria-label='메인으로 이동'
         onClick={handleClick}
         className='focus-visible:shadow-focus-visible radius-4xs cursor-pointer p-(--gap-5xs) outline-none'
       >
