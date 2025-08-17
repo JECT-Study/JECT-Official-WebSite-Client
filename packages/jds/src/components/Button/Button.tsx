7b0b8c2b483d378b361e4eb7c5d9aeaf9707e850
rkdcodus <rcd7275@naver.com>
2025-08-17 23:04:27 +0900

    feat: Button에 토큰을 사용한 임시 스타일을 적용합니다

--- packages/jds/src/components/Button/Button.tsx
+++ packages/jds/src/components/Button/Button.tsx
@@ -1,15 +1,49 @@
+import { ThemeProvider } from '@emotion/react';
+import styled from '@emotion/styled';
 import { forwardRef } from 'react';
 import type { ButtonHTMLAttributes, ReactNode } from 'react';
+
+import { GlobalStyles } from '../../style/globalStyle';
+import { rem } from 'utils';
+import { lightTheme } from 'theme';
 
 export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode;
 }
 
+const StyledButton = styled.button(
+  ({ theme: jds }) => `
+display: inline-flex;
+  min-width: 28px;
+  padding: ${jds.scheme.desktop.spacing[2]}px
+    ${jds.scheme.desktop.spacing[6]}px;
+  justify-content: center;
+  align-items: center;
+
+  border-radius: ${rem(jds.scheme.desktop.radius[4])};
+  opacity: ${jds.scheme.desktop.opacity[100]};
+  background-color: ${jds.color.feedback.positive.neutral};
+  color: ${jds.color.object.static.inverse.boldest};
+  text-align: center;
+
+  /* textStyle/label/lg/normal */
+  font-family: ${jds.typo.desktop.typeface.label};
+  font-size: ${jds.typo.desktop.font.size.label.lg}px;
+  font-style: normal;
+  font-weight: ${jds.typo.desktop.font.weight.label.normal};
+  line-height: ${jds.typo.desktop.font.line.height.label.lg}px;
+  letter-spacing: ${jds.typo.desktop.font.letter.spacing.label.lg}px;
+`,
+);
+
 export const Button = forwardRef<HTMLButtonElement, ButtonProps>(({ children, ...props }, ref) => {
   return (
-    <button ref={ref} {...props}>
-      {children}
-    </button>
+    <ThemeProvider theme={lightTheme}>
+      <GlobalStyles />
+      <StyledButton ref={ref} {...props}>
+        {children}
+      </StyledButton>
+    </ThemeProvider>
   );
 });
 
