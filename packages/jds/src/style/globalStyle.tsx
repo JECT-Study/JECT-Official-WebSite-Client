95f6cf2620a9a41f744530d935b7ad5bcffc2de6
rkdcodus <rcd7275@naver.com>
2025-08-17 23:02:09 +0900

    feat: globalStyle을 생성합니다

---
+++ packages/jds/src/style/globalStyle.tsx
@@ -0,0 +1,54 @@
+/** @jsxImportSource @emotion/react */
+import { css, Global } from '@emotion/react';
+import PretendardVariable from '../../fonts/PretendardVariable.woff2';
+import D2Coding from '../../fonts/D2Coding.woff2';
+
+export const GlobalStyles = () => (
+  <Global
+    styles={css`
+      @font-face {
+        font-family: 'Pretendard Variable';
+        src: url(${PretendardVariable}) format('woff2-variations');
+        font-weight: 45 920; /* 변수 폰트 weight 범위 */
+        font-style: normal;
+        font-display: swap;
+      }
+
+      @font-face {
+        font-family: 'D2Coding';
+        src: url(${D2Coding}) format('woff2');
+        font-weight: 400;
+        font-style: normal;
+        font-display: swap;
+      }
+
+      body {
+        font-family: 'Pretendard Variable', 'D2Coding', sans-serif;
+      }
+
+      *,
+      *::before,
+      *::after {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box; /* 패딩/보더 포함한 크기 계산 */
+      }
+
+      a {
+        text-decoration: none;
+        color: inherit;
+      }
+
+      ul,
+      ol {
+        list-style: none;
+      }
+
+      button {
+        border: none;
+        background: none;
+        cursor: pointer;
+      }
+    `}
+  />
+);
