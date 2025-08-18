cf07e94219b87448ba1782f1f8240ce5e0a9403d
rkdcodus <rcd7275@naver.com>
2025-08-19 03:16:49 +0900

    feat: textStyle함수에 타입 처리를 합니다.

--- packages/jds/src/utils/textStyle.ts
+++ packages/jds/src/utils/textStyle.ts
@@ -1,15 +1,24 @@
 import { Theme } from '@emotion/react';
 import { em, lh, rem } from './cssUnit';
-import { DeviceType, TextStyle } from 'types';
+import {
+  BodyModifierKey,
+  BodyScaleKey,
+  DeviceType,
+  HeroScaleKey,
+  LabelModifierKey,
+  LabelScaleKey,
+  SyntaxScaleKey,
+  TextStyle,
+} from 'types';
 
 export function textStyle(theme: Theme, device: DeviceType, typoToken: TextStyle): string {
   const [element, ...rest] = typoToken.split('.');
 
   if (element === 'hero' || element === 'title') {
-    const scale = Number(rest[0]);
+    const scale = rest[0] as HeroScaleKey;
     const fontSize = theme.typo[device].font.size[element][scale];
 
-    if (scale === 3 || scale === 4) {
+    if (scale === '3' || scale === '4') {
       return `
             font-family: ${theme.typo[device].typeface[element]};
             font-size: ${rem(fontSize)};
@@ -28,8 +37,20 @@ export function textStyle(theme: Theme, device: DeviceType, typoToken: TextStyle
             line-height: ${lh(fontSize, theme.typo[device].font.line.height[element][scale])};
             letter-spacing: ${em(fontSize, theme.typo[device].font.letter.spacing[element][scale])};
     `;
-  } else if (element === 'label' || element === 'body') {
-    const [scale, modifier] = rest;
+  } else if (element === 'label') {
+    const [scale, modifier] = rest as [LabelScaleKey, LabelModifierKey];
+    const fontSize = theme.typo[device].font.size[element][scale];
+
+    return `
+            font-family: ${theme.typo[device].typeface[element]};
+            font-size: ${rem(fontSize)};
+            font-style: normal;
+            font-weight: ${theme.typo[device].font.weight[element][modifier]};
+            line-height: ${lh(fontSize, theme.typo[device].font.line.height[element][scale])};
+            letter-spacing: ${em(fontSize, theme.typo[device].font.letter.spacing[element][scale])};
+    `;
+  } else if (element === 'body') {
+    const [scale, modifier] = rest as [BodyScaleKey, BodyModifierKey];
     const fontSize = theme.typo[device].font.size[element][scale];
 
     return `
@@ -41,18 +62,17 @@ export function textStyle(theme: Theme, device: DeviceType, typoToken: TextStyle
             letter-spacing: ${em(fontSize, theme.typo[device].font.letter.spacing[element][scale])};
     `;
   } else if (element === 'syntax') {
-    const modifier = rest[0];
-    const fontSize = theme.typo[device].font.size[element][modifier];
+    const scale = rest[0] as SyntaxScaleKey;
+    const fontSize = theme.typo[device].font.size[element][scale];
 
     return `
         font-family: ${theme.typo[device].typeface[element]};
         font-size: ${rem(fontSize)};
         font-style: normal;
         font-weight: ${theme.typo[device].font.weight[element].normal};
-        line-height: ${lh(fontSize, theme.typo[device].font.line.height[element][modifier])};
+        line-height: ${lh(fontSize, theme.typo[device].font.line.height[element][scale])};
     `;
   }
 
-  console.error('올바른 토큰이 아닙니다', typoToken);
   return '';
 }
