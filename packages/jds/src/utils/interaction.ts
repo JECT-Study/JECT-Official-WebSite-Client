da4570296e31cb726c284314c213c742cff409c0
rkdcodus <rcd7275@naver.com>
2025-08-22 22:02:43 +0900

    feat: 인터랙션 레이어를 생성하는 interaction 함수를 정의합니다

---
+++ packages/jds/src/utils/interaction.ts
@@ -0,0 +1,109 @@
+import { Theme } from '@emotion/react';
+import { Density, FillColor, Variant } from 'types';
+
+export function interaction(
+  theme: Theme,
+  variant: Variant,
+  density: Density,
+  fillColor: FillColor,
+  isReadonly: boolean = false,
+  isDisabled: boolean = false,
+  isLocked: boolean = false,
+) {
+  const createAfter = (backgroundColor: string) => {
+    const afterBaseStyle = {
+      content: '""',
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      width: '100%',
+      height: '100%',
+      backgroundColor: backgroundColor,
+    };
+
+    if (isLocked) {
+      return {
+        position: 'relative',
+        outline: 'none',
+        '::after': { ...afterBaseStyle, opacity: 0.08 },
+      };
+    }
+
+    if (isDisabled) {
+      return {
+        position: 'relative',
+        outline: 'none',
+        '::after': { ...afterBaseStyle, opacity: 0.05 },
+      };
+    }
+
+    if (isReadonly) {
+      return {
+        position: 'relative',
+        outline: 'none',
+        '::after': { ...afterBaseStyle, opacity: 0 },
+      };
+    }
+
+    return {
+      position: 'relative',
+      outline: 'none',
+      '::after': { ...afterBaseStyle, opacity: 0 },
+      '&:hover::after': { opacity: 0.08 },
+      '&:active::after': { opacity: 0.12 },
+      '&:focus-visible': { boxShadow: `0 0 0 3px ${theme.color.interaction.focus}` },
+    };
+  };
+
+  if (fillColor === 'default') {
+    if (variant === 'normal') {
+      if (density === 'bold') return createAfter(theme.color.interaction.bold);
+      if (density === 'normal') return createAfter(theme.color.interaction.normal);
+      if (density === 'assistive') return createAfter(theme.color.interaction.assistive);
+      if (density === 'subtle') return createAfter(theme.color.interaction.subtle);
+    } else if (variant === 'accent') {
+      if (density === 'bold') return createAfter(theme.color.accent.bold);
+      if (density === 'normal') return createAfter(theme.color.accent.normal);
+      if (density === 'assistive') return createAfter(theme.color.accent.neutral);
+      if (density === 'subtle') return createAfter(theme.color.accent.alternative);
+    } else if (variant === 'positive') {
+      if (density === 'bold') return createAfter(theme.color.feedback.positive.bold);
+      if (density === 'normal') return createAfter(theme.color.feedback.positive.normal);
+      if (density === 'assistive') return createAfter(theme.color.feedback.positive.neutral);
+      if (density === 'subtle') return createAfter(theme.color.feedback.positive.alternative);
+    } else if (variant === 'destructive') {
+      if (density === 'bold') return createAfter(theme.color.feedback.destructive.bold);
+      if (density === 'normal') return createAfter(theme.color.feedback.destructive.normal);
+      if (density === 'assistive') return createAfter(theme.color.feedback.destructive.neutral);
+      if (density === 'subtle') return createAfter(theme.color.feedback.destructive.alternative);
+    }
+  } else if (fillColor === 'inverse') {
+    if (variant === 'normal') {
+      if (density === 'bold') return createAfter(theme.color.interaction.inverse.bold);
+      if (density === 'normal') return createAfter(theme.color.interaction.inverse.normal);
+      if (density === 'assistive') return createAfter(theme.color.interaction.inverse.assistive);
+      if (density === 'subtle') return createAfter(theme.color.interaction.inverse.subtle);
+    } else if (variant === 'accent') {
+      if (density === 'bold') return createAfter(theme.color.accent.inverse.bold);
+      if (density === 'normal') return createAfter(theme.color.accent.inverse.normal);
+      if (density === 'assistive') return createAfter(theme.color.accent.inverse.neutral);
+      if (density === 'subtle') return createAfter(theme.color.accent.inverse.alternative);
+    } else if (variant === 'positive') {
+      if (density === 'bold') return createAfter(theme.color.feedback.positive.inverse.bold);
+      if (density === 'normal') return createAfter(theme.color.feedback.positive.inverse.normal);
+      if (density === 'assistive')
+        return createAfter(theme.color.feedback.positive.inverse.neutral);
+      if (density === 'subtle')
+        return createAfter(theme.color.feedback.positive.inverse.alternative);
+    } else if (variant === 'destructive') {
+      if (density === 'bold') return createAfter(theme.color.feedback.destructive.inverse.bold);
+      if (density === 'normal') return createAfter(theme.color.feedback.destructive.inverse.normal);
+      if (density === 'assistive')
+        return createAfter(theme.color.feedback.destructive.inverse.neutral);
+      if (density === 'subtle')
+        return createAfter(theme.color.feedback.destructive.inverse.alternative);
+    }
+  }
+
+  return {};
+}
