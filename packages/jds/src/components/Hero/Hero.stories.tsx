9993b340d997808a3e04f806385938ae9536ac20
rkdcodus <rcd7275@naver.com>
2025-08-23 18:58:24 +0900

    docs: Hero 컴포넌트 스토리북을 작성합니다

---
+++ packages/jds/src/components/Hero/Hero.stories.tsx
@@ -0,0 +1,42 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Hero } from './Hero';
+
+const meta = {
+  title: 'Components/Hero',
+  component: Hero,
+  parameters: {
+    layout: 'centered',
+  },
+  argTypes: {
+    children: {
+      control: 'text',
+      description: '버튼 내부 콘텐츠',
+      defaultValue: 'Hero',
+    },
+  },
+} satisfies Meta<typeof Hero>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    size: 'lg',
+    textAlign: 'center',
+    children: '히어로',
+  },
+};
+
+export const Test: Story = {
+  name: 'With Wide Parent Element',
+  args: {
+    size: 'lg',
+    textAlign: 'left',
+    children: '히어로',
+  },
+  render: args => (
+    <div style={{ width: '500px', border: '1px solid red' }}>
+      <Hero {...args} />
+    </div>
+  ),
+};
