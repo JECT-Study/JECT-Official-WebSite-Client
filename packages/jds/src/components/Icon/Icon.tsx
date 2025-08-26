96b4c613dda68602e908bcd08a76845f6248de38
rkdcodus <rcd7275@naver.com>
2025-08-26 18:29:07 +0900

    feat: Icon 컴포넌트를 구현합니다

---
+++ packages/jds/src/components/Icon/Icon.tsx
@@ -0,0 +1,14 @@
+import { useTheme } from 'theme';
+import { IconProps } from './Icon.types';
+import { iconMap, sizeMap } from './IconMap';
+
+export const Icon = ({ name, size = 'md', color }: IconProps) => {
+  const IconComponent = iconMap[name];
+  if (!IconComponent) return null;
+
+  const pixelSize = sizeMap[size];
+  const theme = useTheme();
+  const BaseColor = color ? color : theme.color.object.boldest;
+
+  return <IconComponent width={pixelSize} height={pixelSize} style={{ color: BaseColor }} />;
+};
---
+++ packages/jds/src/components/Icon/Icon.types.ts
@@ -0,0 +1,10 @@
+import { iconMap } from './IconMap';
+
+export type IconName = keyof typeof iconMap;
+export type IconSize = '5xl' | '4xl' | '3xl' | '2xl' | 'xl' | 'lg' | 'md' | 'sm' | 'xs' | '2xs';
+
+export interface IconProps {
+  name: IconName;
+  size: IconSize;
+  color: string;
+}
