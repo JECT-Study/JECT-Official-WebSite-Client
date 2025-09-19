5802022a82b2e66b78b6d7c0fd1ec47161717834
rkdcodus <rcd7275@naver.com>
2025-09-19 17:19:35 +0900

    refactor: Badge의 타입을 파일로 분리합니다

---
+++ packages/jds/src/components/Badge/Badge.types.ts
@@ -0,0 +1,4 @@
+export type BadgeHierarchy = 'accent' | 'primary' | 'secondary' | 'tertiary';
+export type BadgeStyle = 'solid' | 'alpha' | 'outlined';
+export type BadgeSize = 'lg' | 'md' | 'sm' | 'xs';
+export type BadgeVariant = 'positive' | 'destructive' | 'notifying';
