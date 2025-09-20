73fe1308bdb1d507563537f99bf62008708e24a5
rkdcodus <rcd7275@naver.com>
2025-09-21 00:18:28 +0900

    feat: Badge에 index.ts를 추가합니다

---
+++ packages/jds/src/components/Badge/index.ts
@@ -0,0 +1,6 @@
+export * from './contentBasicBadge/ContentBasicBadge';
+export * from './contentFeedbackBadge/ContentFeedbackBadge';
+export * from './contentThemeBadge/ContentThemeBadge';
+export * from './dotFeedbackBadge/DotFeedbackBadge';
+export * from './numericBasicBadge/NumericBasicBadge';
+export * from './numericFeedbackBadge/NumericFeedbackBadge';
