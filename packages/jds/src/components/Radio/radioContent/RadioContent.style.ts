a517759453034a60fdea4f0114e7dc91d44f35fc
rkdcodus <rcd7275@naver.com>
2025-10-04 02:34:08 +0900

    feat: scheme 토큰화 및 반응형으로 구현합니다

--- packages/jds/src/components/Radio/radioContent/RadioContent.style.ts
+++ packages/jds/src/components/Radio/radioContent/RadioContent.style.ts
@@ -1,5 +1,5 @@
 import styled from '@emotion/styled';
-import { interaction } from 'utils';
+import { interaction, pxToRem } from 'utils';
 import { RADIO_CONTAINER_SIZE, RadioSize } from './radioContent.variants';
 
 interface RadioContainerProps {
@@ -11,9 +11,11 @@ interface RadioContainerProps {
 
 export const RadioContainerLabel = styled.label<RadioContainerProps>(
   ({ theme, radioSize, isDisabled, isAlignRight, isStyleOutline }) => {
-    const rowGap = RADIO_CONTAINER_SIZE[radioSize].gap;
-    const padding = RADIO_CONTAINER_SIZE[radioSize].padding;
-    const borderRadius = RADIO_CONTAINER_SIZE[radioSize].borderRadius;
+    const rowGap = RADIO_CONTAINER_SIZE[radioSize].gap as keyof typeof theme.scheme.desktop.spacing;
+    const padding = RADIO_CONTAINER_SIZE[radioSize]
+      .padding as keyof typeof theme.scheme.desktop.spacing;
+    const borderRadius = RADIO_CONTAINER_SIZE[radioSize]
+      .borderRadius as keyof typeof theme.scheme.desktop.radius;
     const interactionWidth = RADIO_CONTAINER_SIZE[radioSize].width;
     const interactionHeight = RADIO_CONTAINER_SIZE[radioSize].height;
     const borderColor = isDisabled
@@ -46,7 +48,6 @@ export const RadioContainerLabel = styled.label<RadioContainerProps>(
     return {
       display: 'grid',
       gridTemplateColumns: 'auto 1fr',
-      gap: `6px ${rowGap}px `,
       '& > :nth-child(1)': {
         gridColumn: 1,
         gridRow: 1,
@@ -63,17 +64,31 @@ export const RadioContainerLabel = styled.label<RadioContainerProps>(
         gridColumn: isAlignRight ? '1 / span 2' : 2,
         gridRow: 2,
       },
+
+      gap: `${pxToRem(theme.scheme.desktop.spacing[6])} ${pxToRem(theme.scheme.desktop.spacing[rowGap])} `,
       border: isStyleOutline ? `1px solid ${borderColor}` : 'none',
-      borderRadius,
-      padding: isStyleOutline ? padding : '0',
+      borderRadius: pxToRem(theme.scheme.desktop.radius[borderRadius]),
+      padding: isStyleOutline ? pxToRem(theme.scheme.desktop.spacing[padding]) : '0',
       cursor: isDisabled ? 'default' : 'pointer',
       '& *': { cursor: 'inherit' },
       ...nonCheckedInteraction,
 
+      [theme.breakPoint.tablet]: {
+        gap: `${pxToRem(theme.scheme.tablet.spacing[6])} ${pxToRem(theme.scheme.tablet.spacing[rowGap])} `,
+        borderRadius: pxToRem(theme.scheme.tablet.radius[borderRadius]),
+        padding: isStyleOutline ? pxToRem(theme.scheme.tablet.spacing[padding]) : '0',
+      },
+
+      [theme.breakPoint.mobile]: {
+        gap: `${pxToRem(theme.scheme.mobile.spacing[6])} ${pxToRem(theme.scheme.mobile.spacing[rowGap])} `,
+        borderRadius: pxToRem(theme.scheme.mobile.radius[borderRadius]),
+        padding: isStyleOutline ? pxToRem(theme.scheme.mobile.spacing[padding]) : '0',
+      },
+
       '::after': {
         ...nonCheckedInteraction['::after'],
         ...addonInteraction,
-        transition: `all ${theme.environment.duration[500]}ms ${theme.environment.motion.fluent}`,
+        transition: `all ${theme.environment.duration[100]}ms ${theme.environment.motion.fluent}`,
       },
 
       '&:active::after': {
@@ -94,7 +109,15 @@ export const RadioContainerLabel = styled.label<RadioContainerProps>(
         content: '""',
         position: 'absolute',
         inset: 0,
-        borderRadius,
+        borderRadius: pxToRem(theme.scheme.desktop.radius[borderRadius]),
+
+        [theme.breakPoint.tablet]: {
+          borderRadius: pxToRem(theme.scheme.tablet.radius[borderRadius]),
+        },
+
+        [theme.breakPoint.mobile]: {
+          borderRadius: pxToRem(theme.scheme.mobile.radius[borderRadius]),
+        },
       },
 
       'input[type="radio"]:focus-visible + .visual': {
--- packages/jds/src/components/Radio/radioContent/radioContent.variants.ts
+++ packages/jds/src/components/Radio/radioContent/radioContent.variants.ts
@@ -2,34 +2,43 @@ import { LabelSize } from '@/components/Label/Label.style';
 
 export type RadioSize = 'lg' | 'md' | 'sm' | 'xs';
 
-export const RADIO_CONTAINER_SIZE = {
+export const RADIO_CONTAINER_SIZE: Record<
+  RadioSize,
+  {
+    width: number;
+    height: number;
+    gap: string;
+    padding: string;
+    borderRadius: string;
+  }
+> = {
   lg: {
     width: 13,
     height: 8,
-    gap: 12,
-    padding: 12,
-    borderRadius: 6,
+    gap: '12',
+    padding: '12',
+    borderRadius: '6',
   },
   md: {
     width: 13,
     height: 8,
-    gap: 10,
-    padding: 10,
-    borderRadius: 6,
+    gap: '10',
+    padding: '10',
+    borderRadius: '6',
   },
   sm: {
     width: 11,
     height: 6,
-    gap: 8,
-    padding: 8,
-    borderRadius: 4,
+    gap: '8',
+    padding: '8',
+    borderRadius: '4',
   },
   xs: {
     width: 9,
     height: 6,
-    gap: 8,
-    padding: 6,
-    borderRadius: 4,
+    gap: '8',
+    padding: '6',
+    borderRadius: '4',
   },
 };
 
