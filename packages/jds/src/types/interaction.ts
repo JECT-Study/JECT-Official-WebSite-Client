be571b94e87a19f9dfacc9abd11489775f48f618
onejuneee <onejuneee721@gmail.com>
2025-10-05 05:21:47 +0900

    feat: InteractionLayer의 자체 state 타입을 생성합니다.

--- packages/jds/src/types/interaction.ts
+++ packages/jds/src/types/interaction.ts
@@ -2,3 +2,4 @@ export type Variant = 'normal' | 'accent' | 'positive' | 'destructive';
 export type Density = 'bold' | 'normal' | 'assistive' | 'subtle';
 export type FillColor = 'default' | 'inverse';
 export type InteractionState = 'default' | 'readonly' | 'disabled' | 'locked';
+export type InteractionLayerState = 'rest' | 'hover' | 'active' | 'focus';
