17ddea6ff457562117ed6da702a3e907303432d5
onejuneee <onejuneee721@gmail.com>
2025-10-10 05:20:01 +0900

    refactor: Icon 컴포넌트가 currentColor를 출력하도록 변경합니다.

--- packages/jds/src/components/Icon/Icon.tsx
+++ packages/jds/src/components/Icon/Icon.tsx
@@ -1,14 +1,14 @@
-import { useTheme } from 'theme';
-import { IconProps } from './Icon.types';
+import type { IconProps } from './Icon.types';
 import { iconMap, sizeMap } from './IconMap';
 
-export const Icon = ({ name, size = 'md', color }: IconProps) => {
+export const Icon = ({ name, size = 'md', color = 'currentColor', ...props }: IconProps) => {
   const IconComponent = iconMap[name];
+
   if (!IconComponent) return null;
 
   const pixelSize = sizeMap[size];
-  const theme = useTheme();
-  const BaseColor = color ?? theme.color.object.boldest;
 
-  return <IconComponent width={pixelSize} height={pixelSize} color={BaseColor} />;
+  return (
+    <IconComponent width={pixelSize} height={pixelSize} color={color} {...props} />
+  );
 };
