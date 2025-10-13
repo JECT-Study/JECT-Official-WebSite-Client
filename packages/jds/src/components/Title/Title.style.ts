9a862d813fb3594a1bb3ac8a39e7616b67a8822d
codus <97906653+rkdcodus@users.noreply.github.com>
2025-10-14 02:00:14 +0900

    feat: Title 컴포넌트 구현 (#233)
    * feat: Title 컴포넌트를 구현합니다
    
    * docs: Title 스토리북을 작성합니다
    
    * refactor: TitleSize, TitleTextAlign으로 타입명을 수정합니다
    
    * refactor: Title 컴포넌트의 forwardRef를 제거합니다
    
    * fix: Title.tsx에 있던 스타일 코드를 style 파일에서 처리합니다
    
    * refactor: 매핑 객체 네이밍을 camelCaseMap 형태로 수정합니다
    
    * feat: Title Props를 index.ts에서 export 합니다

---
+++ packages/jds/src/components/Title/Title.style.ts
@@ -0,0 +1,37 @@
+import styled from '@emotion/styled';
+import { textStyle } from 'utils';
+import { TextStyle } from 'types';
+import { TEXT_ALIGN_MAPPING } from '../Hero/Hero.style';
+
+export type TitleSize = 'lg' | 'md' | 'sm' | 'xs';
+export type TitleTextAlign = keyof typeof TEXT_ALIGN_MAPPING;
+
+interface TitleDivProps {
+  size: TitleSize;
+  textAlign: TitleTextAlign;
+  color?: string;
+}
+
+export const titleStylesMap = {
+  lg: 'title.4',
+  md: 'title.3',
+  sm: 'title.2',
+  xs: 'title.1',
+} as const;
+
+export const TitleDiv = styled.div<TitleDivProps>(({ theme, size, textAlign, color }) => {
+  const textStyleKey = titleStylesMap[size] as TextStyle;
+  const justifyContent = TEXT_ALIGN_MAPPING[textAlign];
+  const BaseColor = color || theme.color.object.bolder;
+
+  return {
+    display: 'flex',
+    justifyContent,
+    alignItems: 'center',
+    color: BaseColor,
+    cursor: 'default',
+    [theme.breakPoint.mobile]: { ...textStyle(theme, 'mobile', textStyleKey) },
+    [theme.breakPoint.tablet]: { ...textStyle(theme, 'tablet', textStyleKey) },
+    [theme.breakPoint.desktop]: { ...textStyle(theme, 'desktop', textStyleKey) },
+  };
+});
