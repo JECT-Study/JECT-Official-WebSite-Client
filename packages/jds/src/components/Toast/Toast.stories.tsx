f5cb601c5db046a39323d375c63b92971ec15469
rkdcodus <rcd7275@naver.com>
2025-10-16 21:26:30 +0900

    docs: ToastProvider와 useToast 훅, 전역 toast 함수를 사용한 스토리를 작성합니다

--- packages/jds/src/components/Toast/Toast.stories.tsx
+++ packages/jds/src/components/Toast/Toast.stories.tsx
@@ -1,5 +1,8 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { Toast } from './Toast';
+import { toast, ToastProvider, useToast } from './ToastProvider';
+import { BlockButton } from '../Button/BlockButton';
+import { FlexColumn, FlexRow, Label } from '@storybook-utils/layout';
 
 const meta: Meta<typeof Toast.Basic> = {
   title: 'Components/Toast',
@@ -8,46 +11,13 @@ const meta: Meta<typeof Toast.Basic> = {
     layout: 'centered',
   },
   argTypes: {
-    variant: {
-      description: '토스트의 형태를 지정합니다.',
-      control: 'radio',
-      options: ['toast', 'snackbar'],
-      table: {
-        type: { summary: `'toast' | 'snackbar'` },
-        defaultValue: { summary: `'toast'` },
-      },
+    title: {
+      description: '토스트 타이틀 텍스트입니다.',
+      control: 'text',
     },
     caption: {
       description: '본문 아래에 표시되는 캡션 텍스트입니다.',
       control: 'text',
-      table: {
-        type: { summary: 'ReactNode' },
-      },
-    },
-    prefixButtonProps: {
-      description: '왼쪽(보조) 버튼의 속성입니다. `BlockButton.Basic`으로 전달됩니다.',
-      control: 'object',
-      table: {
-        type: {
-          summary: 'Omit<BlockButtonBasicProps, "hierarchy" | "size" | "variant">',
-        },
-      },
-    },
-    suffixButtonProps: {
-      description: '오른쪽(메인) 버튼의 속성입니다. `BlockButton.Basic`으로 전달됩니다.',
-      control: 'object',
-      table: {
-        type: {
-          summary: 'Omit<BlockButtonBasicProps, "hierarchy" | "size" | "variant">',
-        },
-      },
-    },
-    children: {
-      description: '토스트 타이틀 텍스트입니다.',
-      control: 'text',
-      table: {
-        type: { summary: 'ReactNode' },
-      },
     },
   },
 };
@@ -55,29 +25,12 @@ const meta: Meta<typeof Toast.Basic> = {
 export default meta;
 
 export const Basic: StoryObj<typeof Toast.Basic> = {
-  argTypes: {},
   args: {
-    variant: 'toast',
-    prefixButtonProps: {
-      children: '레이블',
-    },
-    suffixButtonProps: {
-      children: '레이블',
-    },
+    title: '토스트 제목 레이블',
     caption:
       '토스트 내용은 최대 다섯 줄 까지 입력 가능하며, 더 많은 내용을 입력해야 하는 상황에서는 별도의 안내 페이지로 유도합니다.',
-    children: '토스트 제목 레이블',
   },
-  render: args => (
-    <Toast.Basic
-      variant={args.variant}
-      caption={args.caption}
-      prefixButtonProps={args.prefixButtonProps}
-      suffixButtonProps={args.suffixButtonProps}
-    >
-      {args.children}
-    </Toast.Basic>
-  ),
+  render: args => <Toast.Basic title={args.title} caption={args.caption} />,
 };
 
 export const Feedback: StoryObj<typeof Toast.Feedback> = {
@@ -89,27 +42,131 @@ export const Feedback: StoryObj<typeof Toast.Feedback> = {
     },
   },
   args: {
-    variant: 'toast',
     feedback: 'positive',
-    prefixButtonProps: {
-      children: '레이블',
-    },
-    suffixButtonProps: {
-      children: '레이블',
-    },
+    title: '토스트 제목 레이블',
     caption:
       '토스트 내용은 최대 다섯 줄 까지 입력 가능하며, 더 많은 내용을 입력해야 하는 상황에서는 별도의 안내 페이지로 유도합니다.',
-    children: '토스트 제목 레이블',
   },
   render: args => (
-    <Toast.Feedback
-      variant={args.variant}
-      feedback={args.feedback}
-      caption={args.caption}
-      prefixButtonProps={args.prefixButtonProps}
-      suffixButtonProps={args.suffixButtonProps}
-    >
-      {args.children}
-    </Toast.Feedback>
+    <Toast.Feedback title={args.title} feedback={args.feedback} caption={args.caption} />
   ),
+};
+
+export const UseToastProvider: StoryObj<typeof Toast.Basic> = {
+  parameters: {
+    docs: {
+      description: {
+        story:
+          'useToast훅을 사용하여 토스트를 호출할 수 있습니다. useToast훅을 사용하기 위해서는 루트 컴포넌트 레벨에서 ToastProvider 작성이 필요합니다.',
+      },
+    },
+  },
+  decorators: [
+    Story => (
+      <ToastProvider>
+        <Story />
+      </ToastProvider>
+    ),
+  ],
+  render: () => {
+    const { toast } = useToast();
+    const caption =
+      '토스트 내용은 최대 다섯 줄 까지 입력 가능하며, 더 많은 내용을 입력해야 하는 상황에서는 별도의 안내 페이지로 유도합니다.';
+
+    const basicToast = () => toast.basic('베이직 토스트');
+    const positiveToast = () => toast.positive('피드백 토스트 - positive');
+    const destructiveToast = () => toast.destructive('피드백 토스트 - destructive');
+
+    const basicToastCaption = () => toast.basic('베이직 토스트', caption);
+    const positiveToastCaption = () => toast.positive('피드백 토스트 - positive', caption);
+    const destructiveToastCaption = () => toast.destructive('피드백 토스트 - destructive', caption);
+
+    return (
+      <FlexRow>
+        <FlexColumn>
+          <Label>only Title</Label>
+          <BlockButton.Basic onClick={basicToast} variant='outlined'>
+            Basic
+          </BlockButton.Basic>
+          <BlockButton.Feedback onClick={positiveToast} intent='positive'>
+            Feedback
+          </BlockButton.Feedback>
+          <BlockButton.Feedback onClick={destructiveToast} intent='destructive'>
+            Feedback
+          </BlockButton.Feedback>
+        </FlexColumn>
+        <FlexColumn>
+          <Label>with caption</Label>
+          <BlockButton.Basic onClick={basicToastCaption} variant='outlined'>
+            Basic
+          </BlockButton.Basic>
+          <BlockButton.Feedback onClick={positiveToastCaption} intent='positive'>
+            Feedback
+          </BlockButton.Feedback>
+          <BlockButton.Feedback onClick={destructiveToastCaption} intent='destructive'>
+            Feedback
+          </BlockButton.Feedback>
+        </FlexColumn>
+      </FlexRow>
+    );
+  },
+};
+
+export const UseGlobalToast: StoryObj<typeof Toast.Basic> = {
+  parameters: {
+    docs: {
+      description: {
+        story:
+          'Axios interceptor와 같이 ToastProvider 외부에서 토스트를 띄워야하는 경우(useToast훅을 사용할 수 없습 경우), 전역 toast 함수를 사용하여 토스트를 호출할 수 있습니다. 전역 toast 함수를 사용할 때도 ToastProvider 작성이 필요합니다.',
+      },
+    },
+  },
+  decorators: [
+    Story => (
+      <ToastProvider>
+        <Story />
+      </ToastProvider>
+    ),
+  ],
+  render: () => {
+    const caption =
+      '토스트 내용은 최대 다섯 줄 까지 입력 가능하며, 더 많은 내용을 입력해야 하는 상황에서는 별도의 안내 페이지로 유도합니다.';
+
+    const basicToast = () => toast.basic('베이직 토스트');
+    const positiveToast = () => toast.positive('피드백 토스트');
+    const destructiveToast = () => toast.destructive('피드백 토스트');
+
+    const basicToastCaption = () => toast.basic('베이직 토스트', caption);
+    const positiveToastCaption = () => toast.positive('피드백 토스트 - positive', caption);
+    const destructiveToastCaption = () => toast.destructive('피드백 토스트 - destructive', caption);
+
+    return (
+      <FlexRow>
+        <FlexColumn>
+          <Label>only Title</Label>
+          <BlockButton.Basic onClick={basicToast} variant='outlined'>
+            Basic
+          </BlockButton.Basic>
+          <BlockButton.Feedback onClick={positiveToast} intent='positive'>
+            Feedback
+          </BlockButton.Feedback>
+          <BlockButton.Feedback onClick={destructiveToast} intent='destructive'>
+            Feedback
+          </BlockButton.Feedback>
+        </FlexColumn>
+        <FlexColumn>
+          <Label>with caption</Label>
+          <BlockButton.Basic onClick={basicToastCaption} variant='outlined'>
+            Basic
+          </BlockButton.Basic>
+          <BlockButton.Feedback onClick={positiveToastCaption} intent='positive'>
+            Feedback
+          </BlockButton.Feedback>
+          <BlockButton.Feedback onClick={destructiveToastCaption} intent='destructive'>
+            Feedback
+          </BlockButton.Feedback>
+        </FlexColumn>
+      </FlexRow>
+    );
+  },
 };
