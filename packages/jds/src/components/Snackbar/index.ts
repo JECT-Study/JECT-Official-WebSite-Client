8393c1150a4ce90b545184c0364487f58dd0540a
rkdcodus <rcd7275@naver.com>
2025-10-21 00:56:41 +0900

    feat: 스낵바 index.ts를 작성합니다

---
+++ packages/jds/src/components/Snackbar/index.ts
@@ -0,0 +1,8 @@
+export { Snackbar } from './Snackbar';
+export type {
+  SnackbarFeedback,
+  SnackbarStyle,
+  SnackbarBaseProps,
+  SnackbarFeedbackProps,
+} from './snackbar.types';
+export { useSnackbar, SnackbarProvider, snackbar } from './SnackbarProvider';
