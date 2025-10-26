fa22a142d8768d23d6b660e8a69319e00757ac42
rkdcodus <rcd7275@naver.com>
2025-10-27 01:03:20 +0900

    docs: UploaderFile 컴포넌트 스토리북을 작성합니다
    드래그앤드롭 기능 X

---
+++ packages/jds/src/components/Uploader/Uploader.stories.tsx
@@ -0,0 +1,31 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Uploader } from './Uploader';
+
+const meta = {
+  title: 'Components/Uploader',
+  component: Uploader.File,
+  parameters: {
+    layout: 'centered',
+  },
+  argTypes: {
+    isLoading: {
+      control: 'boolean',
+      description: '업로드 로딩 여부',
+    },
+    isDisabled: {
+      control: 'boolean',
+      description: '업로더 비활성화 여부',
+    },
+  },
+} satisfies Meta<typeof Uploader.File>;
+
+export default meta;
+
+export const File: StoryObj<typeof Uploader.File> = {
+  name: 'File',
+  args: {
+    isLoading: false,
+    isDisabled: false,
+  },
+  render: args => <Uploader.File isLoading={args.isLoading} isDisabled={args.isDisabled} />,
+};
