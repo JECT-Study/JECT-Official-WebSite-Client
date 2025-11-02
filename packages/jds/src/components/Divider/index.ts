506c228524cc7037f3fe7bcaeef1971dbe4514e7
onejuneee <onejuneee721@gmail.com>
2025-11-03 06:48:07 +0900

    feat: Divider의 모든 요소를 한 곳에서 export 합니다.

---
+++ packages/jds/src/components/Divider/index.ts
@@ -0,0 +1,8 @@
+export { Divider } from './Divider';
+export type {
+  DividerThickness,
+  DividerOrientation,
+  DividerTextAlign,
+  DividerProps,
+  DividerWithTextProps,
+} from './divider.types';
