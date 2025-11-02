74e6e62d14e50b8c3d562eda4d6a68ccd5d1b350
onejuneee <onejuneee721@gmail.com>
2025-11-03 07:03:08 +0900

    fix: 이미지 컴포넌트의 토큰 명을 수정합니다.

--- packages/jds/src/components/Image/Image.style.ts
+++ packages/jds/src/components/Image/Image.style.ts
@@ -1,7 +1,9 @@
 import styled from '@emotion/styled';
-import { ImgOrientation, ImgRatio } from './Image';
 import { CSSProperties } from 'react';
 import { interaction, pxToRem } from 'utils';
+
+import { ImgOrientation, ImgRatio } from './Image';
+
 import { Label, Icon } from '@/components';
 
 export const getAspectRatioValue = (
@@ -34,7 +36,7 @@ export const ImageButton = styled.button<ImageButtonProps>(
       width: '100%',
       height: '100%',
       aspectRatio: getAspectRatioValue(ratio, orientation),
-      border: `${theme.scheme.desktop.stroke.weight[1]}px solid ${theme.color.stroke.alpha.subtler}`,
+      border: `${theme.scheme.desktop.stroke.weight[1]}px solid ${theme.color.semantic.stroke.alpha.subtler}`,
       borderRadius: 'inherit',
 
       '& img': {
@@ -55,7 +57,7 @@ export const ImageButton = styled.button<ImageButtonProps>(
       },
 
       '&:hover::after': {
-        backgroundColor: theme.color.curtain.dimmer,
+        backgroundColor: theme.color.semantic.curtain.dimmer,
         opacity: isReadonly ? 0 : 1,
         cursor: isReadonly ? 'default' : 'pointer',
       },
@@ -87,7 +89,7 @@ export const ImageLabelDiv = styled.div(({ theme }) => {
     left: '8px',
     zIndex: 50,
     minWidth: `${pxToRem(18)}`,
-    backgroundColor: theme.color.object.static.neutral,
+    backgroundColor: theme.color.semantic.object.static.neutral,
     padding: `0 ${theme.scheme.desktop.spacing[2]}px`,
     borderRadius: theme.scheme.desktop.radius[2],
   };
@@ -95,12 +97,12 @@ export const ImageLabelDiv = styled.div(({ theme }) => {
 
 export const StyledLabel = styled(Label)(({ theme }) => {
   return {
-    color: theme.color.object.static.inverse.boldest,
+    color: theme.color.semantic.object.static.inverse.boldest,
   };
 });
 
 export const StyledIcon = styled(Icon)(({ theme }) => {
   return {
-    color: theme.color.object.static.inverse.boldest,
+    color: theme.color.semantic.object.static.inverse.boldest,
   };
 });
