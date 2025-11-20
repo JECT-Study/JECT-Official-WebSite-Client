60f729c22c8fc17549847f3ef55e5a38db842a6a
Banana_milk <101927445+WonJuneKim@users.noreply.github.com>
2025-11-21 06:05:42 +0900

    feat: 디자인 시스템 - Checkbox 컴포넌트 구현 (#259)
    * feat: NameSpace Pattern을 이용해 Checkbox 컴포넌트를 구현합니다.
    
    * feat: CheckBox 컴포넌트의 스타일링을 선언합니다.
    
    * feat: Checkbox가 Content 형태일 때 크기 조정을 위해 Wrapper를 추가합니다.
    
    * feat: Checkbox의 인터페이스를 외부 API와 내부 private 형태로 분리합니다.
    
    * feat: CheckBox가 형태에 맞게 인터렉션 되는 구조로 변경합니다.
    
    * feat: checkbox에서는 input이 hidden 이기 때문에 자식의 focus를 감지하도록 변경합니다.
    
    * feat: CheckBox.Content의 Interaction 매개변수 처리를 함수를 사용하여 분기합니다.
    
    * feat: refCallBack 방식을 이용해 Input 태그의 ref를 관리합니다.
    - Checkbox 에서 수행되고 있던 불필요한 UseEffect 호출을 지양하기 위함
    - 추가적으로, 타입 선언 방식이 변동되어 반영
    
    * feat: 엄격한 타입으로 관리되던 props들을 checkedState로 일원화 합니다.
    
    * feat: checkbox의 focus 이벤트 속성을 focus-visible 로 변경합니다.
    
    * feat: Checkbox 컴포넌트 관련 속성들을 export합니다.
    
    * feat: Component 단위에서 Checkbox를 export 합니다.
    
    * test: Checkbox의 스토리 파일을 작성합니다.
    
    * fix: 이미지 컴포넌트의 스타일링 에러를 수정합니다.
    
    * feat: Ref 관리 추후 방향성을 NOTE를 사용하여 작성합니다.
    
    * feat: 수정된 디자인 토큰을 checkbox에 적용합니다.
    
    * test: RefCallback 구조의 재렌더링 시점을 확인하기 위해 로깅 처리를 합니다.
    
    * refactor: useLayoutEffect를 사용해 ref를 유지하며 속성 변경이 되도록 합니다.
    -refCallback 시 indeterminate의 의존성 제거
    -useLayoutEffect로 indeterminate 관리
    
    * test: 불필요한 스토리북 케이스를 제거합니다.

---
+++ packages/jds/src/components/Checkbox/index.ts
@@ -0,0 +1,10 @@
+export { Checkbox } from './Checkbox';
+export type {
+  CheckboxBasicProps,
+  CheckboxBoxProps,
+  CheckboxContentProps,
+  CheckboxSize,
+  CheckboxVariant,
+  CheckboxAlign,
+  CheckedState,
+} from './checkbox.types';
