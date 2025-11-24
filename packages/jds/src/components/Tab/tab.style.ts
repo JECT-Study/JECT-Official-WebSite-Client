3ebb23755963c24342845c4bff5c20a6852b7c39
whdgur5717 <whdgur5717@gmail.com>
2025-11-24 20:01:50 +0900

    feat: tab variant 변경(isItemStreched -> layout)

--- packages/jds/src/components/Tab/tab.style.ts
+++ packages/jds/src/components/Tab/tab.style.ts
@@ -1,12 +1,12 @@
-import { sva } from './sva';
-import { theme as t } from '../../tokens/theme';
+import { sva, type RecipeVariantProps } from './sva';
+import { theme } from '../../tokens/theme';
 
 // TODO: 추후 공통화하기
 const interactiveSelector =
   '&:not(:is(:disabled, [disabled], [data-disabled], [aria-disabled=true]))';
 const disabledSelector = '&:is(:disabled, [disabled], [data-disabled], [aria-disabled=true])';
 
-export const tabRecipe = sva({
+export const tabStyles = sva({
   slots: ['root', 'list', 'trigger', 'content', 'indicator'] as const,
   base: {
     root: {
@@ -17,7 +17,7 @@ export const tabRecipe = sva({
     list: {
       display: 'flex',
       alignItems: 'center',
-      gap: t.scheme.semantic.spacing[0],
+      gap: theme.scheme.semantic.spacing[0],
       position: 'relative',
     },
     trigger: {
@@ -26,21 +26,20 @@ export const tabRecipe = sva({
       display: 'inline-flex',
       justifyContent: 'center',
       alignItems: 'center',
-      gap: t.scheme.semantic.spacing[4],
-      color: t.color.semantic.object.alternative,
+      gap: theme.scheme.semantic.spacing[4],
+      color: theme.color.semantic.object.alternative,
       cursor: 'pointer',
       '&[data-state="active"]': {
-        color: t.color.semantic.object.bolder,
+        color: theme.color.semantic.object.bolder,
       },
       '&::after': {
         content: '""',
         position: 'absolute',
         inset: 0,
         borderRadius: 'inherit',
-        backgroundColor: t.color.semantic.interaction.assistive,
+        backgroundColor: theme.color.semantic.interaction.assistive,
         opacity: 0,
-        transition: `opacity ${t.environment.semantic.duration[100]} ${t.environment.semantic.motion.fluent}
-          `,
+        transition: `opacity ${theme.environment.semantic.duration[100]} ${theme.environment.semantic.motion.fluent}`,
         zIndex: -1,
       },
       [`${interactiveSelector}:hover::after`]: {
@@ -58,8 +57,8 @@ export const tabRecipe = sva({
       },
       '&:focus-visible': {
         outline: 'none',
-        boxShadow: `inset 0 0 0 ${t.scheme.semantic.strokeWeight[3]} ${t.color.semantic.interaction.focus}`,
-        opacity: t.scheme.semantic.opacity[100],
+        boxShadow: `inset 0 0 0 ${theme.scheme.semantic.strokeWeight[3]} ${theme.color.semantic.interaction.focus}`,
+        opacity: theme.scheme.semantic.opacity[100],
       },
       [disabledSelector]: {
         cursor: 'not-allowed',
@@ -71,46 +70,48 @@ export const tabRecipe = sva({
     content: {},
     indicator: {
       position: 'absolute',
-      borderWidth: 0,
-      borderBottomWidth: t.scheme.semantic.strokeWeight[2],
+      borderBottomWidth: theme.scheme.semantic.strokeWeight[1],
       borderBottomStyle: 'solid',
-      borderBottomColor: t.color.semantic.stroke.bold,
-      zIndex: 0,
+      borderBottomColor: theme.color.semantic.stroke.bold,
+      zIndex: 10,
     },
   },
   variants: {
     variant: {
       header: {
         list: {
-          borderRadius: t.scheme.semantic.radius[0],
-          borderBottomWidth: t.scheme.semantic.strokeWeight[1],
+          borderRadius: theme.scheme.semantic.radius[0],
+          borderBottomWidth: theme.scheme.semantic.strokeWeight[1],
           borderBottomStyle: 'solid',
-          borderBottomColor: t.color.semantic.stroke.assistive,
-          opacity: t.scheme.semantic.opacity[100],
+          borderBottomColor: theme.color.semantic.stroke.assistive,
+          opacity: theme.scheme.semantic.opacity[100],
         },
         trigger: {
-          padding: `${t.scheme.semantic.spacing[6]} ${t.scheme.semantic.spacing[12]}`,
+          padding: `${theme.scheme.semantic.spacing[6]} ${theme.scheme.semantic.spacing[12]}`,
           minHeight: '39px',
+        },
+        indicator: {
+          display: 'block',
         },
       },
       content: {
         list: {
-          gap: t.scheme.semantic.spacing[8],
+          gap: theme.scheme.semantic.spacing[8],
         },
         trigger: {
-          padding: `${t.scheme.semantic.spacing[8]} ${t.scheme.semantic.spacing[12]}`,
-          border: '1px solid',
-          borderColor: t.color.semantic.stroke.alpha.subtle,
-          borderRadius: t.scheme.semantic.radius[6],
+          padding: `${theme.scheme.semantic.spacing[8]} ${theme.scheme.semantic.spacing[12]}`,
+          border: `${theme.scheme.semantic.strokeWeight[1]} solid ${theme.color.semantic.stroke.alpha.subtle}`,
+          borderRadius: theme.scheme.semantic.radius[6],
           minHeight: '35px',
         },
         indicator: {
           display: 'none',
+          bottom: `calc(-1 * ${theme.scheme.semantic.strokeWeight[1]})`, //list의 borderBottom과 겹치도록 -1로 설정
         },
       },
     },
-    isItemStretched: {
-      false: {
+    layout: {
+      hug: {
         list: {
           width: 'auto',
           justifyContent: 'flex-start',
@@ -119,7 +120,7 @@ export const tabRecipe = sva({
           flex: 'initial',
         },
       },
-      true: {
+      fill: {
         list: {
           width: '100%',
           alignItems: 'stretch',
@@ -134,6 +135,8 @@ export const tabRecipe = sva({
   },
   defaultVariants: {
     variant: 'header',
-    isItemStretched: 'false',
+    layout: 'hug',
   },
 });
+
+export type TabVariantProps = RecipeVariantProps<typeof tabStyles>;
