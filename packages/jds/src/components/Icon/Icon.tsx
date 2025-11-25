5e1fd341e69b4d2c9576bb0988d5380574ebdb3e
onejuneee <onejuneee721@gmail.com>
2025-11-26 08:24:59 +0900

    refactor: Icon의 Wrapper의 타입을 실제 컴포넌트의 타입을 직접 참조하도록 변경합니다.

--- packages/jds/src/components/Icon/Icon.tsx
+++ packages/jds/src/components/Icon/Icon.tsx
@@ -1,10 +1,10 @@
-import { forwardRef } from 'react';
+import { type ElementRef, forwardRef } from 'react';
 
 import { StyledIconWrapper } from './Icon.styles';
 import type { IconProps } from './Icon.types';
 import { iconMap, sizeMap } from './IconMap';
 
-export const Icon = forwardRef<HTMLSpanElement, IconProps>(
+export const Icon = forwardRef<ElementRef<typeof StyledIconWrapper>, IconProps>(
   ({ name, size = 'md', color = 'currentColor', ...props }, ref) => {
     const IconComponent = iconMap[name];
 
