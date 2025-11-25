5c14eaf30e89808b4d19b5b7f41522bff39cddae
onejuneee <onejuneee721@gmail.com>
2025-11-25 18:46:07 +0900

    feat: Card의 인터렉션을 위한 Overlay를 추가합니다.

---
+++ packages/jds/src/components/Card/compound/CardOverlay.tsx
@@ -0,0 +1,19 @@
+import { StyledCardOverlay } from './compound.styles';
+
+import { PolymorphicForwardRef } from '@/utils/forwardRef';
+
+type CardOverlayOwnProps = Record<string, never>;
+
+export const CardOverlay = PolymorphicForwardRef<'a', CardOverlayOwnProps>(
+  ({ as, children, ...restProps }, ref) => {
+    const Component = as || 'a';
+
+    return (
+      <StyledCardOverlay as={Component} ref={ref} data-overlay {...restProps}>
+        {children}
+      </StyledCardOverlay>
+    );
+  },
+);
+
+CardOverlay.displayName = 'Card.Overlay';
