e008dd1a101dfacabc61e720177a2e2733379863
onejuneee <onejuneee721@gmail.com>
2025-11-26 06:43:45 +0900

    feat: Tooltip의 Side를 결정짓는 props를 별도로 선언하지 않고 radix에서 제공하는 인터페이스를 사용합니다.

--- packages/jds/src/components/Tooltip/tooltip.types.ts
+++ packages/jds/src/components/Tooltip/tooltip.types.ts
@@ -1,15 +1,12 @@
 import type { Tooltip } from 'radix-ui';
 import type { ReactNode } from 'react';
 
-export type TooltipSide = 'top' | 'right' | 'bottom' | 'left';
-
 export interface TooltipProps extends Omit<Tooltip.TooltipProps, 'children'> {
   children: ReactNode;
-  side?: TooltipSide;
-  sideOffset?: number;
-  collisionPadding?: number;
 }
 
 export type TooltipTriggerProps = Tooltip.TooltipTriggerProps;
 
-export type TooltipContentProps = Tooltip.TooltipContentProps;
+export interface TooltipContentProps extends Tooltip.TooltipContentProps {
+  children?: ReactNode;
+}
