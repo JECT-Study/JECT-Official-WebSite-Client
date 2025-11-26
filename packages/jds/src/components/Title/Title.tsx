0372d6ba28e8aae879d479073ae1d2758065ef20
rkdcodus <rcd7275@naver.com>
2025-11-26 16:53:09 +0900

    fix: Title 컴포넌트를 다형성 컴포넌트로 변경합니다

--- packages/jds/src/components/Title/Title.tsx
+++ packages/jds/src/components/Title/Title.tsx
@@ -1,27 +1,25 @@
-import type { ReactNode } from 'react';
+import type { ElementRef, ElementType, HTMLAttributes, ReactNode } from 'react';
+import { forwardRef } from 'react';
 
 import type { TitleSize, TitleTextAlign } from './Title.style';
-import { TitleDiv } from './Title.style';
+import { StyledTitle } from './Title.style';
 
-export interface TitleProps {
+export interface TitleProps extends HTMLAttributes<HTMLElement> {
+  as?: ElementType;
   size?: TitleSize;
   textAlign?: TitleTextAlign;
   color?: string;
   children: ReactNode;
 }
 
-export const Title = ({
-  size = 'md',
-  textAlign = 'left',
-  color,
-  children,
-  ...props
-}: TitleProps) => {
-  return (
-    <TitleDiv size={size} textAlign={textAlign} color={color} {...props}>
-      {children}
-    </TitleDiv>
-  );
-};
+export const Title = forwardRef<ElementRef<typeof StyledTitle>, TitleProps>(
+  ({ as, size = 'md', textAlign = 'left', color, children, ...props }, ref) => {
+    return (
+      <StyledTitle as={as} ref={ref} size={size} textAlign={textAlign} color={color} {...props}>
+        {children}
+      </StyledTitle>
+    );
+  },
+);
 
 Title.displayName = 'Title';
