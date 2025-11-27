c3567a3d424deb720c4c8ccb9bdea9f07cb317a8
whdgur5717 <whdgur5717@gmail.com>
2025-11-27 19:32:14 +0900

    feat: storybook main.ts to esm

--- packages/jds/.storybook/main.ts
+++ packages/jds/.storybook/main.ts
@@ -1,6 +1,11 @@
+// This file has been automatically migrated to valid ESM format by Storybook.
+import { fileURLToPath } from "node:url";
 import { createRequire } from "node:module";
 import type { StorybookConfig } from "@storybook/react-vite";
 import { resolve, dirname, join } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const require = createRequire(import.meta.url);
 
