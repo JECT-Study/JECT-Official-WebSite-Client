964ed355960825d5f9f2cc67da14a7047ea65178
onejuneee <onejuneee721@gmail.com>
2025-11-30 19:00:35 +0900

    feat: CardOverlay가 context 내부에서 소비되도록 구조를 변경합니다.

--- packages/jds/src/components/Card/compound/CardOverlay.tsx
+++ packages/jds/src/components/Card/compound/CardOverlay.tsx
@@ -1,3 +1,6 @@
+import { useContext } from "react";
+
+import { CardContext } from "../Card.context";
 import { StyledCardOverlay } from "./compound.styles";
 
 import { PolymorphicForwardRef } from "@/utils/forwardRef";
@@ -7,9 +10,21 @@ type CardOverlayOwnProps = Record<string, never>;
 export const CardOverlay = PolymorphicForwardRef<"a", CardOverlayOwnProps>(
   ({ as, children, ...restProps }, ref) => {
     const Component = as || "a";
+    const context = useContext(CardContext);
+
+    if (!context) {
+      throw new Error("CardOverlay 는 Card.Root 내부에서 사용되어야 합니다.");
+    }
 
     return (
-      <StyledCardOverlay as={Component} ref={ref} data-overlay {...restProps}>
+      <StyledCardOverlay
+        as={Component}
+        ref={ref}
+        data-overlay
+        $variant={context.variant}
+        $cardStyle={context.cardStyle}
+        {...restProps}
+      >
         {children}
       </StyledCardOverlay>
     );
