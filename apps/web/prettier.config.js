ae15d9d8bf8c83fa5048ad3e0aef78cab20e5b12
whdgur5717 <whdgur5717@gmail.com>
2025-12-01 04:37:54 +0900

    chor(web): prettier 설정 변경 - prettier-plugin-tailwindcss 관련 설정 변경

--- apps/web/prettier.config.js
+++ apps/web/prettier.config.js
@@ -1,5 +1,10 @@
 import config from "../../prettier.config.mjs";
+import { createRequire } from "node:module";
+
+const require = createRequire(import.meta.url);
+
 export default {
   ...config,
-  plugins: ["prettier-plugin-tailwindcss"],
+  plugins: [require.resolve("prettier-plugin-tailwindcss")],
+  tailwindStylesheet: "./src/styles/global.css",
 };
