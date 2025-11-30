d2440f413ba65b0b550e0d96c8f39270862b61cd
whdgur5717 <whdgur5717@gmail.com>
2025-12-01 04:56:54 +0900

    chore: lint-staged 설정 변경

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,9 +1,4 @@
 export default {
-  "*.{ts,tsx,js,css,md,json}": {
-    title: "staging된 파일에 대해 prettier를 적용합니다",
-    task: files => {
-      `prettier --write ${files.join(" ")}`;
-    },
-  },
+  "*.{ts,tsx,js,css,md,json}": stagedFiles => `prettier --write ${stagedFiles.join(" ")}`,
 };
 1;
