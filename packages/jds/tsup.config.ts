025db522c63cc2f3bac35715715e52a6afde0fdf
rkdcodus <rcd7275@naver.com>
2025-12-19 02:07:55 +0900

    chore: jds의 utils를 외부에서 사용할 수 있도록 설정합니다

--- packages/jds/tsup.config.ts
+++ packages/jds/tsup.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
     index: "src/index.ts", //컴포넌트 파일
     theme: "src/theme/index.tsx", //테마 파일
     hooks: "src/hooks/index.ts", //hooks 파일
+    utils: "src/utils/index.ts", //utils 파일
   },
   format: ["cjs", "esm"],
   dts: true,
