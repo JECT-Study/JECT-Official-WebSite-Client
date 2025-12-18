3a583231222c9248c9683dbb08d460fec3dd5c71
rkdcodus <rcd7275@naver.com>
2025-12-18 23:27:52 +0900

    feat: PATH 생성 및 GNB에 href를 연결합니다

--- apps/web/src/components/gnb/Menus.tsx
+++ apps/web/src/components/gnb/Menus.tsx
@@ -1,15 +1,25 @@
 import { MegaMenu, MenuItem } from "@ject/jds";
 
+import { PATH } from "@/constants/path";
+
 export const JectMenu = () => {
+  const JDS = "https://www.figma.com/community/file/1547190026603503566/jds";
+
   return (
     <MegaMenu.Root>
       <MegaMenu.Section sectionName='젝트'>
         <MegaMenu.Group className='w-[120px]'>
           <MegaMenu.GroupItem>
-            <MenuItem.Anchor>비전과 스토리</MenuItem.Anchor>
+            <MenuItem.Anchor href={PATH.vision}>비전과 스토리</MenuItem.Anchor>
           </MegaMenu.GroupItem>
           <MegaMenu.GroupItem>
-            <MenuItem.Anchor suffixIcon='external-link-line' suffixIconVisible>
+            <MenuItem.Anchor
+              href={JDS}
+              suffixIcon='external-link-line'
+              suffixIconVisible
+              target='_blank'
+              rel='noopener noreferrer'
+            >
               JDS
             </MenuItem.Anchor>
           </MegaMenu.GroupItem>
@@ -25,13 +35,13 @@ export const ProgramMenu = () => {
       <MegaMenu.Section sectionName='프로그램'>
         <MegaMenu.Group className='w-[120px]'>
           <MegaMenu.GroupItem>
-            <MenuItem.Anchor>팀 프로젝트</MenuItem.Anchor>
+            <MenuItem.Anchor href={PATH.teamProject}>팀 프로젝트</MenuItem.Anchor>
           </MegaMenu.GroupItem>
           <MegaMenu.GroupItem>
-            <MenuItem.Anchor>미니 스터디</MenuItem.Anchor>
+            <MenuItem.Anchor href={PATH.miniStudy}>미니 스터디</MenuItem.Anchor>
           </MegaMenu.GroupItem>
           <MegaMenu.GroupItem>
-            <MenuItem.Anchor>라이브 세션</MenuItem.Anchor>
+            <MenuItem.Anchor href={PATH.liveSession}>라이브 세션</MenuItem.Anchor>
           </MegaMenu.GroupItem>
         </MegaMenu.Group>
       </MegaMenu.Section>
@@ -45,10 +55,10 @@ export const JoinGuideMenu = () => {
       <MegaMenu.Section sectionName='합류 가이드 '>
         <MegaMenu.Group className='w-[120px]'>
           <MegaMenu.GroupItem>
-            <MenuItem.Anchor>지원 안내</MenuItem.Anchor>
+            <MenuItem.Anchor href={PATH.applyInfo}>지원 안내</MenuItem.Anchor>
           </MegaMenu.GroupItem>
           <MegaMenu.GroupItem>
-            <MenuItem.Anchor>FAQ</MenuItem.Anchor>
+            <MenuItem.Anchor href={PATH.faq}>FAQ</MenuItem.Anchor>
           </MegaMenu.GroupItem>
         </MegaMenu.Group>
       </MegaMenu.Section>
