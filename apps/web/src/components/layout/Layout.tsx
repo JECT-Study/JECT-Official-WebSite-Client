6f614af4f4d6b011dc5a50c4c948e47a9693cfff
kimdonggu42 <kdk3280@undermilli.com>
2025-12-19 02:39:30 +0900

    feat: Layout에 jds Footer 컴포넌트 적용

--- apps/web/src/components/layout/Layout.tsx
+++ apps/web/src/components/layout/Layout.tsx
@@ -1,10 +1,10 @@
+import { Footer, Logo } from "@ject/jds";
 import { Outlet } from "react-router-dom";
 
 import PagesContainer from "./PagesContainer";
 import ScrollToTop from "./ScrollToTop";
 
 import Dialog from "@/components/common/dialog/Dialog";
-import Footer from "@/components/common/footer/Footer";
 // import Header from "@/components/common/navigation/Header";
 import Toast from "@/components/common/toast/Toast";
 
@@ -18,7 +18,25 @@ function Layout() {
       </PagesContainer>
       <Toast />
       <Dialog />
-      <Footer />
+      <Footer.Root>
+        <Footer.Content>
+          <Footer.Header>
+            <Footer.LogoDiv>
+              <Logo href="/" height={16} hierarchy="inverse" />
+            </Footer.LogoDiv>
+            <Footer.Social
+              github="https://github.com/JECT-Study"
+              instagram="https://www.instagram.com/ject_official"
+            />
+          </Footer.Header>
+          <Footer.Divider />
+          <Footer.Bottom
+            copyright="© 2025 JECT. All rights reserved."
+            email="jectofficial@ject.kr"
+            privacyLink="https://ject.kr/privacy"
+          />
+        </Footer.Content>
+      </Footer.Root>
     </>
   );
 }
