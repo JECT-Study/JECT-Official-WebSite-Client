0408b280129ef985f39a3b527a16731b56708123
rkdcodus <rcd7275@naver.com>
2025-12-19 02:42:18 +0900

    fix: theme 빌드 에러를 수정합니다. jds/tokens/index.ts를 외부에서 사용할 수 있도록 설정합니다.

--- packages/jds/tsup.config.ts
+++ packages/jds/tsup.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
     theme: "src/theme/index.tsx", //테마 파일
     hooks: "src/hooks/index.ts", //hooks 파일
     utils: "src/utils/index.ts", //utils 파일
+    tokens: "src/tokens/index.ts", //tokens 파일
   },
   format: ["cjs", "esm"],
   dts: true,
