148c54483ed193d9193f4f30aa70ac207c883e4b
rkdcodus <rcd7275@naver.com>
2025-12-23 00:21:53 +0900

    feat: 아코디언 컴포넌트를 구현합니다

---
+++ packages/jds/src/components/Accordion/accordion.styles.ts
@@ -0,0 +1,151 @@
+import { keyframes, type Theme } from "@emotion/react";
+import styled from "@emotion/styled";
+import * as AccordionPrimitive from "@radix-ui/react-accordion";
+import { InteractionLayer } from "utils";
+
+import type { StyledAccordionContentProps, StyledAccordionTriggerProps } from "./accordion.types";
+import { Label } from "../Label";
+
+const createInteractionStyles = (theme: Theme, isStretched: boolean, isReadonly: boolean) => {
+  const borderRadius = 4;
+  const offset = isStretched ? { vertical: 0, horizontal: 0 } : { vertical: 4, horizontal: 6 };
+
+  const makeLayer = (state: "rest" | "hover" | "active" | "focus") =>
+    InteractionLayer({
+      theme,
+      state,
+      variant: "normal",
+      density: "subtle",
+      fillColor: "default",
+      isReadonly,
+      offsetVertical: offset.vertical,
+      offsetHorizontal: offset.horizontal,
+      borderRadius,
+    });
+
+  return {
+    restStyle: makeLayer("rest"),
+    hoverStyle: makeLayer("hover"),
+    activeStyle: makeLayer("active"),
+    focusStyle: makeLayer("focus"),
+  };
+};
+
+export const StyledAccordionTrigger = styled(
+  AccordionPrimitive.Trigger,
+)<StyledAccordionTriggerProps>(({ theme, $isStretched }) => {
+  const interactionStyles = createInteractionStyles(theme, $isStretched, false);
+  const disabledInteractionStyles = createInteractionStyles(theme, $isStretched, true);
+
+  return {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "100%",
+    padding: $isStretched
+      ? `${theme.scheme.semantic.spacing[4]} ${theme.scheme.semantic.spacing[16]}`
+      : 0,
+    ...interactionStyles.restStyle,
+
+    "&:hover": {
+      ...interactionStyles.hoverStyle,
+      "::after": {
+        ...interactionStyles.hoverStyle["::after"],
+        transition: `opacity ${theme.environment.semantic.duration[100]} ${theme.environment.semantic.motion.fluent}`,
+      },
+    },
+
+    "&:active": {
+      ...interactionStyles.activeStyle,
+      "::after": {
+        ...interactionStyles.activeStyle["::after"],
+        transition: "none",
+      },
+    },
+
+    "&:focus-visible": {
+      ...interactionStyles.focusStyle,
+    },
+
+    "&[data-disabled]": {
+      color: theme.color.semantic.object.subtle,
+      ...disabledInteractionStyles.restStyle,
+
+      "&:hover": {
+        ...disabledInteractionStyles.hoverStyle,
+        "::after": {
+          ...interactionStyles.hoverStyle["::after"],
+          transition: `opacity ${theme.environment.semantic.duration[100]} ${theme.environment.semantic.motion.fluent}`,
+        },
+      },
+
+      "&:active": {
+        ...disabledInteractionStyles.activeStyle,
+        "::after": {
+          ...disabledInteractionStyles.activeStyle["::after"],
+          transition: "none",
+        },
+      },
+
+      "&:focus-visible": {
+        ...disabledInteractionStyles.focusStyle,
+      },
+    },
+
+    '&[data-state="open"] .arrowIcon': {
+      transform: "rotate(180deg)",
+    },
+  };
+});
+
+export const StyledAccordionLabelContainer = styled("div")(({ theme }) => ({
+  display: "flex",
+  justifyContent: "flex-start",
+  alignItems: "center",
+  gap: theme.scheme.semantic.spacing[8],
+}));
+
+export const StyledAccordionChevron = styled("div")(({ theme }) => ({
+  transition: `transform ${theme.environment.semantic.duration[100]} ${theme.environment.semantic.motion.fluent}`,
+}));
+
+export const StyleLabel = styled(Label)(() => ({
+  color: "inherit",
+}));
+
+const slideUp = keyframes`
+    from { height: var(--radix-accordion-content-height); }
+    to { height: 0; }
+  `;
+
+const slideDown = keyframes`
+    from { height: 0; }
+    to { height: var(--radix-accordion-content-height); }
+  `;
+
+export const StyledAccordionContent = styled(AccordionPrimitive.Content)(({ theme }) => {
+  return {
+    overflow: "hidden",
+    ...theme.textStyle["semantic-textStyle-body-sm-normal"],
+    color: theme.color.semantic.object.bold,
+
+    "&[data-disabled]": {
+      color: theme.color.semantic.object.subtle,
+    },
+
+    '&[data-state="open"]': {
+      animation: `${slideDown} ${theme.environment.semantic.duration[300]} cubic-bezier(0.87, 0, 0.13, 1)`,
+    },
+
+    '&[data-state="closed"]': {
+      animation: `${slideUp} ${theme.environment.semantic.duration[300]} cubic-bezier(0.87, 0, 0.13, 1)`,
+    },
+  };
+});
+
+export const StyledAccordionContentText = styled("div")<StyledAccordionContentProps>(
+  ({ theme, $isStretched }) => ({
+    marginTop: theme.scheme.semantic.spacing[12],
+    padding: $isStretched ? `0 ${theme.scheme.semantic.spacing[16]}` : 0,
+  }),
+);
