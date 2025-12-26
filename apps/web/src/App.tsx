8d0e380e09839cd0173e3424c6f317b5ef2e160f
onejuneee <onejuneee721@gmail.com>
2025-12-27 04:49:57 +0900

    feat: Toast를 JDS의 Provider를 이용해 사용합니다.

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,6 +1,6 @@
 import "./instrument";
-import "./utils/apiTest";
 
+import { ToastProvider } from "@ject/jds";
 import { JDSThemeProvider } from "@ject/jds/theme";
 import { MutationCache, QueryCache, QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
@@ -10,7 +10,6 @@ import { RouterProvider } from "react-router-dom";
 
 import { ErrorBoundary } from "./components/app/ErrorBoundary";
 import { LoadingFallback } from "./components/app/LoadingFallback";
-import { disabledPage, PATH } from "./constants/path";
 import { useGlobalErrorHandler } from "./hooks/useGlobalErrorHandler";
 import TempMobile from "./pages/TempMobile";
 import router from "./router";
@@ -34,18 +33,16 @@ function App() {
     return <TempMobile />;
   }
 
-  if (disabledPage.includes(window.location.pathname)) {
-    return void router.navigate(PATH.apply);
-  }
-
   return (
     <ErrorBoundary>
       <QueryClientProvider client={queryClient}>
         {import.meta.env.MODE !== "production" && <ReactQueryDevtools />}
         <JDSThemeProvider>
-          <Suspense fallback={<LoadingFallback />}>
-            <RouterProvider router={router} />
-          </Suspense>
+          <ToastProvider>
+            <Suspense fallback={<LoadingFallback />}>
+              <RouterProvider router={router} />
+            </Suspense>
+          </ToastProvider>
         </JDSThemeProvider>
       </QueryClientProvider>
     </ErrorBoundary>
