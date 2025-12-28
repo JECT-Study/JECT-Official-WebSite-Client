c545394b3d0f4bd1c28d2e5a83250dc6a01e9561
onejuneee <onejuneee721@gmail.com>
2025-12-28 16:26:57 +0900

    feat: 지원 완료 페이지의 디자인을 디자인 스펙에 맞춰 변경합니다.

--- apps/web/src/features/apply/steps/CompleteStep.tsx
+++ apps/web/src/features/apply/steps/CompleteStep.tsx
@@ -42,11 +42,11 @@ export function CompleteStep({ jobFamily }: CompleteStepProps) {
           badgeVisible={false}
         />
 
-        <div className='flex flex-col gap-(--semantic-spacing-16)'>
+        <div className='flex flex-col gap-(--semantic-spacing-24)'>
           <Title size='md' textAlign='left'>
             {APPLY_TITLE.complete}
           </Title>
-          <p className='body-lg text-object-alternative-dark'>
+          <p className='semantic-textStyle-body-sm-normal text-(--semantic-object-bold)'>
             젝트에 관심을 갖고 지원해주셔서 감사드립니다!
             <br />
             작성해 주신 지원서는 꼼꼼히 확인할 예정이며, 함께 활동하게 될 날들을 기대하겠습니다.
