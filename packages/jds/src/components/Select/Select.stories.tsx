a49e88d2a986f8e348ef780e1be11c7be47299aa
JaeHyun Bang <jhbang@plea.kr>
2026-03-04 21:20:11 +0900

    fix : storybook package import 수정, web 코드 원복

--- packages/jds/src/components/Select/Select.stories.tsx
+++ packages/jds/src/components/Select/Select.stories.tsx
@@ -1,5 +1,4 @@
-// eslint-disable-next-line storybook/no-renderer-packages
-import type { Meta, StoryObj } from "@storybook/react";
+import type { Meta, StoryObj } from "@storybook/react-vite";
 import { useState } from "react";
 
 import { Select } from "./index";
