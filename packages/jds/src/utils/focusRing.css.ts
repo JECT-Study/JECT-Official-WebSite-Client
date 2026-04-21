4b5bcc735cc70e3debb9f96def872718c6dcd106
onejuneee <onejuneee721@gmail.com>
2026-04-21 19:15:25 +0900

    refactor: 포커스링의 outline 방식을 before 가상요소 방식으로 변경합니다. - outline은 host를 따라가는데 포커스링의 outline은 host가 아니라 가상요소에 있기 때문

--- packages/jds/src/utils/focusRing.css.ts
+++ packages/jds/src/utils/focusRing.css.ts
@@ -3,11 +3,19 @@ import { style } from "@vanilla-extract/css";
 import { vars } from "../tokens/vars.css";
 
 export const focusRing = style({
+  position: "relative",
   outline: "none",
   selectors: {
-    "&[data-focus-visible]": {
-      outline: `3px solid ${vars.color.semantic.interaction.focus}`,
-      outlineOffset: "2px",
+    "&::before": {
+      content: '""',
+      position: "absolute",
+      inset: 0,
+      borderRadius: "inherit",
+      boxShadow: "none",
+      pointerEvents: "none",
+    },
+    "&[data-focus-visible]::before": {
+      boxShadow: `0 0 0 3px ${vars.color.semantic.interaction.focus}`,
     },
   },
 });
