436962ceb73f3f09d3d74b7effb1d266d2d12cc7
아름 <94692566+areumH@users.noreply.github.com>
2026-06-17 14:06:29 +0900

    feat: JDS body typography 유틸리티 함수 구현 (#465)

--- packages/jds/src/utils/typography.css.ts
+++ packages/jds/src/utils/typography.css.ts
@@ -1,13 +1,28 @@
 import { createVar, style } from "@vanilla-extract/css";
 import { recipe } from "@vanilla-extract/recipes";
 
-import type { LabelCursor, LabelSize, LabelTextAlign, LabelWeight, TitleSize } from "./typography";
+import type {
+  BodySize,
+  BodyTextAlign,
+  BodyWeight,
+  LabelCursor,
+  LabelSize,
+  LabelTextAlign,
+  LabelWeight,
+  TitleSize,
+} from "./typography";
 import { vars } from "../tokens/vars.css";
 
 const alignVariants: Record<LabelTextAlign, { justifyContent: string }> = {
   center: { justifyContent: "center" },
   left: { justifyContent: "flex-start" },
   right: { justifyContent: "flex-end" },
+};
+
+const bodyAlignVariants: Record<BodyTextAlign, { textAlign: "center" | "left" | "right" }> = {
+  center: { textAlign: "center" },
+  left: { textAlign: "left" },
+  right: { textAlign: "right" },
 };
 
 export const labelColorVar = createVar();
@@ -117,6 +132,70 @@ export const title = recipe({
   },
   defaultVariants: {
     size: "md",
+    textAlign: "left",
+  },
+});
+
+export const body = recipe({
+  variants: {
+    size: {
+      lg: {},
+      md: {},
+      sm: {},
+      xs: {},
+      "2xs": {},
+    } satisfies Record<BodySize, unknown>,
+    weight: {
+      bold: {},
+      normal: {},
+    } satisfies Record<BodyWeight, unknown>,
+    textAlign: bodyAlignVariants,
+  },
+  compoundVariants: [
+    {
+      variants: { size: "lg", weight: "bold" },
+      style: "semantic-textStyle-body-lg-bold",
+    },
+    {
+      variants: { size: "lg", weight: "normal" },
+      style: "semantic-textStyle-body-lg-normal",
+    },
+    {
+      variants: { size: "md", weight: "bold" },
+      style: "semantic-textStyle-body-md-bold",
+    },
+    {
+      variants: { size: "md", weight: "normal" },
+      style: "semantic-textStyle-body-md-normal",
+    },
+    {
+      variants: { size: "sm", weight: "bold" },
+      style: "semantic-textStyle-body-sm-bold",
+    },
+    {
+      variants: { size: "sm", weight: "normal" },
+      style: "semantic-textStyle-body-sm-normal",
+    },
+    {
+      variants: { size: "xs", weight: "bold" },
+      style: "semantic-textStyle-body-xs-bold",
+    },
+    {
+      variants: { size: "xs", weight: "normal" },
+      style: "semantic-textStyle-body-xs-normal",
+    },
+    {
+      variants: { size: "2xs", weight: "bold" },
+      style: "semantic-textStyle-body-2xs-bold",
+    },
+    {
+      variants: { size: "2xs", weight: "normal" },
+      style: "semantic-textStyle-body-2xs-normal",
+    },
+  ],
+  defaultVariants: {
+    size: "md",
+    weight: "normal",
     textAlign: "left",
   },
 });
