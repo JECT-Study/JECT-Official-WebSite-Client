f3722012798a79fbfb5fa715f05ce41c8e969ba8
areumH <dkfmal0109@gmail.com>
2026-06-23 16:13:16 +0900

    refactor: 불필요한 useMemo 제거

--- packages/jds/src/components/Toast/useToastProvider.ts
+++ packages/jds/src/components/Toast/useToastProvider.ts
@@ -1,4 +1,4 @@
-import { ReactNode, useMemo } from "react";
+import { ReactNode } from "react";
 
 import type { ToastItem, ToastOptions, UseToastProviderProps } from "./toast.types";
 
@@ -7,19 +7,16 @@ import { useLimitedQueueProvider } from "@/hooks/useLimitedQueueProvider";
 export const useToastProvider = ({ toastLimit = 3 }: UseToastProviderProps) => {
   const { items, addItem, removeItem } = useLimitedQueueProvider<ToastItem>({ limit: toastLimit });
 
-  const handler = useMemo(
-    () => ({
-      basic: (title: ReactNode, options?: ToastOptions) =>
-        addItem({ feedback: "none", title, ...options }),
-      positive: (title: ReactNode, options?: ToastOptions) =>
-        addItem({ feedback: "positive", title, ...options }),
-      destructive: (title: ReactNode, options?: ToastOptions) =>
-        addItem({ feedback: "destructive", title, ...options }),
-      notifying: (title: ReactNode, options?: ToastOptions) =>
-        addItem({ feedback: "notifying", title, ...options }),
-    }),
-    [addItem],
-  );
+  const handler = {
+    basic: (title: ReactNode, options?: ToastOptions) =>
+      addItem({ feedback: "none", title, ...options }),
+    positive: (title: ReactNode, options?: ToastOptions) =>
+      addItem({ feedback: "positive", title, ...options }),
+    destructive: (title: ReactNode, options?: ToastOptions) =>
+      addItem({ feedback: "destructive", title, ...options }),
+    notifying: (title: ReactNode, options?: ToastOptions) =>
+      addItem({ feedback: "notifying", title, ...options }),
+  };
 
   return { toasts: items, toast: handler, removeToast: removeItem };
 };
