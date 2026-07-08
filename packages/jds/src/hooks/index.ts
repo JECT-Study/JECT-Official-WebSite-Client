6c0662c1ab9808eb2f57632db5d63ee0178d6b21
지훈 <153643266+itwillbeoptimal@users.noreply.github.com>
2026-07-09 04:50:30 +0900

    refactor: JDS 인터랙션 레이어 CSS 기반으로 전환 (#516)
    * refactor: 인터랙션 상태를 순수 CSS로 전환하고 pressable 훅 제거
    
    # Conflicts:
    #	packages/jds/src/hooks/index.ts
    
    * chore: package-lock.json 동기화
    
    * feat: overlay, focusRing에 delegated 변형 추가
    
    * refactor: File 인터랙션을 공통 유틸 기반으로 전환
    
    * docs: pseudo-element 정책 문서 재작성
    
    * docs: pseudo-element 정책 문서 코드 블록에 언어 지정
    
    * fix: focusRing이 자손 focus에 반응하던 문제를 self/within 분리
    
    * docs: pseudo-element 정책 문서 업데이트

--- packages/jds/src/hooks/index.ts
+++ packages/jds/src/hooks/index.ts
@@ -1,5 +1,2 @@
-export { useContainerPressable } from "./useContainerPressable";
-export { useCompositePressable } from "./useCompositePressable";
-export { usePressable } from "./usePressable";
 export { useMediaQueryFlags } from "./useMediaQuery";
 export type { DeviceType, Breakpoints } from "./useMediaQuery";
