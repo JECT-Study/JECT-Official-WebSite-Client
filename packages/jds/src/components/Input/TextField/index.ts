9ddeb7db0fadb1dd8de7b222c7dbb6d7e34eb2bb
Zero-1016 <iolpd42@gmail.com>
2026-07-12 23:56:56 +0900

    fix: textfield 제거

--- packages/jds/src/components/Input/TextField/index.ts
+++
@@ -1,8 +0,0 @@
-import { TextField as TextFieldBase } from "./TextField";
-import { TextFieldButton } from "./TextFieldButton";
-
-export const TextField = Object.assign(TextFieldBase, {
-  Button: TextFieldButton,
-});
-
-export type { TextFieldPublicProps, TextFieldProps, TextFieldButtonProps } from "./textField.types";
