a88a4f6f12a5951bbef2ae16e2ef82b309395850
itwillbeoptimal <itwillbeoptimal@gmail.com>
2026-07-14 22:44:14 +0900

    chore: 랜딩 인트로 각주 Label 주석 처리

--- apps/web/src/components/main/sections/IntroSection.tsx
+++ apps/web/src/components/main/sections/IntroSection.tsx
@@ -77,9 +77,9 @@ const IntroSection = () => {
                   </Callout.Basic>
                 ))}
               </div>
-              <Label as='span' size='sm' weight='normal' color='var(--semantic-object-assistive)'>
-                {/**현 4기 기준, 진행 완료 및 진행중 프로젝트 포함.*/}
-              </Label>
+              {/*<Label as='span' size='sm' weight='normal' color='var(--semantic-object-assistive)'>*/}
+              {/*  *현 4기 기준, 진행 완료 및 진행중 프로젝트 포함.*/}
+              {/*</Label>*/}
             </div>
 
             <LabelButton.Basic
