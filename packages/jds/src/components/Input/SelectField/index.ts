8fc77d178974b82f17767c25ae9ac6db519a6f20
itwillbeoptimal <itwillbeoptimal@gmail.com>
2026-08-04 23:18:18 +0900

    chore: Emotion 기반 컴포넌트 제거
    # Conflicts:
    #	packages/jds/src/components/Banner/Banner.tsx
    #	packages/jds/src/components/Dialog/Dialog.stories.tsx
    #	packages/jds/src/components/Dialog/Dialog.tsx
    #	packages/jds/src/components/Dialog/index.ts
    #	packages/jds/src/components/Input/SelectField/SelectField.stories.tsx
    #	packages/jds/src/components/Input/TagField/TagField.stories.tsx
    #	packages/jds/src/components/Input/TagField/TagFieldButton.tsx
    #	packages/jds/src/components/Input/TagField/TagItem.tsx
    #	packages/jds/src/components/Navigation/GlobalNavigation/GlobalNavigation.stories.tsx
    #	packages/jds/src/components/Uploader/UploaderFile.stories.tsx
    #	packages/jds/src/components/Uploader/UploaderImage.stories.tsx

--- packages/jds/src/components/Input/SelectField/index.ts
+++
@@ -1,8 +0,0 @@
-import { SelectField as SelectFieldBase } from "./SelectField";
-import { SelectFieldButton } from "./SelectFieldButton";
-
-export const SelectField = Object.assign(SelectFieldBase, {
-  Button: SelectFieldButton,
-});
-
-export type { SelectFieldProps, SelectFieldButtonProps } from "./selectField.types";
