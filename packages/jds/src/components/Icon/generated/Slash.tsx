54827cbdbc1d73c4ac988044d90eae58205fe00b
d5ng <dong5ffice@gmail.com>
2026-08-14 20:07:54 +0900

    refactor: (JDS) Development와 Devices 아이콘을 Lucide로 마이그레이션

--- packages/jds/src/components/Icon/generated/Slash.tsx
+++ packages/jds/src/components/Icon/generated/Slash.tsx
@@ -1,5 +1,5 @@
 import type { SVGProps } from "react";
-const SvgTabletLine = (props: SVGProps<SVGSVGElement>) => (
+const SvgSlash = (props: SVGProps<SVGSVGElement>) => (
   <svg
     xmlns='http://www.w3.org/2000/svg'
     width='1em'
@@ -10,8 +10,8 @@ const SvgTabletLine = (props: SVGProps<SVGSVGElement>) => (
   >
     <path
       fill='currentColor'
-      d='M6 4.007v16h12v-16zm-1-2h14a1 1 0 0 1 1 1v18a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-18a1 1 0 0 1 1-1m7 15a1 1 0 1 1 0 2 1 1 0 0 1 0-2'
+      d='M21.293 1.293a1 1 0 1 1 1.414 1.414l-20 20a1 1 0 1 1-1.414-1.414z'
     />
   </svg>
 );
-export default SvgTabletLine;
+export default SvgSlash;
