2df697c01bf84f651c0061dc5f04468c66fc09a7
itwillbeoptimal <itwillbeoptimal@gmail.com>
2026-08-19 21:06:54 +0900

    chore: 선언 파일이 참조하는 csstype을 의존성으로 추가

--- packages/jds/vite.dts.config.ts
+++ packages/jds/vite.dts.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 import pkg from "./package.json" with { type: "json" };
 import { srcAliases } from "./vite.aliases";
 
-const externalPackages = [...Object.keys(pkg.dependencies), ...Object.keys(pkg.peerDependencies), "csstype"];
+const externalPackages = [...Object.keys(pkg.dependencies), ...Object.keys(pkg.peerDependencies)];
 
 const isExternal = (id: string) =>
   externalPackages.some(name => id === name || id.startsWith(`${name}/`)) ||
