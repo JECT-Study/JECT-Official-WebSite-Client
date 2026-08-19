cf01955e6960e64c62c696586865e365079ab85c
아름 <94692566+areumH@users.noreply.github.com>
2026-08-19 18:34:53 +0900

    refactor: (JDS) Kbd, Code 컴포넌트 타이포그래피 유틸 적용 및 스타일 구성 정리 (#598)
    * feat: getSyntaxClassName 유틸 구현
    
    * refactor: Code 컴포넌트에 syntax 타이포그래피 유틸 적용
    
    * refactor: Kbd 컴포넌트에 타이포그래피 유틸 적용
    
    * refactor: Kbd 컴포넌트 스타일 구조 및 사이즈 규격 정리
    
    * docs: Kbd 컴포넌트 스토리북 작성
    
    * refactor: Kbd 컴포넌트의 muted prop을 isMuted로 통일
    
    * fix: Kbd border에 storke 토큰 적용
    
    * fix: Code border에 storke 토큰 적용
    
    * docs: Code changeset 문서 작성
    
    * docs: Kbd changeset 문서 작성
    
    * docs: syntax 타이포그래피 유틸 함수 changeset 문서 작성
    
    * docs: Code, Kbd 컴포넌트 changeset 문서 재작성
    
    * chore(vrt): CI에서 baseline 재생성
    
    * fix: Kbd 컴포넌트의 상하 패딩 제거
    
    * fix: Kbd 컴포넌트 타이포그래피 스타일 충돌 방지
    
    * docs: Kbd changeset 문서 수정
    
    * fix: Kbd paddingInline에 spacing 토큰 적용
    
    * docs: Kbd changeset 문서 수정
    
    * refactor: Code, Kbd의 타이포그래피 크기 매핑 분리
    
    * fix: syntax 타이포그래피 클래스명 타입 검증 추가
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- packages/jds/src/components/Kbd/kbd.types.ts
+++ packages/jds/src/components/Kbd/kbd.types.ts
@@ -1,11 +1,11 @@
 import type { ComponentPropsWithoutRef, ReactNode } from "react";
 
-export type KbdSize = "lg" | "md" | "sm" | "xs";
+export type KbdSize = "lg" | "md" | "sm";
 export type KbdType = "key" | "text" | "function";
 
 export interface KbdProps extends ComponentPropsWithoutRef<"kbd"> {
   children: ReactNode;
   size?: KbdSize;
   type?: KbdType;
-  muted?: boolean;
+  isMuted?: boolean;
 }
