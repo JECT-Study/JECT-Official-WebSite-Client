0d1ab6743bc45237c128b8d9e2d931fbefbd34a6
itwillbeoptimal <itwillbeoptimal@gmail.com>
2026-08-28 02:42:15 +0900

    feat: TextField 글자 수 카운터 지원

--- packages/jds/src/components/TextField/index.ts
+++ packages/jds/src/components/TextField/index.ts
@@ -4,4 +4,5 @@ export type { TextFieldInputProps } from "./compound/Input";
 export type {
   FieldLabelProps as TextFieldLabelProps,
   FieldHelperProps as TextFieldHelperProps,
+  FieldFooterProps as TextFieldFooterProps,
 } from "../Field";
