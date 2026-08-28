e0ec7ef808b8b79dd3315070cb5c214ad89eae4c
itwillbeoptimal <itwillbeoptimal@gmail.com>
2026-08-29 03:06:09 +0900

    chore: 참조되지 않는 FieldCounterState export 제거

--- packages/jds/src/components/Field/index.tsx
+++ packages/jds/src/components/Field/index.tsx
@@ -5,5 +5,4 @@ export type { FieldContentProps } from "./compound/Content";
 export type { FieldLabelProps } from "./compound/Label";
 export type { FieldHelperProps } from "./compound/Helper";
 export type { FieldCounterProps } from "./compound/Counter";
-export type { FieldCounterState } from "./field.types";
 export type { FieldFooterProps } from "./compound/Footer";
