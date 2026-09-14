bdbcbcfa8335835798669509df262c454564ac02
지훈 <153643266+itwillbeoptimal@users.noreply.github.com>
2026-09-14 19:02:26 +0900

    refactor: (JDS) Menu를 펼쳐둔 목록과 드롭다운으로 분리 (#697)
    * refactor: 메뉴 항목 구현과 스타일을 Menu 디렉토리로 이동
    
    * feat: 펼쳐둔 목록용 Menu와 드롭다운용 DropdownMenu로 분리
    
    * docs: changeset 문서 작성
    
    * chore(vrt): CI에서 baseline 재생성
    
    * fix: 비활성 링크 스토리가 disabled prop을 사용하도록 수정
    
    * chore(vrt): CI에서 baseline 재생성

---
+++ packages/jds/src/components/Menu/index.ts
@@ -0,0 +1,20 @@
+export { Menu } from "./Menu";
+export { DropdownMenu } from "./DropdownMenu";
+export type {
+  MenuStyle,
+  MenuSize,
+  MenuButtonVariant,
+  MenuAnchorVariant,
+  MenuRootProps,
+  MenuContentProps,
+  MenuCategoryProps,
+  MenuGroupProps,
+  MenuButtonProps,
+  MenuAnchorProps,
+  DropdownMenuRootProps,
+  DropdownMenuTriggerProps,
+  DropdownMenuContentProps,
+  DropdownMenuButtonProps,
+  DropdownMenuAnchorProps,
+  DropdownMenuTreeProps,
+} from "./menu.types";
