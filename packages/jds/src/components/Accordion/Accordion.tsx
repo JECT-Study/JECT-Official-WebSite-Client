505b8b1b0f4667af8c995b506e2a1d480d236f31
areumH <dkfmal0109@gmail.com>
2026-09-18 16:24:34 +0900

    refactor: Accordion의 Radix UI import 경로 통일
    - Radix 하위 패키지 직접 참조를 통합 패키지 export로 변경
    - 렌더링 구조와 공개 타입 및 스타일 동작은 그대로 유지

--- packages/jds/src/components/Accordion/Accordion.tsx
+++ packages/jds/src/components/Accordion/Accordion.tsx
@@ -1,5 +1,5 @@
-import * as AccordionPrimitive from "@radix-ui/react-accordion";
 import { clsx } from "clsx";
+import { Accordion as AccordionPrimitive } from "radix-ui";
 import { forwardRef } from "react";
 
 import { AccordionProvider, useAccordionContext } from "./accordion.context";
