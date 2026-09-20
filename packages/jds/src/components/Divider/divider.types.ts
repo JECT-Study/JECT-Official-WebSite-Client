f8a44cce9e6bc1763785ade31987f9b72b384ab3
ccconac <qwerpoiu91@naver.com>
2026-09-20 20:06:32 +0900

    refactor: (JDS) Divider props 유니온 멤버를 interface로 분리

--- packages/jds/src/components/Divider/divider.types.ts
+++ packages/jds/src/components/Divider/divider.types.ts
@@ -4,17 +4,22 @@ export type DividerThickness = "normal" | "bold" | "bolder" | "boldest";
 export type DividerOrientation = "horizontal" | "vertical";
 export type DividerVariant = "solid" | "dashed";
 
-export interface BaseDividerProps extends Omit<
-  ComponentPropsWithoutRef<"hr">,
-  "children" | "color"
-> {
+interface BaseDividerProps extends Omit<ComponentPropsWithoutRef<"hr">, "children" | "color"> {
   thickness?: DividerThickness;
   decorative?: boolean;
   orientation?: DividerOrientation;
 }
 
-export type DividerProps = BaseDividerProps &
-  (
-    | { variant?: Exclude<DividerVariant, "dashed">; dashLength?: never; dashGap?: never }
-    | { variant: "dashed"; dashLength?: number; dashGap?: number }
-  );
+interface SolidDividerProps {
+  variant?: Exclude<DividerVariant, "dashed">;
+  dashLength?: never;
+  dashGap?: never;
+}
+
+interface DashedDividerProps {
+  variant: "dashed";
+  dashLength?: number;
+  dashGap?: number;
+}
+
+export type DividerProps = BaseDividerProps & (SolidDividerProps | DashedDividerProps);
